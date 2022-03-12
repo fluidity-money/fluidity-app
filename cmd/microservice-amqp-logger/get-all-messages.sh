3e1cda258513c0dfdee6890e459838d06b53def6
Alex <>
2022-03-12 23:26:43 +1100

    Rename and replace '*' with '#' to get all messages

--- cmd/microservice-amqp-logger/get-all-messages.sh
+++ cmd/microservice-amqp-logger/get-all-messages.sh
@@ -3,5 +3,5 @@
 \
 	FLU_WORKER_ID=microservice-amqp-logger \
 	FLU_AMQP_QUEUE_ADDR=amqp://fluidity:fluidity@127.0.0.1 \
-	FLU_AMQP_TOPIC_CONSUME='*' \
+	FLU_AMQP_TOPIC_CONSUME='#' \
 	./microservice-amqp-logger.o
