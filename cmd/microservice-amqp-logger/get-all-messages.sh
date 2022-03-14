38bfa0f5a376363e91e5178218511b734e527998
Alex <>
2022-03-14 17:29:00 +1100

    Add "drain-messages.sh" script to make it easier to drain when things go wrong

--- cmd/microservice-amqp-logger/get-all-messages.sh
+++ cmd/microservice-amqp-logger/get-all-messages.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-\
-	FLU_WORKER_ID=microservice-amqp-logger \
-	FLU_AMQP_QUEUE_ADDR=amqp://fluidity:fluidity@127.0.0.1 \
-	FLU_AMQP_TOPIC_CONSUME='#' \
-	./microservice-amqp-logger.o
+export FLU_AMQP_QUEUE_ADDR="amqp://fluidity:fluidity@127.0.0.1"
+
+./drain-queue.sh "microservice-amqp-logger" "#"
