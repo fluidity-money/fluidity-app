f9346ada1f4cc5d7f8411180634d9fa9105f062b
Connor <mussonconnor@gmail.com>
2022-03-16 14:47:25 +0100

    made link button and added new scaling for smaller devices

--- web/ethereum.fluidity.money/src/components/Toolbar/Toolbar.tsx
+++ web/ethereum.fluidity.money/src/components/Toolbar/Toolbar.tsx
@@ -1,3 +1,4 @@
+import Button from "components/Button";
 import React from "react";
 import { useEffect } from "react";
 
@@ -45,24 +46,34 @@ const Toolbar = ({ children }: { children: JSX.Element }) => {
       {children}
       {desiredNetwork === false ? (
         <div className="change-network-message">
-          App network (
-          {process.env.REACT_APP_CHAIN_ID === `3`
-            ? `Ethereum Ropsten`
-            : process.env.REACT_APP_CHAIN_ID === `1`
-            ? `Ethereum Mainnet`
-            : `Ethereum`}
-          )
-          {` doesn't match to network selected in wallet (network with id:
-          ${chainId}). Learn how to change`}
-          <a
-            className="change-network-link"
-            href="https://metamask.zendesk.com/hc/en-us/articles/4404424659995"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {` network in wallet`}
-          </a>
-          {` or `}
+          <div>
+            App network (
+            {process.env.REACT_APP_CHAIN_ID === `3`
+              ? `Ethereum Ropsten`
+              : process.env.REACT_APP_CHAIN_ID === `1`
+              ? `Ethereum Mainnet`
+              : `Ethereum`}
+            )
+            {` doesn't match to network selected in wallet (network with id:
+          ${chainId}). Learn how to`}
+            <a
+              className="learn-change-network-link"
+              href="https://metamask.zendesk.com/hc/en-us/articles/4404424659995"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {` change network in wallet`}
+            </a>
+            {` or   `}
+            {/* <Button
+            label={"Change Network"}
+            theme="primary-text"
+            texttheme="header-text"
+            goto={() => (window.location.href = "https://chainlist.org/")}
+            priviledge={0}
+            selected={true}
+          /> */}
+          </div>
           <a
             className="change-network-link"
             href="https://chainlist.org/"
@@ -71,7 +82,6 @@ const Toolbar = ({ children }: { children: JSX.Element }) => {
           >
             change network
           </a>
-          .
         </div>
       ) : (
         <></>
