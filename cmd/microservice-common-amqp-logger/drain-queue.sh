bae2177d45abe02dc276b169a812b1dd6d4d77c7
Alex <>
2022-03-17 15:38:04 +1100

    Have overloadable default for FLU_AMQP_QUEUE_ADDR in the AMQP logger

--- cmd/microservice-common-amqp-logger/drain-queue.sh
+++ cmd/microservice-common-amqp-logger/drain-queue.sh
@@ -7,7 +7,7 @@ queue_name="$2"
 
 [ -z "$queue_name" ] && exit 1
 
-export FLU_AMQP_QUEUE_ADDR="amqp://fluidity:fluidity@127.0.0.1"
+export FLU_AMQP_QUEUE_ADDR="${FLU_AMQP_QUEUE_ADDR:-amqp://fluidity:fluidity@127.0.0.1}"
 
 export \
 	FLU_AMQP_TOPIC_CONSUME="$queue_name" \
--- cmd/microservice-common-amqp-logger/get-all-messages.sh
+++ cmd/microservice-common-amqp-logger/get-all-messages.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-export FLU_AMQP_QUEUE_ADDR="amqp://fluidity:fluidity@127.0.0.1"
-
 ./drain-queue.sh "microservice-amqp-logger" "#"
