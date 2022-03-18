2f4b690e823c956e13c7fcb39034150a51cb1f97
glo12345 <glorison@fluidity.money>
2022-03-18 20:20:53 +1100

    Readded erc20 types as lib/databases/ethereum/erc20/transfer.go requires it

---
+++ lib/types/ethereum/erc20/erc20.go
@@ -0,0 +1,23 @@
+package erc20
+
+// erc20 implements some of the ERC20 methods with extra information baked in
+
+import (
+	"time"
+
+	"github.com/fluidity-money/fluidity-app/lib/types/ethereum"
+	"github.com/fluidity-money/fluidity-app/lib/types/misc"
+)
+
+// Transfer made using a ERC20 contract
+type Transfer struct {
+	ContractAddress ethereum.Address `json:"contract_address"`
+	FromAddress     ethereum.Address `json:"from_address"`
+	ToAddress       ethereum.Address `json:"to_address"`
+	Amount          misc.BigInt      `json:"amount"`
+
+	// PickedUp is when we processed this event
+	PickedUp time.Time `json:"time"`
+
+	TransactionHash ethereum.Hash `json:"transaction_hash"`
+}
