4ffdcda2dfd78e7fea7a608efd53990aac1a4cb3
Alex <>
2022-03-22 00:53:46 +1100

    Add client code to Solana worker

--- cmd/microservice-solana-worker-server/main.go
+++ cmd/microservice-solana-worker-server/main.go
@@ -1,32 +1,27 @@
 package main
 
 import (
-	"context"
 	"math/big"
-	"os"
 
 	"github.com/fluidity-money/fluidity-app/lib/log"
+	"github.com/fluidity-money/fluidity-app/lib/queue"
 	"github.com/fluidity-money/fluidity-app/lib/queues/breadcrumb"
 	"github.com/fluidity-money/fluidity-app/lib/queues/user-actions"
+	"github.com/fluidity-money/fluidity-app/lib/types/worker"
 	"github.com/fluidity-money/fluidity-app/lib/util"
 
 	"github.com/fluidity-money/fluidity-app/common/calculation/probability"
-	"github.com/fluidity-money/fluidity-app/common/solana/fluidity"
 	"github.com/fluidity-money/fluidity-app/common/solana/solend"
 
 	prizePool "github.com/fluidity-money/fluidity-app/cmd/microservice-solana-prize-pool/lib/solana"
 
 	solana "github.com/gagliardetto/solana-go"
 	solanaRpc "github.com/gagliardetto/solana-go/rpc"
-	"github.com/near/borsh-go"
 )
 
 const (
 	// EnvSolanaRpcUrl is the RPC url of the solana node to connect to
 	EnvSolanaRpcUrl = `FLU_SOLANA_RPC_URL`
-
-	// EnvUpdateInterval is the interval between each prize pool update
-	EnvUpdateInterval = `FLU_SOLANA_PRIZE_POOL_UPDATE_TIME`
 
 	// EnvFluidityPubkey is the program id of the fluidity program
 	EnvFluidityPubkey = `FLU_SOLANA_PROGRAM_ID`
@@ -55,20 +50,13 @@ const (
 	// account
 	EnvSwitchboardPubkey = `FLU_SOLANA_SWITCHBOARD_PUBKEY`
 
-	// EnvSolPythPubkey is the public key of the pyth price account for SOL
-	EnvSolPythPubkey = `FLU_SOLANA_SOL_PYTH_PUBKEY`
-
 	// EnvPayerPrikey is a private key of an account that holds solana funds
 	// this must be the payout authority of the contract
 	EnvPayerPrikey = `FLU_SOLANA_PAYER_PRIKEY`
 
-	// EnvPDAPubkey is the public key of the program derived account of the
-	// fluidity contract
-	EnvPDAPubkey = `FLU_SOLANA_PDA_PUBKEY`
-
-	// EnvDebugFakePayouts is a boolean that if true, logs payout instead of
-	// submitting them to solana
-	EnvDebugFakePayouts = `FLU_DEBUG_FAKE_PAYOUTS`
+	// EnvTopicWinnerQueue to use when transmitting to a client the topic of
+	// a winner
+	EnvTopicWinnerQueue = `FLU_SOLANA_WINNER_QUEUE_NAME`
 )
 
 const (
@@ -91,7 +79,9 @@ const (
 func main() {
 
 	var (
-		rpcUrl = util.GetEnvOrFatal(EnvSolanaRpcUrl)
+		rpcUrl           = util.GetEnvOrFatal(EnvSolanaRpcUrl)
+		payerPrikey      = util.GetEnvOrFatal(EnvPayerPrikey)
+		topicWinnerQueue = util.GetEnvOrFatal(EnvTopicWinnerQueue)
 
 		fluidityPubkey    = pubkeyFromEnv(EnvFluidityPubkey)
 		fluidMintPubkey   = pubkeyFromEnv(EnvFluidityMintPubkey)
@@ -101,16 +91,11 @@ func main() {
 		reservePubkey     = pubkeyFromEnv(EnvReservePubkey)
 		pythPubkey        = pubkeyFromEnv(EnvPythPubkey)
 		switchboardPubkey = pubkeyFromEnv(EnvSwitchboardPubkey)
-		payerPrikey       = util.GetEnvOrFatal(EnvPayerPrikey)
-		PDAPubkey         = pubkeyFromEnv(EnvPDAPubkey)
-		splPubkey         = solana.MustPublicKeyFromBase58(SplProgramId)
-
-		debugFakePayouts   = os.Getenv(EnvDebugFakePayouts) == "true"
 	)
 
 	var (
-		decimalPlacesRat       = big.NewRat(UsdcDecimalPlaces, 1)
-		usdcDecimalPlacesRat   = big.NewRat(UsdcDecimalPlaces, 1)
+		decimalPlacesRat     = big.NewRat(UsdcDecimalPlaces, 1)
+		usdcDecimalPlacesRat = big.NewRat(UsdcDecimalPlaces, 1)
 	)
 
 	solanaClient := solanaRpc.New(rpcUrl)
@@ -203,14 +188,15 @@ func main() {
 			entireAmountOwned,
 		)
 
-		// recentBlockHash is only set if a winner is found so a hash request is
-		// made, but not if there's no winner in the block. only set inside the
-		// user actions loop.
-
-		var recentBlockHash solana.Hash
-
 		for _, userAction := range userActions {
+
 			// skip if it's not a send
+
+			var (
+				userActionTransactionHash  = userAction.TransactionHash
+				userActionSenderAddress    = userAction.SenderAddress
+				userActionRecipientAddress = userAction.RecipientAddress
+			)
 
 			if userAction.Type != "send" {
 				return
@@ -281,171 +267,17 @@ func main() {
 				return
 			}
 
-			// since there was a winner, it's time to set the block hash if it's not
-			// set already.
+			// send a message to the client that will kick off the payment of the
+			// winnings!
 
-			if recentBlockHash.IsZero() {
-
-				recentBlockHashResult, err := solanaClient.GetRecentBlockhash(
-					context.Background(),
-					solanaRpc.CommitmentFinalized,
-				)
-
-				if err != nil {
-					log.Fatal(func(k *log.Log) {
-						k.Message = "Failed to get a recent Solana blockhash!"
-						k.Payload = err
-					})
-				}
-
-				recentBlockHash = recentBlockHashResult.Value.Blockhash
+			winnerAnnouncement := worker.SolanaWinnerAnnouncement{
+				WinningTransactionHash: userActionTransactionHash,
+				SenderAddress:          userActionSenderAddress,
+				RecipientAddress:       userActionRecipientAddress,
+				WinningAmount:          winningAmount,
 			}
 
-			// call the payout function!
-
-			var (
-				aPubkey = solana.MustPublicKeyFromBase58(userAction.SenderAddress)
-				bPubkey = solana.MustPublicKeyFromBase58(userAction.RecipientAddress)
-			)
-
-			var (
-				// solanaAccountMetaSpl is used to know where to send transactions
-				solanaAccountMetaSpl = solana.NewAccountMeta(splPubkey, false, false)
-
-				// solanaAccountFluidMint is needed to be writable and used as a mint,
-				// tracking the amounts of Fluid tokens that the account has. is set to true
-				// to indicate mutability
-				solanaAccountFluidMint = solana.NewAccountMeta(fluidMintPubkey, true, false)
-
-				// solanaAccountPDA is used as an authority to sign off on minting
-				// by the payout function
-				solanaAccountPDA = solana.NewAccountMeta(PDAPubkey, false, false)
-
-				// solanaAccountObligation to use to track the amount of Solend
-				// obligations that Fluidity owns to pass the account to do a calculation for
-				// the prize pool
-				solanaAccountObligation = solana.NewAccountMeta(
-					obligationPubkey,
-					false,
-					false,
-				)
-
-				// solanaAccountReserve to use to track the exchange rate of obligation
-				// collateral
-				solanaAccountReserve = solana.NewAccountMeta(
-					reservePubkey,
-					false,
-					false,
-				)
-
-				// accounts used to indicate the payout recipients from the reward function.
-				// both accounts are mutable to support sending to the token accounts
-
-				solanaAccountA = solana.NewAccountMeta(aPubkey, true, false)
-				solanaAccountB = solana.NewAccountMeta(bPubkey, true, false)
-
-				// solanaAccountPayer, with true set to sign amounts paid out to the
-				// recipients, strictly the fluidity authority
-				solanaAccountPayer = solana.NewAccountMeta(
-					payer.PublicKey(),
-					true,
-					true,
-				)
-			)
-
-			accountMetas := solana.AccountMetaSlice{
-				solanaAccountMetaSpl,
-				solanaAccountFluidMint,
-				solanaAccountPDA,
-				solanaAccountObligation,
-				solanaAccountReserve,
-				solanaAccountA,
-				solanaAccountB,
-				solanaAccountPayer,
-			}
-
-			payoutInstruction := fluidity.InstructionPayout{
-				fluidity.VariantPayout,
-				winningAmount,
-				TokenName,
-				BumpSeed,
-			}
-
-			// if flag is set, print debug information and don't actually send payout
-			if debugFakePayouts {
-				log.Debug(func(k *log.Log) {
-					k.Format(
-						`Payout would have been sent with account metas:
-{SPL program: %v, fluid mint: %v, PDA: %v, obligation: %v, reserve: %v, account A: %v, account B: %v, payer: %v}
-and instruction data %+v`,
-						splPubkey,
-						fluidMintPubkey,
-						PDAPubkey,
-						obligationPubkey,
-						reservePubkey,
-						aPubkey,
-						bPubkey,
-						payer.PublicKey(),
-						payoutInstruction,
-					)
-				})
-				return
-			}
-
-			payoutInstructionBytes, err := borsh.Serialize(payoutInstruction)
-
-			if err != nil {
-				log.Fatal(func(k *log.Log) {
-					k.Message = "Failed to serialize payout instruction data!"
-					k.Payload = err
-				})
-			}
-
-			instruction := solana.NewInstruction(
-				fluidityPubkey,
-				accountMetas,
-				payoutInstructionBytes,
-			)
-
-			instructions := []solana.Instruction{instruction}
-
-			transaction, err := solana.NewTransaction(instructions, recentBlockHash)
-
-			if err != nil {
-				log.Fatal(func(k *log.Log) {
-					k.Message = "Failed to construct payout transaction!"
-					k.Payload = err
-				})
-			}
-
-			_, err = transaction.Sign(func(key solana.PublicKey) *solana.PrivateKey {
-
-				if payer.PublicKey().Equals(key) {
-					return &payer.PrivateKey
-				}
-
-				return nil
-			})
-
-			if err != nil {
-				log.Fatal(func(k *log.Log) {
-					k.Message = "Failed to sign transaction!"
-					k.Payload = err
-				})
-			}
-
-			sig, err := solanaClient.SendTransaction(context.Background(), transaction)
-
-			if err != nil {
-				log.Fatal(func(k *log.Log) {
-					k.Message = "Failed to send payout transaction!"
-					k.Payload = err
-				})
-			}
-
-			log.App(func(k *log.Log) {
-				k.Format("Sent payout transaction with signature %v", sig)
-			})
+			queue.SendMessage(topicWinnerQueue, winnerAnnouncement)
 		}
 	})
 }
