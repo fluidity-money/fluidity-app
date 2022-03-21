54c14a1bddfab4fd8a381287b4c30446f0103d31
glo12345 <glorison@fluidity.money>
2022-03-21 19:08:11 +1100

    Refactored microservice to enumerate VHosts first before querying queues

--- cmd/microservice-common-rabbitmq-backlog-checker/lib/types.go
+++ cmd/microservice-common-rabbitmq-backlog-checker/lib/types.go
@@ -10,5 +10,26 @@ type (
 		MessagesUnacked uint64 `json:"messages_unacknowledged"`
 	}
 
+	Vhost struct {
+		Cluster                       interface{} `json:"cluster_state"`
+		Description                   string      `json:"description"`
+		Messages                      uint64      `json:"messages"`
+		MessagesDetails               interface{} `json:"messages_details"`
+		MessagesReady                 uint64      `json:"messages_ready"`
+		MessagesReadyDetails          interface{} `json:"messages_ready_details"`
+		MessagesUnacked               uint64      `json:"messages_unacknowledged"`
+		MessagesUnacknowledgedDetails interface{} `json:"messages_unacknowledged_details"`
+		Metadata                      interface{} `json:"metadata"`
+		Name                          string      `json:"name"`
+		RecvOct                       uint64      `json:"recv_oct"`
+		RecvOctDetails                interface{} `json:"recv_oct_details"`
+		SendOct                       uint64      `json:"send_oct"`
+		SendOctDetails                interface{} `json:"send_oct_details"`
+		Tags                          []string    `json:"tags"`
+		Tracing                       bool        `json:"tracing"`
+	}
+
 	RmqQueuesResponse []RmqQueue
+
+	VhostsResponse []Vhost
 )
