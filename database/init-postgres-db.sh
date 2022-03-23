6a7e2cdb42233a07b937b02cccaeb9bdf92d6eec
Alex <>
2022-03-23 17:04:49 +1030

    Change scripts for db-mate setup and accidentally add some frontend code

--- database/init-postgres-db.sh
+++ database/init-postgres-db.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -e
-
 REPO="/usr/src/fluidity-migrations-postgres"
 
-dbmate -u "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@:5432/${POSTGRES_DB}?sslmode=disable" -d "$REPO/build/postgres" up
-
+dbmate \
+	-u "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@:5432/${POSTGRES_DB}?sslmode=disable" \
+	-d "$REPO/build/postgres" \
+	up
--- database/init-timescale-db.sh
+++ database/init-timescale-db.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -e
-
 REPO="/usr/src/fluidity-migrations-timescale"
 
-dbmate -u "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@:5432/${POSTGRES_DB}?sslmode=disable" -d "$REPO/build/timescale" up
-
+dbmate \
+	-u "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@:5432/${POSTGRES_DB}?sslmode=disable" \
+	-d "$REPO/build/timescale" \
+	up
