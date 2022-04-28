c00220d712fd083d38c82234a79106df069013c7
Connor <mussonconnor@gmail.com>
2022-04-28 14:03:55 +0200

    updated popup to show the browser wallet chain and added other test networks

--- web/ethereum.fluidity.money/src/components/Toolbar/Toolbar.tsx
+++ web/ethereum.fluidity.money/src/components/Toolbar/Toolbar.tsx
@@ -2,62 +2,27 @@ import React from "react";
 import { useEffect } from "react";
 import ChainId, { chainIdFromEnv, toChainId } from "util/chainId";
 
-interface AddEthereumChainParameter {
-  chainId: string; // A 0x-prefixed hexadecimal string
-  chainName: string;
-  nativeCurrency: {
-    name: string;
-    symbol: string; // 2-6 characters long
-    decimals: 18;
-  };
-  rpcUrls: string[];
-  blockExplorerUrls?: string[];
-  iconUrls?: string[]; // Currently ignored.
-}
-
-const kovan: AddEthereumChainParameter = {
-  chainId: `0x${Number(45).toString(16)}`,
-  chainName: "Ethereum Testnet Kovan",
-  nativeCurrency: {
-    name: "Kovan",
-    symbol: "KOV",
-    decimals: 18,
-  },
-  rpcUrls: [
-    "https://kovan.poa.network",
-    "http://kovan.poa.network:8545",
-    "https://kovan.infura.io/v3/d1ac3dc1af2649908a69582ffa1a424d",
-    "wss://kovan.infura.io/ws/v3/d1ac3dc1af2649908a69582ffa1a424d",
-    "ws://kovan.poa.network:8546",
-  ],
-  blockExplorerUrls: ["https://kovan.etherscan.io"],
-};
-
-// switchEthereumChain for Ropsten and Mainnet
+// switchEthereumChain for Ropsten/Kovan/Mainnet
 const changeNetwork = async () => {
   try {
     if (!(window as any).ethereum) throw new Error("No crypto wallet found");
     await (window as any).ethereum.request({
       method: "wallet_switchEthereumChain",
-      params: [{ chainId: `0x${process.env.REACT_APP_CHAIN_ID}` }],
+      params: [
+        {
+          chainId: chainIdFromEnv() === 42 ? `0x2a` : `0x${chainIdFromEnv()}`,
+        },
+      ],
     });
-  } catch (err: any) {
-    if (err.code === 4902) {
-      try {
-        await (window as any).ethereum.request({
-          method: "wallet_addEthereumChain",
-          params: [kovan],
-        });
-      } catch (err) {
-        throw err;
-      }
-    }
+  } catch (err) {
+    throw err;
   }
 };
 
 const Toolbar = ({ children }: { children: JSX.Element }) => {
   const [desiredNetwork, setDesiredNetwork] = React.useState(true);
   const [chainId, setChainId] = React.useState<ChainId>(0);
+  const [browserChainId, setBrowserChainId] = React.useState<ChainId | null>();
 
   const handleNetworkSwitch = async () => {
     await changeNetwork();
@@ -70,6 +35,7 @@ const Toolbar = ({ children }: { children: JSX.Element }) => {
         method: "eth_chainId",
       });
       const browserChain = toChainId(chain);
+      // setBrowserChainId(browserChain);
       const envChain = chainIdFromEnv();
 
       browserChain && setChainId(envChain);
@@ -84,6 +50,7 @@ const Toolbar = ({ children }: { children: JSX.Element }) => {
         const envChain = chainIdFromEnv();
 
         browserChain && setChainId(envChain);
+        setBrowserChainId(browserChain);
         setDesiredNetwork(browserChain === envChain);
       });
     }
@@ -109,8 +76,12 @@ const Toolbar = ({ children }: { children: JSX.Element }) => {
               ? `Ethereum Kovan`
               : `Ethereum`}
             )
-            {` doesn't match to network selected in wallet (network with id:
-          ${chainId}). Learn how to`}
+            {` doesn't match to network selected in wallet
+            ${
+              browserChainId === null
+                ? ""
+                : ` (network with id:${browserChainId})`
+            }. Learn how to`}
             <a
               className="learn-change-network-link"
               href="https://metamask.zendesk.com/hc/en-us/articles/4404424659995"
