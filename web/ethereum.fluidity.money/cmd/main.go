60d8104eb0f11381f143795d0acf90324866c4fb
harlexkhal <harlexibeh01@gmail.com>
2022-05-05 14:35:57 +0100

    add winning-chances api interface to web endpoint

--- web/ethereum.fluidity.money/cmd/main.go
+++ web/ethereum.fluidity.money/cmd/main.go
@@ -22,6 +22,8 @@ func main() {
 
 	web.JsonEndpoint("/my-history", api_fluidity_money.HandleMyHistory)
 
+	web.Endpoint("/winning-chances", api_fluidity_money.HandleWinningChances)
+
 	updateNotificationsHandlerEthereum := api_fluidity_money.HandleUpdateNotifications(
 		updateMessagesEthereum,
 	)
