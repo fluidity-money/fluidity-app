87b4bdd1b0823fd222851875e75950d332dabc4d
harlexkhal <harlexibeh01@gmail.com>
2022-05-07 01:46:29 +0100

    handle JSON serializer and GraphQL errors

--- web/ethereum.fluidity.money/lib-backend/winning-chances.go
+++ web/ethereum.fluidity.money/lib-backend/winning-chances.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/big"
 	"net/http"
+	"errors"
 
 	"github.com/graphql-go/graphql"
 	"github.com/fluidity-money/fluidity-app/lib/queues/worker"
@@ -35,10 +36,6 @@ var queryType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "Query",
 		Fields: graphql.Fields{
-			/* fetch winning-chance using query
-			*  Example API query request
-			*  http://localhost:8080/winning-chances?query={fetch(gasFee:3,atx:36000000,bpyStakedUsd:150,sizeOfThePool:10,underlyingTokenDecimalsRat:6,averageTransfersInBlock:13,secondsSinceLastBlock:15,tokenName:"USDT"){n,payouts}}
-			*/
 			"fetch": &graphql.Field{
 				Type:        resultType,
 				Description: "Get winnig-chances",
@@ -70,39 +67,48 @@ var queryType = graphql.NewObject(
 				},
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 
-					var (
-						gasFee                           = new(big.Rat).SetFloat64(p.Args["gasFee"].(float64))
-						atx                              = new(big.Rat).SetFloat64(p.Args["atx"].(float64))
-						bpyStakedUsd                     = new(big.Rat).SetFloat64(p.Args["bpyStakedUsd"].(float64))
-						sizeOfThePool                    = new(big.Rat).SetUint64(uint64(p.Args["sizeOfThePool"].(int)))
-						underlyingTokenDecimalsRat       = new(big.Rat).SetUint64(uint64(p.Args["underlyingTokenDecimalsRat"].(int)))
-						averageTransfersInBlock          = p.Args["averageTransfersInBlock"].(int)
-						secondsSinceLastBlock            = p.Args["secondsSinceLastBlock"].(int)
-						tokenName                        = p.Args["tokenName"].(string)
-					)
-
-					emission := worker.NewEthereumEmission()
-	                emission.Network = "ethereum"
-	                emission.TokenDetails = token_details.New(tokenName, p.Args["underlyingTokenDecimalsRat"].(int))
-
-					randomN, randomPayouts := probability.WinningChances(
-						gasFee,
-						atx,
-						bpyStakedUsd,
-						sizeOfThePool,
-						underlyingTokenDecimalsRat,
-						averageTransfersInBlock,
-						uint64(secondsSinceLastBlock),
-						emission,
-					)
-
 					var output Result
-					output.N = randomN;
-					for _, value := range randomPayouts {
-						output.Payouts = append(output.Payouts, value.Int64())
-					}
 
-					return output, nil
+					requestOk := p.Args["gasFee"] != nil && p.Args["atx"] != nil &&
+					p.Args["bpyStakedUsd"] != nil && p.Args["sizeOfThePool"] != nil &&
+					p.Args["underlyingTokenDecimalsRat"] != nil && p.Args["averageTransfersInBlock"] != nil &&
+					p.Args["secondsSinceLastBlock"] != nil && p.Args["tokenName"] != nil
+					
+					if requestOk {
+						var (
+							gasFee                           = new(big.Rat).SetFloat64(p.Args["gasFee"].(float64))
+							atx                              = new(big.Rat).SetFloat64(p.Args["atx"].(float64))
+							bpyStakedUsd                     = new(big.Rat).SetFloat64(p.Args["bpyStakedUsd"].(float64))
+							sizeOfThePool                    = new(big.Rat).SetUint64(uint64(p.Args["sizeOfThePool"].(int)))
+							underlyingTokenDecimalsRat       = new(big.Rat).SetUint64(uint64(p.Args["underlyingTokenDecimalsRat"].(int)))
+							averageTransfersInBlock          = p.Args["averageTransfersInBlock"].(int)
+							secondsSinceLastBlock            = p.Args["secondsSinceLastBlock"].(int)
+							tokenName                        = p.Args["tokenName"].(string)
+						)
+						
+						emission := worker.NewEthereumEmission()
+						emission.Network = "ethereum"
+						emission.TokenDetails = token_details.New(tokenName, p.Args["underlyingTokenDecimalsRat"].(int))
+	
+						randomN, randomPayouts := probability.WinningChances(
+							gasFee,
+							atx,
+							bpyStakedUsd,
+							sizeOfThePool,
+							underlyingTokenDecimalsRat,
+							averageTransfersInBlock,
+							uint64(secondsSinceLastBlock),
+							emission,
+						)
+
+						output.N = randomN;
+						for _, value := range randomPayouts {
+							output.Payouts = append(output.Payouts, value.Int64())
+						}
+	
+						return output, nil
+					}
+					return output, errors.New("Bad request, missing payload value")
 				},
 			},
 		},
@@ -119,13 +125,27 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 		Schema:        schema,
 		RequestString: query,
 	})
-	if len(result.Errors) > 0 {
-		fmt.Printf("errors: %v", result.Errors)
-	}
+
 	return result
 }
 
 func HandleWinningChances(w http.ResponseWriter, r *http.Request) {
 	result := executeQuery(r.URL.Query().Get("query"), schema)
-	json.NewEncoder(w).Encode(result)
+
+	//Error check from GraphQL request
+	if len(result.Errors) > 0 {
+		fmt.Errorf(
+			"Request error %v",
+			result.Errors,
+		)
+	}
+
+	//Error check from results received
+	err := json.NewEncoder(w).Encode(result)
+	if err != nil {
+		fmt.Errorf(
+			"Failed to encode result data %v",
+			err,
+		)
+	}
 }
