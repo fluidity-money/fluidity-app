2643e892f9ad5e17e77989dd2bab5621250681ec
glo12345 <glorison@fluidity.money>
2022-05-10 16:07:54 +1000

    Removed testing edits to solana contract

--- contracts/solana/src/lib.rs
+++ contracts/solana/src/lib.rs
@@ -7,9 +7,9 @@ use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubke
 
 mod error;
 pub mod instruction;
-pub mod math;
+mod math;
 pub mod processor;
-pub mod state;
+mod state;
 
 // declare the pubkey of the program
 #[cfg(all(target_arch = "bpf", not(feature = "exclude_entrypoint")))]
