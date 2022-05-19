6cb8cb8654105a38f42399ac1ca0e7c6a8e98cac
glo12345 <glorison@fluidity.money>
2022-05-19 16:44:53 +1000

    Changed Tribeca authority to Fluidity Authority

--- contracts/tribeca-govern-contracts/programs/trf-data-store/src/constants.rs
+++ contracts/tribeca-govern-contracts/programs/trf-data-store/src/constants.rs
@@ -2,5 +2,4 @@
 
 // Authority is the only account allowed to
 // initialize CalculateNArgs
-//pub const AUTHORITY: &str = "sohTpNitFg3WZeEcbrMunnwoZJWP4t8yisPB5o3DGD5";
-pub const AUTHORITY: &str = "J6e6KqD71AK4kkfz4A8EncxyZAFvoKMsimE4R8v4tmYj";
+pub const AUTHORITY: &str = "sohTpNitFg3WZeEcbrMunnwoZJWP4t8yisPB5o3DGD5";
