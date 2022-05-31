95de78212bd44df066b1054834f07cfb6ac3b49e
e <frumgerth@gmail.com>
2022-05-31 11:35:15 +0930

    use token details in db code

--- lib/types/worker/worker.go
+++ lib/types/worker/worker.go
@@ -94,6 +94,13 @@ type (
 		TokenDetails    token_details.TokenDetails `json:"token_details"`
 	}
 
+	EthereumSpooledRewards struct {
+		Winner     ethereum.Address `json:"winner"`
+		WinAmount  *misc.BigInt     `json:"amount"`
+		FirstBlock *misc.BigInt     `json:"first_block"`
+		LastBlock  *misc.BigInt     `json:"last_block"`
+	}
+
 	EthereumBlockLog struct {
 		BlockHash    ethereum.Hash          `json:"block_hash"`
 		BlockBaseFee misc.BigInt            `json:"block_base_fee"`
