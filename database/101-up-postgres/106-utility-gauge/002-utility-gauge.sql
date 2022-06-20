114432f40383419e7d12b295b51fcce41a4c614d
glo12345 <glorison@fluidity.money>
2022-06-20 14:28:26 +1000

    refactor: added tabs instead of spaces in db folder

--- database/101-up-postgres/106-utility-gauge/002-utility-gauge.sql
+++ database/101-up-postgres/106-utility-gauge/002-utility-gauge.sql
@@ -1,19 +1,16 @@
 -- migrate:up
 
--- reusable datatypes across the postgres/timescale relations
--- since the backend's the same.
-
 CREATE TABLE utility_gauges (
-    -- network args belongs to
+	-- network args belongs to
 	chain network_blockchain NOT NULL,
-    network VARCHAR NOT NULL,
+	network VARCHAR NOT NULL,
 	gauge VARCHAR NOT NULL,
 
 	epoch BIGINT NOT NULL,
 	disabled BOOLEAN NOT NULL,
 	total_power uint256 NOT NULL,
 
-    primary key(gauge)
+	primary key(gauge)
 );
 
 -- migrate:down
