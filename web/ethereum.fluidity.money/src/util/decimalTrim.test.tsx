8bf93e2099ad66b90a187309c9abec1dd169c73e
harlexkhal <harlexibeh01@gmail.com>
2022-07-15 07:49:49 +0100

    update decimalTrim test for eth util

--- web/ethereum.fluidity.money/src/util/decimalTrim.test.tsx
+++ web/ethereum.fluidity.money/src/util/decimalTrim.test.tsx
@@ -2,15 +2,15 @@ import { decimalTrim } from "./decimalTrim";
 
 describe("decimalTrim", () => {
   it("0.22235233", () => {
-    expect(decimalTrim("0.22235233", 3)).toBe("0.22");
+    expect(decimalTrim("0.22235233", 3)).toBe("0.222");
   })
   it("218814.22235233", () => {
-    expect(decimalTrim("218814.69235233", 1)).toBe("218814");
+    expect(decimalTrim("218814.69235233", 1)).toBe("218814.6");
   })
   it("218814.22235233", () => {
     expect(decimalTrim("218814.69235233", 0)).toBe("218814.69235233");
   })
   it("3.141592653589793238", () => {
-    expect(decimalTrim("3.141592653589793238", 6)).toBe("3.14159");
+    expect(decimalTrim("3.141592653589793238", 6)).toBe("3.141592");
   })
 })
