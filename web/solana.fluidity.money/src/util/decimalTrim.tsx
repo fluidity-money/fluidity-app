cb722592a3d8f62f7af6093f83d368b199105826
harlexkhal <harlexibeh01@gmail.com>
2022-07-15 07:46:46 +0100

    add some more sol fe test

--- web/solana.fluidity.money/src/util/decimalTrim.tsx
+++ web/solana.fluidity.money/src/util/decimalTrim.tsx
@@ -1,5 +1,10 @@
 //trim a string to <limit> decimal places
 export const decimalTrim = (amount: string, limit: number) => {
+
+    if (limit <= 0) {
+        return amount;
+    }
+
     const trimIndex = amount.indexOf('.');
     const trim = trimIndex > -1 ? amount.slice(0, trimIndex + limit + 1) : amount;
     return trim;
---
+++ web/solana.fluidity.money/src/util/numbers.test.tsx
@@ -0,0 +1,40 @@
+import * as numbers from "./numbers";
+
+describe("amountToDecimalString", () => {
+  it("Amount: 299, Decimal: 2", () => {
+    expect(numbers.amountToDecimalString("299", 2)).toBe("2.99");
+  })
+  it("Amount: 5000, Decimal: 5", () => {
+    expect(numbers.amountToDecimalString("5000", 5)).toBe("0.05");
+  })
+  it("Amount: 15000, Decimal: 3", () => {
+    expect(numbers.amountToDecimalString("5000", 3)).toBe("5");
+  })
+  it("Amount: 70, Decimal: 0", () => {
+    expect(numbers.amountToDecimalString("70", 0)).toBe(".7");
+  })
+  it("Amount: 3141592653589793238, Decimal: 10", () => {
+    expect(numbers.amountToDecimalString("3141592653589793238", 18)).toBe("3.141592653589793238");
+  })
+  it("Amount: 3141592653589793238, Decimal: 10", () => {
+    expect(numbers.amountToDecimalString("3141592653589793238", 10)).toBe("314159265.3589793238");
+  })
+})
+
+describe("clearTrailingZeros", () => {
+    it("2000.000", () => {
+      expect(numbers.clearTrailingZeros("2000.000")).toBe("2000");
+    })
+})
+
+/*describe("decimalToTokenAmount", () => {
+    it("29 + 29 + 92]", () => {
+      expect(currencyParser([29,29,92])).toBe("$150.00");
+    })
+})
+
+describe("trailingDecimalZeros", () => {
+    it("29 + 29 + 92]", () => {
+      expect(currencyParser([29,29,92])).toBe("$150.00");
+    })
+})*/
