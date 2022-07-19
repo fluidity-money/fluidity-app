ea09bd0b9eb62b076e0f3d49771cf007ae80f552
user <alex@iometelabs.io>
2022-07-19 15:17:15 +0200

    Remove the unavailable application servers

--- scripts/docker-compose-ethereum-hardhat.sh
+++ scripts/docker-compose-ethereum-hardhat.sh
@@ -100,5 +100,4 @@ docker-compose \
 	-f "$automation_dir/docker-compose.ethereum-connectors.yml" \
 	-f "$automation_dir/docker-compose.ethereum-hardhat.yml" \
 	-f "$automation_dir/docker-compose.ethereum-worker-hardhat.yml" \
-	-f "$automation_dir/docker-compose.ethereum-application-server.yml" \
 	$@
