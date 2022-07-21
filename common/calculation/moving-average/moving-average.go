aa7e06fc49d3593b72dbb140e39c9f3815298420
user <alex@iometelabs.io>
2022-07-21 23:39:45 +0200

    Add extra copyright to a few Go files without it

--- common/calculation/moving-average/moving-average.go
+++ common/calculation/moving-average/moving-average.go
@@ -1,3 +1,7 @@
+// Copyright 2022 Fluidity Money. All rights reserved. Use of this
+// source code is governed by a GPL-style license that can be found in the
+// LICENSE.md file.
+
 package moving_average
 
 // use redis with the state package in fluidity-app/lib to store a ring
