f46a3f823d3087179266f43b55344aba6bf51a2e
user <alex@iometelabs.io>
2022-07-21 23:32:46 +0200

    Replace LICENSE with LICENSE.md

--- lib/web/util_test.go
+++ lib/web/util_test.go
@@ -1,6 +1,6 @@
 // Copyright 2022 Fluidity Money. All rights reserved. Use of this
 // source code is governed by a GPL-style license that can be found in the
-// LICENSE file.
+// LICENSE.md file.
 
 package web
 
