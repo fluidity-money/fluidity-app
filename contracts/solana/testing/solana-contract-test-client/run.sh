ad3e166d705ef3e9ef871991f3b999b395df89ac
glo12345 <glorison@fluidity.money>
2022-07-22 16:46:40 +0800

    feat: added unwrap test

--- contracts/solana/testing/solana-contract-test-client/run.sh
+++ contracts/solana/testing/solana-contract-test-client/run.sh
@@ -1,8 +1,8 @@
-FLU_FLUID_MINT=w7XghtW67KZT3y6qGLz5c8KLZpgaxbSDyne8sa7CSUZ \
-  FLU_PROGRAM_ID=Hcvpm3FwSX8QuR9Cmv1agHotdGQeXPg29hKaVSYkD6Qy \
+FLU_FLUID_MINT=2hFRoLHprEn6wEiJm2TiGbddNEWx91JaP6KYjyLzEVJF \
+  FLU_PROGRAM_ID=B4cZZouMYHUApypoqSKYQhDPdUSzkvMiRhVVzf6TopU2 \
   FLU_TOKEN_MINT=zVzi5VAf4qMEwzv7NXECVx5v2pQ7xnqVVjCXZwS9XzA \
   FLU_TOKEN_NAME=USDC \
   SOLANA_ACCOUNTS=accounts.json \
   SOLANA_NODE_ADDRESS=https://api.devnet.solana.com \
   SLND_COMMON=devnet.json \
-  cargo test -- --nocapture
+  cargo test
--- contracts/solana/testing/solana-contract-test-client/src/lib.rs
+++ contracts/solana/testing/solana-contract-test-client/src/lib.rs
@@ -17,11 +17,13 @@ use solana_sdk::{
 };
 
 use crate::accounts::{DevnetAccount, FluidityData};
-use crate::instructions::FluidContract;
+use crate::instructions::*;
 
 pub mod accounts;
 pub mod instructions;
 pub mod rpc;
+
+static INIT: Once = Once::new();
 
 #[derive(Debug)]
 pub enum TestingCommands {
@@ -38,18 +40,16 @@ pub enum TestingCommands {
     DumpKeys,
 }
 
-static INIT: Once = Once::new();
-
 pub fn send_instruction(
     client: &RpcClient,
-    instructions: Vec<Instruction>,
+    instructions: &Vec<Instruction>,
     payer: Pubkey,
     signers: Vec<&Keypair>,
 ) -> Result<Vec<String>, String> {
     // get recent blockhash
     let recent_blockhash = client.get_latest_blockhash().unwrap();
 
-    let mut txn = Transaction::new_with_payer(&instructions, Some(&payer));
+    let mut txn = Transaction::new_with_payer(instructions, Some(&payer));
     txn.sign(&signers, recent_blockhash);
 
     send_txn(&client, txn)
@@ -68,7 +68,7 @@ pub fn setup_accs(client: &RpcClient, config: &ConfigOptions, solend_accounts: &
     );
 
     // init obligation
-    let init_obligation_accs = instructions::InitObligationInstructionAccountMetas {
+    let init_obligation_accs = InitObligationInstructionAccountMetas {
         payer_pubkey: AccountMeta::new(payer.get_public_key(), true),
         collateral_mint: AccountMeta::new(accounts::collateral_mint(&config, &solend_accounts), true),
         solend_program: AccountMeta::new_readonly(accounts::solend_program(&solend_accounts), false),
@@ -96,13 +96,13 @@ pub fn setup_accs(client: &RpcClient, config: &ConfigOptions, solend_accounts: &
 
     dbg!(send_instruction(
         &client,
-        init_obligation_inst,
+        &init_obligation_inst,
         payer.get_public_key(),
         vec![&payer.keypair.as_ref().unwrap()]
     ));
 
     // Init data
-    let init_data_accs = instructions::InitDataInstructionAccountMetas {
+    let init_data_accs = InitDataInstructionAccountMetas {
         system_program: AccountMeta::new_readonly(system_program::ID, false),
         payer_pubkey: AccountMeta::new(payer.get_public_key(), false),
         data_pubkey: AccountMeta::new(accounts::data_account(&config), false),
@@ -111,7 +111,7 @@ pub fn setup_accs(client: &RpcClient, config: &ConfigOptions, solend_accounts: &
         pda_pubkey: AccountMeta::new(accounts::pda(&config), false),
     };
 
-    let init_data_args = instructions::InitDataInstructionArgs {
+    let init_data_args = InitDataInstructionArgs {
         token_name: config.token_name.clone(),
         bump_seed,
         space: std::mem::size_of::<FluidityData>() as u64,
@@ -124,7 +124,7 @@ pub fn setup_accs(client: &RpcClient, config: &ConfigOptions, solend_accounts: &
 
     dbg!(send_instruction(
         &client,
-        init_data_inst,
+        &init_data_inst,
         payer.get_public_key(),
         vec![&payer.keypair.as_ref().unwrap()]
     ));
@@ -134,7 +134,7 @@ pub fn setup_accs(client: &RpcClient, config: &ConfigOptions, solend_accounts: &
 
 pub fn setup_user(client: &RpcClient, user: &DevnetAccount, fluid_contract: &FluidContract) {
         // Init tvl data
-        let init_tvl_data_args = instructions::InitTvlDataInstructionArgs {
+        let init_tvl_data_args = InitTvlDataInstructionArgs {
             payer_pubkey: user.get_public_key(),
             tvl_data_pubkey: user.tvl_data_account.unwrap(),
             space: std::mem::size_of::<u64>() as u64,
@@ -147,7 +147,7 @@ pub fn setup_user(client: &RpcClient, user: &DevnetAccount, fluid_contract: &Flu
 
         dbg!(send_instruction(
             &client,
-            init_tvl_data_inst,
+            &init_tvl_data_inst,
             user.get_public_key(),
             vec![&user.keypair.as_ref().unwrap()]
         ));
@@ -157,26 +157,9 @@ pub fn setup_user(client: &RpcClient, user: &DevnetAccount, fluid_contract: &Flu
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str::FromStr;
-
-    use accounts::{ConfigOptions, SolendAccounts};
-    use borsh::BorshDeserialize;
-    use rpc::send_txn;
-    use solana_client::rpc_client::RpcClient;
-    use solana_sdk::{
-        account::Account,
-        instruction::{AccountMeta, Instruction},
-        pubkey::Pubkey,
-        signature::Keypair,
-        signer::Signer,
-        system_instruction, system_program, sysvar,
-        transaction::Transaction,
-    };
-
-    use crate::accounts::FluidityData;
-
+    
     #[test]
-    fn test_wrap() {
+    fn test_wrap_and_unwrap() {
         let mut config = ConfigOptions::new();
 
         let solend_accounts = SolendAccounts::new(&config.solend_account_file);
@@ -185,7 +168,7 @@ mod tests {
 
         let client = RpcClient::new(config.solana_node_address.clone());
 
-        let fluid_contract = instructions::FluidContract::new(&config.program_id);
+        let fluid_contract = FluidContract::new(&config.program_id);
 
         let mint_account_seed = format!("FLU:{}_OBLIGATION", config.token_name);
         let (_, bump_seed) = Pubkey::find_program_address(
@@ -196,19 +179,19 @@ mod tests {
         setup_accs(&client, &config, &solend_accounts, &fluid_contract);
 
         // Wrap
-        let wrapping_user = &config.accounts[1];
+        let wrapping_user = &config.accounts[5];
         
         setup_user(&client, &wrapping_user, &fluid_contract);
 
-        let starting_fluid_token = client
+        let starting_fluid_token_amount = client
             .get_token_account_balance(&wrapping_user.fluid_token_account.unwrap())
             .unwrap();
 
-        let starting_base_token = client
+        let starting_base_token_amount = client
             .get_token_account_balance(&wrapping_user.base_token_account.unwrap())
             .unwrap();
 
-        let wrap_accs = instructions::WrapInstructionAccountMetas {
+        let wrap_accs = WrapInstructionAccountMetas {
             fluidity_data_account: AccountMeta::new(accounts::data_account(&config), false),
             token_program: AccountMeta::new_readonly(spl_token::ID, false),
             base_token_id: AccountMeta::new(accounts::base_token(&config, &solend_accounts), false),
@@ -257,42 +240,120 @@ mod tests {
 
         let wrap_amt = 5;
 
-        let wrap_args = instructions::WrapInstructionArgs {
+        let wrap_args = WrapInstructionArgs {
             amount: wrap_amt,
             token_name: config.token_name.clone(),
             bump_seed,
         };
 
         let wrap_inst = fluid_contract.wrap(wrap_accs, wrap_args);
-        
-        println!("hello");
+
+        let unwrap_accs = UnwrapInstructionAccountMetas {
+            fluidity_data_account: AccountMeta::new(accounts::data_account(&config), false),
+            token_program: AccountMeta::new_readonly(spl_token::ID, false),
+            base_token_id: AccountMeta::new(accounts::base_token(&config, &solend_accounts), false),
+            fluid_token_id: AccountMeta::new(accounts::fluid_token(&config), false),
+            pda_pubkey: AccountMeta::new(accounts::pda(&config), false),
+            payer_pubkey: AccountMeta::new(wrapping_user.get_public_key(), true),
+            base_token_account: AccountMeta::new(wrapping_user.base_token_account.unwrap(), false),
+            fluid_token_account: AccountMeta::new(
+                wrapping_user.fluid_token_account.unwrap(),
+                false,
+            ),
+            solend_program: AccountMeta::new_readonly(
+                accounts::solend_program(&solend_accounts),
+                false,
+            ),
+            collateral_account: AccountMeta::new(accounts::collateral_account(&config), false),
+            reserve: AccountMeta::new(accounts::reserve(&config, &solend_accounts), false),
+            reserve_liquidity_supply: AccountMeta::new(
+                accounts::reserve_liquidity(&config, &solend_accounts),
+                false,
+            ),
+            collateral_mint: AccountMeta::new(
+                accounts::collateral_mint(&config, &solend_accounts),
+                false,
+            ),
+            lending_market: AccountMeta::new(accounts::lending_market(&solend_accounts), false),
+            market_authority: AccountMeta::new_readonly(
+                accounts::market_authority(&solend_accounts),
+                false,
+            ),
+            collateral_supply: AccountMeta::new(
+                accounts::collateral_supply(&config, &solend_accounts),
+                false,
+            ),
+            obligation_account: AccountMeta::new(accounts::obligation_account(&config), false),
+            pyth_account: AccountMeta::new_readonly(
+                accounts::pyth_account(&config, &solend_accounts),
+                false,
+            ),
+            switchboard_account: AccountMeta::new_readonly(
+                accounts::switchboard_account(&config, &solend_accounts),
+                false,
+            ),
+            clock_program: AccountMeta::new_readonly(sysvar::clock::ID, false),
+        };
+
+        // Unwrap the same amount as wrapped
+        let unwrap_args = UnwrapInstructionArgs {
+            amount: wrap_amt,
+            token_name: config.token_name.clone(),
+            bump_seed,
+        };
+
+        let unwrap_inst = fluid_contract.unwrap(unwrap_accs, unwrap_args);
 
         assert!(dbg!(send_instruction(
             &client,
-            wrap_inst,
+            &wrap_inst,
             wrapping_user.get_public_key(),
             vec![&wrapping_user.keypair.as_ref().unwrap()]
         ))
         .is_ok());
-        
-        println!("hello2");
 
-        let new_fluid_token = client
+        let wrapped_fluid_token_amount = client
             .get_token_account_balance(&wrapping_user.fluid_token_account.unwrap())
             .unwrap();
 
-        let new_base_token = client
+        let wrapped_base_token_amount = client
             .get_token_account_balance(&wrapping_user.base_token_account.unwrap())
             .unwrap();
 
         assert_eq!(
-            starting_fluid_token.amount.parse::<u64>().unwrap() + wrap_amt,
-            new_fluid_token.amount.parse::<u64>().unwrap()
+            starting_fluid_token_amount.amount.parse::<u64>().unwrap() + wrap_amt,
+            wrapped_fluid_token_amount.amount.parse::<u64>().unwrap()
         );
 
         assert_eq!(
-            starting_base_token.amount.parse::<u64>().unwrap() - wrap_amt,
-            new_base_token.amount.parse::<u64>().unwrap()
+            starting_base_token_amount.amount.parse::<u64>().unwrap() - wrap_amt,
+            wrapped_base_token_amount.amount.parse::<u64>().unwrap()
+        );
+
+        assert!(dbg!(send_instruction(
+            &client,
+            &unwrap_inst,
+            wrapping_user.get_public_key(),
+            vec![&wrapping_user.keypair.as_ref().unwrap()]
+        ))
+        .is_ok());
+
+        let unwrapped_fluid_token_amount = client
+            .get_token_account_balance(&wrapping_user.fluid_token_account.unwrap())
+            .unwrap();
+
+        let unwrapped_base_token_amount = client
+            .get_token_account_balance(&wrapping_user.base_token_account.unwrap())
+            .unwrap();
+
+        assert_eq!(
+            starting_fluid_token_amount.amount.parse::<u64>().unwrap() + wrap_amt,
+            unwrapped_fluid_token_amount.amount.parse::<u64>().unwrap()
+        );
+
+        assert_eq!(
+            starting_base_token_amount.amount.parse::<u64>().unwrap() - wrap_amt,
+            unwrapped_base_token_amount.amount.parse::<u64>().unwrap()
         );
     }
 }
