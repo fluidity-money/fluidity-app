246c3a689370d1d937ea8b37e57a299a26c9c342
Connor <mussonconnor@gmail.com>
2022-07-27 10:17:33 +0300

    added folder and file structure

--- web/fluidity.money/src/App.tsx
+++ web/fluidity.money/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App">fluidity.money</div>;
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        {/* switch 
+        route 
+        route
+        switch */}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
