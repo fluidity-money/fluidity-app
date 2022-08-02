f6f357f81628a8a0c3da31191e13308851b5684a
Connor <mussonconnor@gmail.com>
2022-08-02 17:27:15 +0300

    footer layout complete with dummy data

---
+++ web/fluidity.money/src/components/Socials/Socials.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Socials.module.scss";
+
+const Socials = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>Tw</div>
+      <div className={styles.social}>Di</div>
+      <div className={styles.social}>LIn</div>
+      <div className={styles.social}>Te</div>
+    </div>
+  );
+};
+
+export default Socials;
--- web/fluidity.money/src/screens/Footer/Footer.tsx
+++ web/fluidity.money/src/screens/Footer/Footer.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { TextButton } from "../../components/Button";
+import ContinuousCarousel from "../../components/ContinuousCarousel";
+import FooterItem from "../../components/FooterItem";
+import Socials from "../../components/Socials";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
@@ -6,7 +10,67 @@ const Footer = () => {
   data,
   continuous carousel at the bottom of large text
    */
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.footerItems}>
+          <FooterItem items={howItWorks}>How it works</FooterItem>
+          <FooterItem items={ecosystem}>Ecosystem</FooterItem>
+          <FooterItem items={fluidStats}>Fluid stats</FooterItem>
+          <FooterItem items={resources}>Resources</FooterItem>
+        </div>
+        <div className={styles.communication}>
+          <TextButton colour={"coloured"}>LET'S TALK</TextButton>
+          <Socials />
+          <h6>
+            © 2022 Fluidity Money. <br /> All Rights Reserved.
+          </h6>
+        </div>
+      </div>
+
+      <div className={styles.carousel}>
+        <ContinuousCarousel direction={"right"}>
+          <div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+            <div className={styles.text}>MONEY DESIGNED TO BE MOVED</div>
+          </div>
+        </ContinuousCarousel>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
+
+const howItWorks = [
+  { title: "Fluidity demo", src: "", type: "arrow" },
+  { title: "Roadmap", src: "", type: "box" },
+];
+
+const ecosystem = [
+  { title: "DeFi", src: "", type: "arrow" },
+  { title: "DEX", src: "", type: "arrow" },
+  { title: "Transactions", src: "", type: "arrow" },
+  { title: "NFTs", src: "", type: "box" },
+];
+
+const fluidStats = [
+  { title: "Overview", src: "", type: "arrow" },
+  { title: "Rewards", src: "", type: "arrow" },
+  { title: "Transactions", src: "", type: "arrow" },
+];
+
+const resources = [
+  { title: "Articles", src: "", type: "arrow" },
+  { title: "Fluniversity", src: "", type: "arrow" },
+  { title: "Whitepapers", src: "", type: "arrow" },
+  { title: "Documentation", src: "", type: "box" },
+];
