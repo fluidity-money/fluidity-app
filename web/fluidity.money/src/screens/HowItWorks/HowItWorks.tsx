943f4880f5f4c6670834443b3129a3fc4b867ab7
Connor <mussonconnor@gmail.com>
2022-08-02 13:01:35 +0300

    how it works continuos image text highlight complete

--- web/fluidity.money/src/screens/HowItWorks/HowItWorks.tsx
+++ web/fluidity.money/src/screens/HowItWorks/HowItWorks.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TextButton } from "../../components/Button";
 import styles from "./HowItWorks.module.scss";
 
@@ -8,10 +8,49 @@ const HowItWorks = () => {
   paragraph highlighted has different specific image,
   scrolls thought automatically and constantly
    */
+  const images = ["Image 1", "Image 2", "Image 3"];
+  const [currentImage, setCurrentImage] = useState("Image 1");
+
+  useEffect(() => {
+    let counter = 0;
+    const intervalId = setInterval(() => {
+      setCurrentImage(images[counter]);
+      counter++;
+      if (counter === 3) counter = 0;
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className={styles.container}>
-      <TextButton colour="white">MORE ON HOW IT WORKS</TextButton>
-      <div>
+      <div className={styles.grid}>
+        <div className={styles.left}>
+          <p
+            className={currentImage === "Image 1" ? styles.bold : styles.normal}
+          >
+            Fluid assets are a 1:1 wrapped asset with perpetual payout
+            properties.
+          </p>
+          <p
+            className={currentImage === "Image 2" ? styles.bold : styles.normal}
+          >
+            They distribute yield when when used on any on-chain use-case. Yiled
+            is gained through utility.
+          </p>
+          <p
+            className={currentImage === "Image 3" ? styles.bold : styles.normal}
+          >
+            The user is incentivised through governance.
+          </p>
+          <TextButton colour="white">MORE ON HOW IT WORKS</TextButton>
+        </div>
+        <div className={styles.right}>
+          <div>{currentImage}</div>
+        </div>
+      </div>
+
+      <div className={styles.footer}>
         <h1>HOW</h1>
         <h1>FLUIDITY</h1>
         <h1>WORKS</h1>
