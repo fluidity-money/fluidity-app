24a489332c34d4be9d50344b3c626ff586ade59c
Connor <mussonconnor@gmail.com>
2022-08-03 14:52:46 +0300

    built reusable manual carousel with scroll component

--- web/fluidity.money/src/components/ManualCarousel/ManualCarousel.tsx
+++ web/fluidity.money/src/components/ManualCarousel/ManualCarousel.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import styles from "./ManualCarousel.module.scss";
 
-const ManualCarousel = () => {
-  return <div>ManualCarousel</div>;
+interface ICarouselProps {
+  children: ReactNode;
+}
+
+const ManualCarousel = ({ children }: ICarouselProps) => {
+  /* carousel that displays mapped content as children with scrollbar below,
+  to ensure spacing between scrollbar and content, add margin-bottom:50px 
+  potential to upgrade with separate scrollbar component that is more
+  customisable and easier to position
+  */
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 };
 
 export default ManualCarousel;
--- web/fluidity.money/src/screens/Tweets/Tweets.tsx
+++ web/fluidity.money/src/screens/Tweets/Tweets.tsx
@@ -1,8 +1,51 @@
 import React from "react";
+import ManualCarousel from "../../components/ManualCarousel";
 import styles from "./Tweets.module.scss";
 
 const Tweets = () => {
-  return <div className={styles.container}>Tweets</div>;
+  return (
+    <div className={styles.container}>
+      <ManualCarousel>
+        {tweets.map((tweet) => (
+          <div className={styles.tweetContainer}>
+            <p>{tweet.text}</p>
+            <div>{tweet.date}</div>
+          </div>
+        ))}
+      </ManualCarousel>
+    </div>
+  );
 };
 
 export default Tweets;
+
+const tweets = [
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque ad, possimus, nostrum voluptates commodi totam atque animi vero hic veritatis voluptas nesciunt, nisi tenetur quibusdam odio tempora magnam unde?",
+    date: "7:08PM - Jun 23 2022",
+  },
+];
