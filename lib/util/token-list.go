6b4750cbfdc90f9e61b76b8c8391c45503c84015
elle <elle@catgirl.software>
2022-08-09 10:49:42 +0930

    restore original token list behaviour

--- lib/util/token-list.go
+++ lib/util/token-list.go
@@ -42,8 +42,9 @@ func GetTokensListBase(tokensList_ string) []TokenDetailsBase {
 		}
 
 		var (
-			tokenName    = trimWhitespace(tokenDetails_[0])
-			decimals_    = trimWhitespace(tokenDetails_[1])
+			_            = trimWhitespace(tokenDetails_[0])
+			tokenName    = trimWhitespace(tokenDetails_[1])
+			decimals_    = trimWhitespace(tokenDetails_[2])
 		)
 
 		decimals, err := strconv.Atoi(decimals_)
