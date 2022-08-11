cd1ee6be3dd41479128b9917e3306c705b86b3a3
Connor <mussonconnor@gmail.com>
2022-08-10 21:26:04 -0700

    inital filter logic added for buttons

--- web/fluidity.money/src/screens/FluidProjects/FluidProjects.tsx
+++ web/fluidity.money/src/screens/FluidProjects/FluidProjects.tsx
@@ -1,21 +1,7 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import styles from "./FluidProjects.module.scss";
 
 const FluidProjects = () => {
-  /* scrolls to location on pageload if it contains same ID or scrolls to the top
-   for ResourcesNavModal to work*/
-  const location = useLocation();
-  useEffect(() => {
-    if (location.hash) {
-      let elem = document.getElementById(location.hash.slice(1));
-      if (elem) {
-        elem.scrollIntoView({ behavior: "smooth" });
-      }
-    } else {
-      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    }
-  }, [location]);
   return (
     <div className={styles.container} id="fluidprojects">
       FluidProjects
