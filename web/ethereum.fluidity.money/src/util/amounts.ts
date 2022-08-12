a713889cd252c7425f4e900cd2a186ab029731ea
harlexkhal <harlexibeh01@gmail.com>
2022-08-12 12:38:15 +0100

    feat: amount display for large values for both ethereum and solana

--- web/ethereum.fluidity.money/src/util/amounts.ts
+++ web/ethereum.fluidity.money/src/util/amounts.ts
@@ -104,3 +104,21 @@ export const shortBalance = (balance: string) =>
     : `${balance}`.substr(0, 4) +
       "..." +
       `${balance}`.substr(`${balance}`.length - 2, `${balance}`.length - 1);
+
+
+export const shorthandAmountFormatter = (value: string, decimals: number): string => {
+
+  let num: number = parseFloat(value)
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "K" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "B" },
+    { value: 1e12, symbol: "T" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup.slice().reverse().find(function(item) {
+    return num >= item.value;
+  });
+  return item ? (num / item.value).toFixed(decimals).replace(rx, "$1") + item.symbol : "0";
+};
