81834c17280ae1cd1ff42e0704b890e3e448c76b
user <alex@iometelabs.io>
2022-08-15 10:11:27 +0930

    Remove the AMQP uri from logging messages

--- cmd/microservice-common-rabbitmq-backlog-checker/main.go
+++ cmd/microservice-common-rabbitmq-backlog-checker/main.go
@@ -50,7 +50,7 @@ func main() {
 
 	if err != nil {
 		log.Fatal(func(k *log.Log) {
-			k.Format("could not retrieve Vhosts from RMQ Management (%v)!", queueAddress)
+			k.Format("could not retrieve Vhosts from RMQ Management!")
 			k.Payload = err
 		})
 	}
@@ -60,7 +60,7 @@ func main() {
 
 		if err != nil {
 			log.Fatal(func(k *log.Log) {
-				k.Format("could not retrieve queues from RMQ Management (%v)!", queueAddress)
+				k.Format("could not retrieve queues from RMQ Management!")
 				k.Payload = err
 			})
 		}
