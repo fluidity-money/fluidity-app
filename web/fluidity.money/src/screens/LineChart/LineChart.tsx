2758a9a3960baa01be1a6ebd1e50d9f796d03e60
Joshua Steele <jsteele@nitrocade.com>
2022-08-16 10:15:42 +0930

    Shift chart work to library

--- web/fluidity.money/src/screens/LineChart/LineChart.tsx
+++ web/fluidity.money/src/screens/LineChart/LineChart.tsx
@@ -1,27 +1,8 @@
 import React from "react";
 import styles from "./LineChart.module.scss";
 
-import { useLivePrizePool } from "data/LivePrivePool";
-
-import {
-  AreaClosed
-} from "@visx/shape"
-
-import {
-  withParentSizeModern
-} from "@visx/responsive"
-
-
-const ChartItself = () => {
-  const d = new Date().getTime() - new Date(2020, 0, 1).getTime()
-  return <AreaClosed
-    data={[]}
-  />
-}
-
 const LineChart = () => {
   return <div className={styles.container}>
-    withParentSizeModern()
   </div>;
 };
 
