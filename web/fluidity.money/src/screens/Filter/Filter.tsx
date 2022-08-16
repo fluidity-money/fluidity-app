46b9c509c3ee3458ccf6feaf65202f52b6b5f836
Connor <mussonconnor@gmail.com>
2022-08-16 08:59:58 -0700

    cleaned code and added project info and sort functionality

--- web/fluidity.money/src/screens/Filter/Filter.tsx
+++ web/fluidity.money/src/screens/Filter/Filter.tsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import FIlterCriteria from "../../components/FilterCriteria";
 import FluidProject from "../../components/FluidProject";
 import styles from "./Filter.module.scss";
+import { clearForAny, handleFilterButton } from "./util";
+
+interface IOption {
+  name: string;
+  selected: boolean;
+}
 
 const Filter = () => {
   const [catOptions, setCatOptions] = useState(filters[0]);
@@ -10,14 +16,17 @@ const Filter = () => {
   const [filterList, setFilterList] = useState<string[]>([
     "anyCat",
     "anyChain",
-    "anyDate",
+    "anyYear",
   ]);
   const [projects, setProjects] = useState(fluidProjects);
 
-  interface IOption {
-    name: string;
-    selected: boolean;
-  }
+  useEffect(() => {
+    // filter based on filter list whenever the list changes
+    handleFilterProjects();
+    updateAny(chains, setChains);
+    updateAny(years, setYears);
+    updateAny(catOptions, setCatOptions);
+  }, [filterList, catOptions, chains, years]);
 
   // updates filter to be any when last option is deselected
   const updateAny = (
@@ -37,14 +46,33 @@ const Filter = () => {
     }
   };
 
-  useEffect(() => {
-    console.log("FL", filterList);
-    // filter based on filter list whenever the list changes
-    handleFilterProjects();
-    updateAny(chains, setChains);
-    updateAny(years, setYears);
-    updateAny(catOptions, setCatOptions);
-  }, [filterList, catOptions, chains, years]);
+  // add and remove items from the list used to filter the data
+  const handleFilterList = (option: IOption) => {
+    //if option was not selected
+    if (option.selected === false) {
+      if (option.name.includes("any")) {
+        clearForAny(option, setFilterList);
+      }
+      // add to filterList
+      else {
+        !filterList.includes(option.name) &&
+          setFilterList(() => [...filterList, option.name]);
+      }
+    }
+    // if option was selected
+    if (option.selected === true) {
+      // if any reset the filterList
+      if (option.name.includes("any")) {
+        clearForAny(option, setFilterList);
+      }
+      // remove from filterList
+      else {
+        setFilterList((initalState) =>
+          initalState.filter((item) => item !== option.name)
+        );
+      }
+    }
+  };
 
   // filter the projects based on cat/chains/years options
   const handleFilterProjects = () => {
@@ -61,146 +89,18 @@ const Filter = () => {
     );
   };
 
-  const handleFilterList = (
+  // consolidated filter function
+  const handleFilter = (
     option: IOption,
     setOption: React.Dispatch<React.SetStateAction<IOption[]>>
   ) => {
-    //if option was not selected
-    if (option.selected === false) {
-      if (option.name.includes("any")) {
-        if (option.name === "anyCat") {
-          setFilterList((initalState) =>
-            initalState.filter(
-              (item) =>
-                ![
-                  "defi",
-                  "dex",
-                  "nft",
-                  "gaming",
-                  "payments",
-                  "metaverse",
-                  "dao",
-                ].includes(item)
-            )
-          );
-        }
-        if (option.name === "anyChain") {
-          setFilterList((initalState) =>
-            initalState.filter(
-              (item) => !["ethereum", "polygon", "solana"].includes(item)
-            )
-          );
-        }
-        if (option.name === "anyDate") {
-          setFilterList((initalState) =>
-            initalState.filter(
-              (item) => !["2020", "2021", "2022"].includes(item)
-            )
-          );
-        }
-      }
-      // add to filterList
-      else {
-        !filterList.includes(option.name) &&
-          setFilterList(() => [...filterList, option.name]);
-      }
-    }
-    // if option was selected
-    if (option.selected === true) {
-      // if any reset the filterList
-      if (option.name.includes("any")) {
-        if (option.name === "anyCat") {
-          setFilterList((initalState) =>
-            initalState.filter(
-              (item) =>
-                ![
-                  "defi",
-                  "dex",
-                  "nft",
-                  "gaming",
-                  "payments",
-                  "metaverse",
-                  "dao",
-                ].includes(item)
-            )
-          );
-        }
-        if (option.name === "anyChain") {
-          setFilterList((initalState) =>
-            initalState.filter(
-              (item) => !["ethereum", "polygon", "solana"].includes(item)
-            )
-          );
-        }
-        if (option.name === "anyDate") {
-          setFilterList((initalState) =>
-            initalState.filter(
-              (item) => !["2020", "2021", "2022"].includes(item)
-            )
-          );
-        }
-      }
-      // remove from filterList
-      else {
-        setFilterList((initalState) =>
-          initalState.filter((item) => item !== option.name)
-        );
-      }
-    }
+    handleFilterButton(option, setOption);
+    handleFilterList(option);
   };
 
-  // updates button option from selected to not selected
-  const handleFilterOption = (
-    option: IOption,
-    setOption: React.Dispatch<React.SetStateAction<IOption[]>>,
-    options: IOption[]
-  ) => {
-    // if option is any and it isn't selected, make any selected and clear options
-    if (option.name.includes("any") && option.selected === false) {
-      setOption((previousState) =>
-        previousState.map((item) => {
-          if (item.name.includes("any")) {
-            return item;
-          }
-          return { ...item, selected: false };
-        })
-      );
-    }
-
-    // if option isn't any, and isn't selected, unselect any
-    if (!option.name.includes("any") && option.selected === false) {
-      setOption((previousState) =>
-        previousState.map((item) => {
-          if (!item.name.includes("any")) {
-            return item;
-          }
-          return { ...item, selected: false };
-        })
-      );
-    }
-    // invert option
-    setOption((previousState) =>
-      previousState.map((item) => {
-        // if it isn't the same or if it is any and selected, no change
-        if (
-          item.name !== option.name ||
-          (item.name.includes("any") && item.selected === true)
-        ) {
-          return item;
-        }
-        // invert button type
-        return { ...item, selected: !option.selected };
-      })
-    );
-  };
-
-  const handleFilter = (
-    option: IOption,
-    setOption: React.Dispatch<React.SetStateAction<IOption[]>>,
-    options: IOption[]
-  ) => {
-    handleFilterOption(option, setOption, options);
-    handleFilterList(option, setOption);
+  // sort projects displayed
+  const sortProjects = () => {
+    setProjects([...projects].sort((a, b) => b.topPrize - a.topPrize));
   };
 
   return (
@@ -236,11 +136,11 @@ const Filter = () => {
             <div>{`${projects.length ? "1" : 0} - ${projects.length} of ${
               fluidProjects.length
             } Projects`}</div>
-            <div>Sort by Top Prize $$$ v</div>
+            <div onClick={() => sortProjects()}>Sort by Top Prize $$$ v</div>
           </div>
           <div className={styles.grid}>
             {projects.map((project) => (
-              <FluidProject title={project.title} />
+              <FluidProject project={project} />
             ))}
           </div>
         </div>
@@ -274,7 +174,7 @@ const filters: IOption[][] = [
     { name: "ethereum", selected: false },
   ],
   [
-    { name: "anyDate", selected: true },
+    { name: "anyYear", selected: true },
     { name: "2020", selected: false },
     { name: "2021", selected: false },
     { name: "2022", selected: false },
@@ -295,7 +195,8 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 20000,
   },
   {
     title: "🦍",
@@ -310,25 +211,29 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 10000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "dex", "nft", "metaverse", "dao"],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 5000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "dex", "nft", "gaming", "payments"],
     chains: ["anyChain", "solana"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 22000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "nft", "gaming", "payments", "metaverse", "dao"],
     chains: ["anyChain", "polygon"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 18000,
   },
   {
     title: "🦍",
@@ -342,31 +247,36 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 3000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "dao"],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 100,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "dex", "nft"],
     chains: ["anyChain", "solana"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 700,
   },
   {
     title: "🦍",
     categories: ["anyCat", "dex", "nft", "gaming", "dao"],
     chains: ["anyChain", "solana"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 7000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "dex", "nft", "gaming", "metaverse", "dao"],
     chains: ["anyChain", "polygon"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 8000,
   },
   {
     title: "🦍",
@@ -381,7 +291,8 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "polygon"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 13000,
   },
   {
     title: "🦍",
@@ -396,25 +307,29 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "solana"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 11000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "metaverse", "dao"],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 6000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "dex", "nft", "gaming", "payments"],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 1000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "nft", "gaming", "metaverse"],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 2000,
   },
   {
     title: "🦍",
@@ -429,7 +344,8 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "polygon"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 19300,
   },
   {
     title: "🦍",
@@ -444,7 +360,8 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "solana"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 18200,
   },
   {
     title: "🦍",
@@ -459,13 +376,15 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 15000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "gaming", "payments"],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 4000,
   },
   {
     title: "🦍",
@@ -480,12 +399,14 @@ const fluidProjects = [
       "dao",
     ],
     chains: ["anyChain", "ethereum"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 2000,
   },
   {
     title: "🦍",
     categories: ["anyCat", "defi", "dex", "nft", "gaming", "dao"],
     chains: ["anyChain", "solana"],
-    years: ["anyDate", "2022"],
+    years: ["anyYear", "2022"],
+    topPrize: 9000,
   },
 ];
