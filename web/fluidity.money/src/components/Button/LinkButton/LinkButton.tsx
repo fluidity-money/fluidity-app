fa12a95ba45d73c52f60a36d3f160e1d28da4d8d
Connor <mussonconnor@gmail.com>
2022-08-17 13:52:25 -0700

    added Link Button Component

---
+++ web/fluidity.money/src/components/Button/LinkButton/LinkButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./LinkButton.module.scss";
+
+interface ILinkButtonProps {
+  children: string;
+  size: "small" | "medium" | "large";
+  type: "internal" | "external";
+}
+
+const LinkButton = ({ children, size, type }: ILinkButtonProps) => {
+  return (
+    <button className={styles.button}>
+      <div className={`${styles.text} ${styles[size]}`}>
+        {children.toUpperCase()}
+      </div>
+      <img
+        className={styles.icon}
+        src={
+          type === "external"
+            ? "/assets/images/buttonIcons/arrowTopRightWhite.svg"
+            : "/assets/images/buttonIcons/arrowRightWhite.svg"
+        }
+        alt="link icon"
+      />
+    </button>
+  );
+};
+
+export default LinkButton;
--- web/fluidity.money/src/components/Button/index.tsx
+++ web/fluidity.money/src/components/Button/index.tsx
@@ -3,5 +3,13 @@ import ToggleButton from "./ToggleButton/ToggleButton";
 import FilterButton from "./FilterButton";
 import AnchorButton from "./AnchorButton";
 import GeneralButton from "./GeneralButton/GeneralButton";
+import LinkButton from "./LinkButton";
 
-export { TextButton, ToggleButton, FilterButton, AnchorButton, GeneralButton };
+export {
+  TextButton,
+  ToggleButton,
+  FilterButton,
+  AnchorButton,
+  GeneralButton,
+  LinkButton,
+};
