59321fe95b75d0ffae0e54c030fb41e22805650c
harlexkhal <harlexibeh01@gmail.coM>
2022-08-18 08:20:55 +0100

    mod: modifying and moving article component around

--- web/fluidity.money/src/components/Article/ArticleDisplayCard/ArticleDisplayCard.tsx
+++ web/fluidity.money/src/components/Article/ArticleDisplayCard/ArticleDisplayCard.tsx
@@ -9,20 +9,19 @@ export interface ArticleDisplayCardType {
 }
 
 const ArticleDisplayCard = (article: ArticleDisplayCardType) => {
-
-    return (
-        <div style={{width: "80%", height:  "100%", border: "1px red solid", padding: "40px 40px 40px 40px", marginRight: "40px"}}>
-        <div style={{width: "100%", height:  "350px", border: "1px red solid", margin:"10px 10px 10px 10px"}}>
-        <img  style={{width: "100%", height: "100%"}} src={article.img}/>
-        </div>
-        <h1>{article.title}</h1>
-        <p>
-         {article.desc}
-        </p>
-        <p>{article.info} min read ARTICLE</p>
-        <TextButton colour="coloured">ALL ARTICLES</TextButton>
-    </div>
-    );
+  return (
+      <div style={{width: "80%", height:  "100%", border: "1px red solid", padding: "40px 40px 40px 40px", marginRight: "40px"}}>
+      <div style={{width: "100%", height:  "350px", border: "1px red solid", margin:"10px 10px 10px 10px"}}>
+      <img  style={{width: "100%", height: "100%"}} src={article.img}/>
+      </div>
+      <h1>{article.title}</h1>
+      <p>
+        {article.desc}
+      </p>
+      <p>{article.info} min read ARTICLE</p>
+      <TextButton colour="coloured">ALL ARTICLES</TextButton>
+  </div>
+  );
 };
 
 export default ArticleDisplayCard;
