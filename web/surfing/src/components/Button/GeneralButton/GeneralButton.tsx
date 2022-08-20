90c18434837e66c0f447971cc4c250106f105667
glo12345 <glorison@fluidity.money>
2022-08-20 14:58:04 +1000

    feat: Added GeneralButton pseudostates

--- web/surfing/src/components/Button/GeneralButton/GeneralButton.tsx
+++ web/surfing/src/components/Button/GeneralButton/GeneralButton.tsx
@@ -17,36 +17,57 @@ const GeneralButton = ({
   size,
   handleClick,
   disabled,
+  className,
+  ...props
 }: IGeneralButtonProps) => {
+  const classProps = className || "";
+
   return (
     <>
       {version === "primary" && type === "text" ? (
         <button
           onClick={handleClick}
-          className={`${styles.primary} ${styles[size]}`}
+          className={`${styles.primary} ${styles[size]} ${classProps}`}
           disabled={disabled}
+          {...props}
         >
-          {children.toLocaleUpperCase()}
+          {children}
         </button>
       ) : version === "primary" && type === "icon before" ? (
-        <button onClick={handleClick} className={styles.primary} disabled={disabled} >
-          {children.toLocaleUpperCase()}
+        <button
+          onClick={handleClick}
+          className={`${styles.primary} ${classProps}`}
+          disabled={disabled}
+          {...props}
+        >
+          {children}
         </button>
       ) : version === "primary" && type === "icon after" ? (
-        <button onClick={handleClick} className={styles.primary} disabled={disabled} >
-          {children.toLocaleUpperCase()}
+        <button
+          onClick={handleClick}
+          className={`${styles.primary} ${classProps}`}
+          disabled={disabled}
+          {...props}
+        >
+          {children}
         </button>
       ) : type === "icon only" ? (
-        <button onClick={handleClick} className={styles.iconOnly}>
+        <button
+          onClick={handleClick}
+          className={`${styles.iconOnly} ${classProps}`}
+          {...props}
+        >
           {children}
         </button>
       ) : (
+        // Secondary Buttons are text only
         <button
           onClick={handleClick}
-          className={`${styles.secondary} ${styles[size]}`}
+          className={`${styles.secondary} ${styles[size]} ${classProps}`}
           disabled={disabled}
+          {...props}
         >
-          {children.toLocaleUpperCase()}
+          {children}
         </button>
       )}
     </>
