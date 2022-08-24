14092f481f11b9d0c9ec02fd43d6cf7af6f2957f
glo12345 <glorison@fluidity.money>
2022-08-24 11:35:54 +1000

    feat: added reactive row container stories

--- web/surfing/src/components/Container/Row.tsx
+++ web/surfing/src/components/Container/Row.tsx
@@ -2,15 +2,20 @@ import type { HTMLProps } from "react";
 
 import styles from "./Row.module.scss";
 
+interface IRowProps extends HTMLProps<HTMLDivElement> {
+  reverse?: boolean;
+}
+
 const Row = ({
   children,
   className,
+  reverse,
   ...props
-}: HTMLProps<HTMLDivElement> ) => {
+}: IRowProps ) => {
   const classProps = className || "";
 
   return (
-    <div className={`${styles.row} ${classProps}`} {...props} >
+    <div className={`${styles.row} ${reverse && styles.reverse} ${classProps}`} {...props} >
       {children}
     </div>
   );
