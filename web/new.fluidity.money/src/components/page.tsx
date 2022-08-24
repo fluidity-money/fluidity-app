2761859161aa24eb258c2993fea603053687ee73
glo12345 <glorison@fluidity.money>
2022-08-25 00:37:12 +1000

    feat: Added footer to webapp pages comp

--- web/new.fluidity.money/src/components/page.tsx
+++ web/new.fluidity.money/src/components/page.tsx
@@ -1,12 +1,14 @@
 import type { HTMLProps } from 'react';
 
-import { GeneralButton } from 'surfing';
+import { GeneralButton, Row, Heading } from 'surfing';
 
 const Page = ({ children, ...props }: HTMLProps<HTMLDivElement>) => {
+  const fluVersion = 1.5;
+
   return (
     <div {...props} >
       {/** Heading + Buttons*/}
-      <div>
+      <Row>
         <h2>Dashboard</h2>
         {/** Button Group */}
         <div>
@@ -29,10 +31,22 @@ const Page = ({ children, ...props }: HTMLProps<HTMLDivElement>) => {
             Fluidify Money
           </GeneralButton>
         </div>
-      </div>
+      </Row>
       {/** Sidebar */}
       {children}
       {/** Footer */}
+      <Row>
+        {/** General Links */}
+        <div>
+          <Heading as={"h6"}>
+            Fluidity Money V{fluVersion}
+          </Heading>
+        </div>
+        
+        {/** Social Links */}
+        <div>
+        </div>
+      </Row>
     </div>
   )
 }
