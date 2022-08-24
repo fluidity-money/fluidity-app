1831885854a4f0c7df96c47577429e0348598534
Connor <mussonconnor@gmail.com>
2022-08-24 08:25:27 -0700

    added socials with carousel and footer updates

--- web/fluidity.money/src/components/Socials/Socials.tsx
+++ web/fluidity.money/src/components/Socials/Socials.tsx
@@ -1,13 +1,78 @@
+import ContinuousCarousel from "components/ContinuousCarousel";
 import React from "react";
 import styles from "./Socials.module.scss";
 
 const Socials = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.social}>Tw</div>
-      <div className={styles.social}>Di</div>
-      <div className={styles.social}>LIn</div>
-      <div className={styles.social}>Te</div>
+      <ContinuousCarousel direction="right">
+        <div className={styles.content}>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/twitter.svg" alt="" />
+          </div>
+          <h3>@FLUIDITYMONEY</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/discord.svg" alt="" />
+          </div>
+          <h3>DISCORD</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/telegram.svg" alt="" />
+          </div>
+          <h3>TELEGRAM</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/linkedin.svg" alt="" />
+          </div>
+          <h3>LINKEDIN</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/twitter.svg" alt="" />
+          </div>
+          <h3>@FLUIDITYMONEY</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/discord.svg" alt="" />
+          </div>
+          <h3>DISCORD</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/telegram.svg" alt="" />
+          </div>
+          <h3>TELEGRAM</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/linkedin.svg" alt="" />
+          </div>
+          <h3>LINKEDIN</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/twitter.svg" alt="" />
+          </div>
+          <h3>@FLUIDITYMONEY</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/discord.svg" alt="" />
+          </div>
+          <h3>DISCORD</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/telegram.svg" alt="" />
+          </div>
+          <h3>TELEGRAM</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/linkedin.svg" alt="" />
+          </div>
+          <h3>LINKEDIN</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/twitter.svg" alt="" />
+          </div>
+          <h3>@FLUIDITYMONEY</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/discord.svg" alt="" />
+          </div>
+          <h3>DISCORD</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/telegram.svg" alt="" />
+          </div>
+          <h3>TELEGRAM</h3>
+          <div className={styles.imageContainer}>
+            <img src="/assets/images/socials/linkedin.svg" alt="" />
+          </div>
+          <h3>LINKEDIN</h3>
+        </div>
+      </ContinuousCarousel>
     </div>
   );
 };
