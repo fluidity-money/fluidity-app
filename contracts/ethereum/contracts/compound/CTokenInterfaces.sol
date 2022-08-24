d2db5824b8560eec822c11e7278df61aa97addf3
user <alex@iometelabs.io>
2022-08-24 19:32:02 +0930

    License the contracts

--- contracts/ethereum/contracts/compound/CTokenInterfaces.sol
+++ contracts/ethereum/contracts/compound/CTokenInterfaces.sol
@@ -1,3 +1,7 @@
+// Copyright 2022 Fluidity Money. All rights reserved. Use of this
+// source code is governed by a GPL-style license that can be found in the
+// LICENSE.md file.
+
 pragma solidity ^0.8.11;
 
 interface CErc20Interface {
