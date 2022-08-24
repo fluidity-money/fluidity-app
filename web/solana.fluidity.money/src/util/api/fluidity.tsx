2b4e684b3ff7eb7121e23096e579c8c6106a71d4
user <alex@iometelabs.io>
2022-08-24 21:55:24 +0930

    Use wildcard for subdomains for access control allow origin

--- web/solana.fluidity.money/src/util/api/fluidity.tsx
+++ web/solana.fluidity.money/src/util/api/fluidity.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const headers = {
-  "Content-Type": "application/json"
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*.fluidity.money",
+  'Access-Control-Allow-Methods': 'POST'
 }
 
 export default axios.create({
