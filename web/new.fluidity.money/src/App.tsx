40ce764a097dd859234992ff24af43bf95058a28
glo12345 <glorison@fluidity.money>
2022-08-24 11:48:53 +1000

    feat: added routing libs

--- web/new.fluidity.money/src/App.tsx
+++ web/new.fluidity.money/src/App.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Dashboard from './screens/authenticated/dashboard';
+import Rewards from './screens/authenticated/rewards';
+import Assets from './screens/authenticated/assets';
+import Dao from './screens/authenticated/dao';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -6,28 +12,42 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+  <Router>
+    <Switch>
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <Home />
+        )}
+      />
+        
+      <ProtectedRoute
+        exact={true}
+        path="/dashboard"
+        component={Dashboard}
+        extraProps={{}}
+      />
+      <ProtectedRoute
+        exact={true}
+        path="/rewards"
+        component={Reward}
+        extraProps={{}}
+      />
+      <ProtectedRoute
+        exact={true}
+        path="/assets"
+        component={Assets}
+        extraProps={{}}
+      />
+      <ProtectedRoute
+        exact={true}
+        path="/dao"
+        component={Dao}
+        extraProps={{}}
+      />
+    </Switch>
+  </Router>
   )
 }
 
