f2a4b242b8da99844645753ead2f1f2b995b85b6
Connor <mussonconnor@gmail.com>
2022-08-25 11:54:25 -0700

    updated HIW template, created ReuseGrid template

--- web/fluidity.money/src/components/HowItWorksTemplate/HowItWorksTemplate.tsx
+++ web/fluidity.money/src/components/HowItWorksTemplate/HowItWorksTemplate.tsx
@@ -3,31 +3,18 @@ import styles from "./HowItWorksTemplate.module.scss";
 
 interface ITemplateProps {
   children: string;
+  header: string;
+  info: string[];
 }
 
-const HowItWorksTemplate = ({ children }: ITemplateProps) => {
+const HowItWorksTemplate = ({ children, header, info }: ITemplateProps) => {
   return (
-    <div className={styles.grid}>
-      <div className={styles.left}>
-        <div className={styles.content}>
-          <h1>{children}</h1>
-          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-            nisi reprehenderit, corporis excepturi quos, porro assumenda
-            exercitationem voluptatem quidem labore.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-            nisi reprehenderit, corporis excepturi quos, porro assumenda
-            exercitationem voluptatem quidem labore, neque consectetur illo
-            atque! Aut atque sapiente maiores nisi dignissimos?
-          </p>
-        </div>
-      </div>
-      <div className={styles.right}>
-        <div style={{ fontSize: 160 }}>🦍</div>
-      </div>
+    <div className={styles.content}>
+      <h1>{children}</h1>
+      <h2>{header}</h2>
+      {info.map((paragraph) => (
+        <p>{paragraph}</p>
+      ))}
     </div>
   );
 };
