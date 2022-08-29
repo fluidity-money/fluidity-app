33394e06ab85c0d04724cdcce744bf809f5e8048
Connor <mussonconnor@gmail.com>
2022-08-29 10:44:24 -0700

    removed react-router link components from navigation

--- web/surfing/src/components/Navigation/Navigation.tsx
+++ web/surfing/src/components/Navigation/Navigation.tsx
@@ -1,6 +1,5 @@
 import { AnchorButton } from "components/Button";
 import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
 import styles from "./Navigation.module.scss";
 
 interface INavigation {
@@ -10,24 +9,14 @@ interface INavigation {
 
 const Navigation = ({ pageLocations, page }: INavigation) => {
   /* scrolls to location on pageload if it contains same ID or scrolls to the top */
-  const location = useLocation();
-  useEffect(() => {
-    if (location.hash) {
-      let elem = document.getElementById(location.hash.slice(1));
-      if (elem) {
-        elem.scrollIntoView({ behavior: "smooth" });
-      }
-    } else {
-      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    }
-  }, [location]);
+
   return (
     <div className={styles.container}>
       {pageLocations.map((location) => (
         <h4>
-          <Link to={`/${page}#${location.replace(/\s/g, "")}`}>
+          <a href={`/${page}#${location.replace(/\s/g, "")}`}>
             {<AnchorButton>{location.toUpperCase()}</AnchorButton>}
-          </Link>
+          </a>
         </h4>
       ))}
     </div>
