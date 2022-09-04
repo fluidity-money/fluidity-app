bcc63058dc92b8da205455ffb44f5b7bc718be84
Connor <mussonconnor@gmail.com>
2022-09-04 10:41:13 -0700

    updated demo and page

--- web/fluidity.money/src/screens/Demo/Demo.tsx
+++ web/fluidity.money/src/screens/Demo/Demo.tsx
@@ -8,31 +8,41 @@ import styles from "./Demo.module.scss";
 
 const Demo = () => {
   const { apiState } = useChainContext();
-  const weekTotalRewards = apiState.weekWinnings
-    .reduce((weekSum, { winning_amount, token_decimals }) => weekSum + (winning_amount / 10 ** token_decimals), 0)
+  const weekTotalRewards = apiState.weekWinnings.reduce(
+    (weekSum, { winning_amount, token_decimals }) =>
+      weekSum + winning_amount / 10 ** token_decimals,
+    0
+  );
   /*
   full screen demo
   */
   return (
     <>
       <div className={`${styles.container} bg-light`}>
-        <video  autoPlay muted loop className={styles.demoVideo}>
-          <source src={window.location.origin + '/assets/videos/sample.mp4'} type="video/mp4"/>   
+        <video autoPlay muted loop className={styles.demoVideo}>
+          <source
+            src={window.location.origin + "/assets/videos/sample.mp4"}
+            type="video/mp4"
+          />
         </video>
         <div>
           <h1>{numberToMonetaryString(weekTotalRewards)}</h1>
           <h3>Fluid prizes claimed in the last week.</h3>
           <p>Connect your wallet to see what you could make</p>
           <section>
-            <GeneralButton version={"primary"} type={"text"} size={"medium"} handleClick={function (): void {       
+            {/* <GeneralButton version={"primary"} type={"text"} size={"medium"} handleClick={function (): void {       
             } }>
               SHOW ME A DEMO
-            </GeneralButton>
-            <GeneralButton version={"secondary"} type={"text"} size={"medium"} handleClick={function (): void {
-            } }>
+            </GeneralButton> */}
+            <GeneralButton
+              version={"secondary"}
+              type={"text"}
+              size={"medium"}
+              handleClick={function (): void {}}
+            >
               LAUNCH FLUIDITY
-            </GeneralButton>  
-          </section>   
+            </GeneralButton>
+          </section>
         </div>
       </div>
     </>
