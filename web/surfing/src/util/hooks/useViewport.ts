35e5a1fc78dae554da94069560a413c28e152e64
Joshua Steele <jsteele@nitrocade.com>
2022-09-18 19:43:10 +0930

    Be SSR safe

--- web/surfing/src/util/hooks/useViewport.ts
+++ web/surfing/src/util/hooks/useViewport.ts
@@ -1,12 +1,15 @@
 import React from "react";
 // used to detect window width
 const useViewport = () => {
-  const [width, setWidth] = React.useState(window.innerWidth);
+
+  const [width, setWidth] = React.useState(typeof window !== "undefined" ? window.innerWidth : 0);
 
   React.useEffect(() => {
-    const handleWindowResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleWindowResize);
-    return () => window.removeEventListener("resize", handleWindowResize);
+    if (typeof window !== "undefined") { 
+      const handleWindowResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleWindowResize);
+      return () => window.removeEventListener("resize", handleWindowResize);
+    }
   }, []);
 
   // Return the width so we can use it in our components
@@ -14,3 +17,4 @@ const useViewport = () => {
 };
 
 export default useViewport;
+
