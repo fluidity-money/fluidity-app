b1460f86b1f5227f3dc089da6d8784d14afd3cc0
Joshua Steele <josh@fluidity.money>
2022-09-18 20:03:48 +0930

    useViewport but SSR safe (#789)
    * Be SSR safe
    
    * Using window out of useEffect is unsafe

--- web/surfing/src/util/hooks/useViewport.ts
+++ web/surfing/src/util/hooks/useViewport.ts
@@ -1,15 +1,13 @@
 import React from "react";
 // used to detect window width
 const useViewport = () => {
-
-  const [width, setWidth] = React.useState(typeof window !== "undefined" ? window.innerWidth : 0);
+  const [width, setWidth] = React.useState(0);
 
   React.useEffect(() => {
-    if (typeof window !== "undefined") { 
-      const handleWindowResize = () => setWidth(window.innerWidth);
-      window.addEventListener("resize", handleWindowResize);
-      return () => window.removeEventListener("resize", handleWindowResize);
-    }
+    setWidth(window.innerWidth)
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
 
   // Return the width so we can use it in our components
