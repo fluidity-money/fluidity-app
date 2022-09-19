035653c588360d7892aa5ca1d942d4366b63546a
eli-d <64763513+eli-d@users.noreply.github.com>
2022-09-19 14:56:06 +0930

    spacing

--- web/new.fluidity.money/src/App.tsx
+++ web/new.fluidity.money/src/App.tsx
@@ -4,7 +4,7 @@ import Rewards from './screens/authenticated/rewards';
 import Assets from './screens/authenticated/assets';
 import Dao from './screens/authenticated/dao';
 import Home from './screens/Home';
-import { Chain, chainContext, Network, Chains, NullableChain, SupportedUnwrappedToken, SupportedFluidToken} from "./components/chains/chainContext";
+import {chainContext, Chains} from "./components/chains/chainContext";
 import './App.css'
 import {useContext} from "react";
 import ChainInterface from "./components/chains";
@@ -13,34 +13,28 @@ const Div = () => {
   const con = useContext(chainContext)
   const {connected, chain, network, setNetwork, connect, disconnect, wrap, unwrap, setChain} = con || {};
 
-  // if (chain === "solana") setNetwork("devnet")
+  return <div>
+    {`connected: ${connected}. chain: ${chain}, network: ${network}`}
+    <br/>
+    <button onClick={() => chain !== null && connect(network)}>connect</button>
+    <button onClick={disconnect}>disconnect</button>
+    <br/>
+    <button onClick={() => wrap("USDC", 10000)}>wrap</button>
+    <button onClick={() => unwrap("USDC", 10000)}>unwrap</button>
+    <br/>
+    <button onClick={() => setChain("solana")}>solana</button>
+    <button onClick={() => setChain("ethereum")}>ethereum</button>
+    {chain !== null && 
+      <ul>{Chains[chain].map((k,i)=>
+        <li style={{cursor: 'pointer'}} onClick={() => setNetwork(k)} key={i}>
+        {k}
+        </li>)}
+      </ul>
+    }
+  </div>
+}
 
-return <div>
-      {`connected: ${connected}. chain: ${chain}, network: ${network}`}
-      <br/>
-      <button onClick={() => chain !== null && connect(network)}>connect</button>
-      <button onClick={disconnect}>disconnect</button>
-      <br/>
-      <button onClick={() => wrap("USDC", 10000)}>wrap</button>
-      <button onClick={() => unwrap("USDC", 10000)}>unwrap</button>
-      <br/>
-      <button onClick={() => setChain("solana")}>solana</button>
-      <button onClick={() => setChain("ethereum")}>ethereum</button>
-      {chain !== null && 
-        <ul>{Chains[chain].map((k,i)=>
-          <li style={{cursor: 'pointer'}} onClick={() => setNetwork(k)} key={i}>
-          {k}
-          </li>)}
-        </ul>
-      }
-      </div>
-  }
 const App = () => {
-  // const isConnected = () => {
-  //   const wallet = useWallet();
-  //   return wallet.status === "connected";
-  // }
-
   return <>
     <ChainInterface>
     <Div/>
