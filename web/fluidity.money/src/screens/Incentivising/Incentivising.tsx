156bd51769dc6ac1d3c35dd3bdabc085032dfe7b
harlexkhal <harlexibeh01@gmail.com>
2022-09-23 14:42:35 +0100

    add usestate to incentivising screen

--- web/fluidity.money/src/screens/Incentivising/Incentivising.tsx
+++ web/fluidity.money/src/screens/Incentivising/Incentivising.tsx
@@ -5,6 +5,7 @@
 import { Display, Heading } from "@fluidity-money/surfing";
 import Video from "components/Video";
 import useViewport from "hooks/useViewport";
+import { useState } from "react";
 import { isFirefox } from "react-device-detect";
 import styles from "./Incentivising.module.scss";
 
