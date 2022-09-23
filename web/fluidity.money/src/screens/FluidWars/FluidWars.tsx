37bb4dfb6b9275d877836f8c8bc1becde35c7a87
harlexkhal <harlexibeh01@gmail.com>
2022-09-23 07:12:39 +0100

    fix and optimise fluid wars screen

--- web/fluidity.money/src/screens/FluidWars/FluidWars.tsx
+++ web/fluidity.money/src/screens/FluidWars/FluidWars.tsx
@@ -2,7 +2,7 @@
 // code is governed by a commercial license that can be found in the
 // LICENSE_TRF.md file.
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import HowItWorksTemplate from "../../components/HowItWorksTemplate";
 import { ReusableGrid } from "@fluidity-money/surfing";
@@ -14,22 +14,52 @@ const FluidWars = () => {
   const { width } = useViewport();
   const breakpoint = 860;
 
+  //for whatever happens that stops the video from playing like on ios due to powersaver turned on. - play a low res webp
+  //also for an awful network - play a low res webp while main vid loads
+  const [onHomeVidLoaded, setOnHomeVidLoaded] = useState(false);
+
   const left =
-    width > breakpoint ? (
+  width > breakpoint ? (
+    <>
+      <img
+      src="/assets/images/Animations/FluidityWars.webp"
+      style={{
+        position: "relative",
+        width: "200%",
+        display: `${onHomeVidLoaded === true ? "none" : "block"}`,
+      }}
+      alt="Fluidity Wars" />
       <Video
         src={'/assets/videos/FluidityFluidWars.mp4'}
         type={"fit"}
+        display= {!onHomeVidLoaded ? "none" : "inline"}
         loop={true}
+        onLoad={!onHomeVidLoaded ? () => setOnHomeVidLoaded(true) : () => { } }
         scale={2.0}
         margin={"0 0 0 270px"}
       />
-    ) : (
+    </>
+
+  ) : (
+    <>
+      <img
+        src="/assets/images/Animations/FluidityWars.webp"
+        style={{
+          position: "relative",
+          width: "100%",
+          display: `${onHomeVidLoaded === true ? "none" : "block"}`,
+        }}
+        alt="Fluidity Wars" />
+
       <Video
         src={'/assets/videos/FluidityFluidWars.mp4'}
         type={"fit"}
+        display= {!onHomeVidLoaded ? "none" : "inline"}
         loop={true}
-      />
-    );
+        onLoad={!onHomeVidLoaded ? () => setOnHomeVidLoaded(true) : () => { } }
+      />     
+    </>
+  );
 
   const right = 
   <HowItWorksTemplate header={header} info={info}>
