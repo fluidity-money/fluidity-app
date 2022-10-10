05da19024703bcd9dc5b7e52cbb0668cdded0d75
Connor <mussonconnor@gmail.com>
2022-10-10 10:25:12 -0600

    updated rewards gradient border to have a radius

--- web/app.fluidity.money/app/routes/$network/dashboard/rewards/common.tsx
+++ web/app.fluidity.money/app/routes/$network/dashboard/rewards/common.tsx
@@ -25,55 +25,63 @@ const UserRewards = ({ claimNow }: IUserRewards) => {
   return (
     <>
       {/* Info card*/}
-      <Card id="user-rewards" component="div" rounded={true} type={"holobox"}>
-        <section id="unclaimed-left">
-          {/* Icon */}
-          <img
-            id="logo"
-            src="/images/fluidTokensMetallicCropped.svg"
-            alt="tokens"
-            style={{ width: 200 }}
-          />
+      <Card
+        id="user-rewards"
+        className="card-outer"
+        component="div"
+        rounded={true}
+        type={"box"}
+      >
+        <div className="card-inner">
+          <section id="unclaimed-left">
+            {/* Icon */}
+            <img
+              id="logo"
+              src="/images/fluidTokensMetallicCropped.svg"
+              alt="tokens"
+              style={{ width: 200 }}
+            />
 
-          {/* Unclaimed fluid rewards */}
-          <section id="unclaimed">
-            <Text size="md">Unclaimed fluid rewards</Text>
-            <Display className="unclaimed-total" size="sm">
-              {numberToMonetaryString(6745)}
-            </Display>
-            <GeneralButton
-              size={"large"}
-              version={"primary"}
-              buttonType="text"
-              handleClick={onClick}
-              className="view-breakdown-button"
-            >
-              {buttonText}
-            </GeneralButton>
+            {/* Unclaimed fluid rewards */}
+            <section id="unclaimed">
+              <Text size="md">Unclaimed fluid rewards</Text>
+              <Display className="unclaimed-total" size="sm">
+                {numberToMonetaryString(6745)}
+              </Display>
+              <GeneralButton
+                size={"large"}
+                version={"primary"}
+                buttonType="text"
+                handleClick={onClick}
+                className="view-breakdown-button"
+              >
+                {buttonText}
+              </GeneralButton>
+            </section>
           </section>
-        </section>
 
-        {/* Auto-claims infobox */}
-        <section id="infobox">
-          <Heading className="claims-title" as="h5">
-            Auto-claims
-          </Heading>
-          <Text size="xs">{autoClaimInfo.join("\n")}</Text>
-          <hr className="gradient-line" />
-          <Heading className="claims-title" as="h5">
-            Instant-claim fees
-          </Heading>
-          <section className="fees">
-            <Text size="xs">Network fee</Text>
-            <Text size="xs">$0.002 FUSDC</Text>
+          {/* Auto-claims infobox */}
+          <section id="infobox">
+            <Heading className="claims-title" as="h5">
+              Auto-claims
+            </Heading>
+            <Text size="xs">{autoClaimInfo.join("\n")}</Text>
+            <hr className="gradient-line" />
+            <Heading className="claims-title" as="h5">
+              Instant-claim fees
+            </Heading>
+            <section className="fees">
+              <Text size="xs">Network fee</Text>
+              <Text size="xs">$0.002 FUSDC</Text>
+            </section>
+            <hr className="line" />
+            <section className="fees">
+              <Text size="xs">Gas fee</Text>
+              <Text size="xs">$0.002 FUSDC</Text>
+            </section>
+            <hr className="line" />
           </section>
-          <hr className="line" />
-          <section className="fees">
-            <Text size="xs">Gas fee</Text>
-            <Text size="xs">$0.002 FUSDC</Text>
-          </section>
-          <hr className="line" />
-        </section>
+        </div>
       </Card>
 
       {/* Total claimed, Reward History */}
