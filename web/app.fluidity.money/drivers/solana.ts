c80ccb9914123516797a4935ae2991f8ab3ae84a
harlexkhal <harlexibeh01@gmail.com>
2022-10-13 12:26:49 +0100

    solana drivers impl + code cleaning and refactoring

--- web/app.fluidity.money/drivers/solana.ts
+++ web/app.fluidity.money/drivers/solana.ts
@@ -7,6 +7,8 @@ import { PipedTransaction } from "./types";
 
 import config from "~/webapp.config.server";
 
+import { amountToDecimalString, shorthandAmountFormatter } from "../app/util";
+
 let LastSignature = ``;
 export const solGetTransactionsObservable = (
   token: string,
@@ -16,40 +18,50 @@ export const solGetTransactionsObservable = (
   new Observable<PipedTransaction>((subscriber) => {
     const SolanaConnection = new Connection(
       config.drivers["solana"][network].rpc.http,
-      "confirmed"
+      { wsEndpoint: config.drivers["solana"][network].rpc.ws }
     );
-
     SolanaConnection.onLogs(
       new PublicKey(address),
       (transactionLog) => {
-        //Because solana logs transaction twice :(
-        if (LastSignature === transactionLog.signature) return;
-
         SolanaConnection.getParsedTransaction(transactionLog.signature)
           .then((event) => {
-            const SrcPostBalance: any =
-              event?.meta?.postTokenBalances?.at(0)?.uiTokenAmount?.amount;
-            const SrcPreBalance: any =
-              event?.meta?.preTokenBalances?.at(0)?.uiTokenAmount?.amount;
+            // Because solana logs token transaction twice :(
+            if (LastSignature === transactionLog.signature) return;
 
-            const PostTokenBalance_Source = new BigNumber(SrcPostBalance, 10);
-            const PreTokenBalance_Source = new BigNumber(SrcPreBalance, 10);
+            const SrcPostBalance = String(
+              event?.meta?.postTokenBalances?.at(0)?.uiTokenAmount?.amount
+            );
+            const SrcPreBalance = String(
+              event?.meta?.preTokenBalances?.at(0)?.uiTokenAmount?.amount
+            );
 
-            const source: any = event?.meta?.postTokenBalances?.at(0)?.owner;
-            const destination: any =
-              event?.meta?.postTokenBalances?.at(1)?.owner;
-            const amount = new BigNumber(
-              PreTokenBalance_Source.sub(PostTokenBalance_Source)
-            ).toString(10);
-            const transaction: any = {
+            const PostTokenBalance_Source = new BigNumber(SrcPostBalance);
+            const PreTokenBalance_Source = new BigNumber(SrcPreBalance);
+            const TokenDecimal = Number(
+              event?.meta?.preTokenBalances?.at(0)?.uiTokenAmount?.decimals
+            );
+
+            const source = String(event?.meta?.postTokenBalances?.at(0)?.owner);
+            const destination = String(
+              event?.meta?.postTokenBalances?.at(1)?.owner
+            );
+            const amount = PreTokenBalance_Source.sub(PostTokenBalance_Source);
+
+            //Spam transaction: not possible for an account signer to transfer more amount of token than it owns.
+            if (amount.isNeg()) return;
+            if (source == `undefined` || destination == `undefined`) return;
+
+            const uiTokenAmount = amountToDecimalString(
+              amount.toString(10),
+              TokenDecimal
+            );
+            const transaction: PipedTransaction = {
               source,
               destination,
-              amount: amount,
+              amount: shorthandAmountFormatter(uiTokenAmount, 3),
               token,
             };
-            // console.log("Updated account info: ", transactionLog.signature)
             LastSignature = transactionLog.signature;
-
             subscriber.next(transaction);
           })
           .catch((error) => subscriber.error(error));
