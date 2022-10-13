0afbeeffdf31e0a602d579f6aa1c657ef468d952
harlexkhal <harlexibeh01@gmail.com>
2022-10-13 12:39:32 +0100

    reset app listening port back to 3000 -Mybad

--- web/app.fluidity.money/server.ts
+++ web/app.fluidity.money/server.ts
@@ -117,7 +117,7 @@ app.all(
       }
 );
 
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3000;
 
 httpServer.listen(port, () => {
   // require the built app so we're ready when the first request comes in
