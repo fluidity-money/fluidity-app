9b9e18d2fc2a72bc6f5d8af1a205a289e0736e7c
elle <elle@catgirl.software>
2022-10-14 09:28:38 +1030

    Revert "make web backends receive all messages"
    This reverts commit 281008091a0ef53b4f5a6d23e568d5b9ac70bdfa.

--- lib/queue/queue.go
+++ lib/queue/queue.go
@@ -61,21 +61,13 @@ func (message Message) Decode(decoded interface{}) {
 	log.Debugf("Successfully decoded a message from JSON!")
 }
 
-func GetMessages(topic string, f func(message Message)) {
-	getMessagesInternal(false, topic, f)
-}
-
-func GetBroadcastMessages(topic string, f func(message Message)) {
-	getMessagesInternal(true, topic, f)
-}
-
 // GetMessages from the AMQP server, calling the function each time a
 // message is received. If newTopic is set to true, a new queue name
 // will be generated that will make this useful for receiving messages
 // that would be received over broadcast that is unique to this worker.
 // If it is set to false, every message that would be received is
 // distributed across any worker sharing the same worker id.
-func getMessagesInternal(newTopic bool, topic string, f func(message Message)) {
+func GetMessages(topic string, f func(message Message)) {
 
 	amqpDetails := <-chanAmqpDetails
 
@@ -91,10 +83,6 @@ func getMessagesInternal(newTopic bool, topic string, f func(message Message)) {
 		consumerId = generateRandomConsumerId(workerId)
 		queueName  = fmt.Sprintf("%v.%v", topic, workerId)
 	)
-
-	if newTopic {
-		queueName = fmt.Sprintf("%v.%v", queueName, consumerId)
-	}
 
 	messages, err := queueConsume(
 		queueName,
