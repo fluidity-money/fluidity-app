1d70b19da7fd967c745bebb276a25210a10f99fc
harlexkhal <harlexibeh01@gmail.com>
2022-10-15 13:00:33 +0100

    implenment tool tip ui notification pop example + test

--- web/app.fluidity.money/app/routes/index.tsx
+++ web/app.fluidity.money/app/routes/index.tsx
@@ -1,6 +1,11 @@
 import { Display, Text } from "@fluidity-money/surfing";
+import { useToolTip } from "~/components";
 
 export default function IndexPage() {
+  const toolTip = useToolTip();
+  const showToast = () =>
+    toolTip.open(`Receive 10fusdc from 0x494..`);
+
   return (
     <div>
       <Display>
@@ -14,6 +19,11 @@ export default function IndexPage() {
 
       <Text size="xxl">Connect your wallet to see what you could make.</Text>
       <button>Make it rain</button>
+
+      <button style={{ backgroundColor: 'blue', marginLeft:"10px", padding: "20px"}} onClick={showToast}>
+        Pop Notification Demo
+      </button>
+
     </div>
   );
 }
