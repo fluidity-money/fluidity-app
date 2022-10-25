9d5afff4481a236dc88c5b97581edee3650c5727
harlexkhal <harlexibeh01@gmail.com>
2022-10-26 00:33:30 +0100

    configure loader to access web app config + test

--- web/app.fluidity.money/app/routes/index.tsx
+++ web/app.fluidity.money/app/routes/index.tsx
@@ -1,10 +1,16 @@
 import { Display, Text } from "@fluidity-money/surfing";
+
+import config from "../webapp.config";
+import { io } from "socket.io-client";
+import { PipedTransaction } from "drivers/types";
+
 import { useToolTip } from "~/components";
 import { ToolTipContent } from "~/components/ToolTip";
 
 export default function IndexPage() {
   const toolTip = useToolTip();
-  const showNotification = () =>
+  const showNotification = () => {
+    // This just used to test ui
     toolTip.open(
       `#0000ff`,
       <ToolTipContent
@@ -15,7 +21,63 @@ export default function IndexPage() {
         linkUrl={"#"}
       />
     );
+  }
+    
+    
+  const socket = io();
+  
+  const connected_wallet = "Ez2zVjw85tZan1ycnJ5PywNNxR6Gm4jbXQtZKyQNu3Lv"
+  
+  socket.emit("subscribeTransactions", {
+    protocol: `solana`,
+    address: connected_wallet,
+  });
 
+  socket.on("Transactions", (log :PipedTransaction) => {
+
+    let tokenProps = config.tokens;
+
+    let bgColour :string;
+    let tokenLogo :string;
+
+    switch(log.token){
+      case `fUSDC`: 
+        bgColour = tokenProps.colours.fUSDC;
+        tokenLogo = tokenProps.logos.fUSDC;
+        break;
+      case `fUSDT`: 
+        bgColour = config.tokens.colours.fUSDT;
+        tokenLogo = tokenProps.logos.fUSDC;
+        break;
+      case `fTUSD`: 
+        bgColour = tokenProps.colours.fTUSD;
+        tokenLogo = tokenProps.logos.fTUSD;
+        break;
+      case `fDAI`: 
+        bgColour = tokenProps.colours.fDAI;
+        tokenLogo = tokenProps.logos.fDAI;
+        break;
+      case `fFRAX`: 
+        bgColour = tokenProps.colours.fFRAX;
+        tokenLogo = tokenProps.logos.fFRAX;
+        break;
+      default:
+        bgColour = `fffffff`
+        tokenLogo = ``;
+    }
+
+    toolTip.open(
+      bgColour
+      ,
+      <ToolTipContent
+        tokenLogoSrc={tokenLogo}
+        boldTitle={log.amount + ` ` + log.token}
+        details={log.source === connected_wallet ? `Sent to ` + log.destination : `Received from ` + log.source}
+        linkLabel={"ASSETS"}
+        linkUrl={"#"}
+      />
+    )
+  });
   return (
     <div>
       <Display>
