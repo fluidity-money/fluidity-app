84c4af4ff5ae7aa121a4bc2789c44765743ae571
eli-d <64763513+eli-d@users.noreply.github.com>
2022-10-27 14:46:45 +1030

    use highest reward query in dashboard

--- web/app.fluidity.money/app/queries/useHighestRewardStatistics.ts
+++ web/app.fluidity.money/app/queries/useHighestRewardStatistics.ts
@@ -5,15 +5,15 @@ import { jsonPost } from "~/util/api/rpc";
 const query: Queryable = {
   ethereum: gql`
     query HighestRewards($network: network_blockchain!) {
-      highest_rewards_monthly(where:{network:{_eq:{$network}}}) {
+      highest_rewards_monthly(where: {network: {_eq: $network}}) {
         network
         transaction_hash
         winning_address
-        awarded_day 
+        awarded_time
         token_short_name
         winning_amount_scaled
       }
-      highest_reward_winner_totals(where:{network:{_eq:{$network}}}) {
+      highest_reward_winner_totals(where: {network: {_eq: $network}}) {
         transaction_count
         winning_address
         total_winnings
@@ -30,7 +30,7 @@ type HighestRewardBody = {
   query: string;
 };
 
-type HighestRewardResponse = {
+export type HighestRewardResponse = {
   data: {
     highest_rewards_monthly: Array<{
       network: string
@@ -46,6 +46,7 @@ type HighestRewardResponse = {
       total_winnings: number
     }>
   }
+  error?: string
 };
 
 const useHighestRewardStatistics = async (network: string) => {
