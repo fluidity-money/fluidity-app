acbf61cfcaefe44f9633481ed384c7214c935dcd
harlexkhal <harlexibeh01@gmail.com>
2022-11-02 22:19:04 +0100

    make environment variable use same naming as configurations in config.toml

--- web/app.fluidity.money/app/routes/$network.tsx
+++ web/app.fluidity.money/app/routes/$network.tsx
@@ -39,8 +39,8 @@ export const loader: LoaderFunction = async ({ params }) => {
   // Prevent unknown network params
   const { network } = params;
   
-  const solanaRpcUrl = process.env.REACT_APP_FLU_SOL_RPC;
-  const ethereumRpcUrl = process.env.REACT_APP_FLU_SOL_RPC;
+  const solanaRpcUrl = process.env.FLU_ETH_RPC_HTTP;
+  const ethereumRpcUrl = process.env.FLU_SOL_RPC_HTTP;
 
   const redirectTarget = redirect("/");
 
