b682e7caca698d3a77f930b3954c68f120b8d3ee
glo12345 <glorison@fluidity.money>
2022-11-10 16:45:56 +1100

    feat: added padding to blockchain modal

--- web/app.fluidity.money/app/util/api/rpc.ts
+++ web/app.fluidity.money/app/util/api/rpc.ts
@@ -1,7 +1,12 @@
-const jsonPost = async <Req, Res>(url: string, body: Req): Promise<Res> => {
+const jsonPost = async <Req, Res>(
+  url: string,
+  body: Req,
+  headers?: { [key: string]: string }
+): Promise<Res> => {
   const res = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
+      ...(headers ? headers : {}),
     },
     method: "POST",
     body: JSON.stringify(body),
