4d1af2fe41cc3ab8ef9f2d123cc481e5de67f358
eli-d <64763513+eli-d@users.noreply.github.com>
2022-11-17 16:58:13 +1030

    combine `ALTER TABLE` statements

--- database/101-up-postgres/107-solana/004-per-token-limits.sql
+++ database/101-up-postgres/107-solana/004-per-token-limits.sql
@@ -5,24 +5,17 @@
 DELETE FROM solana_users;
 
 ALTER TABLE solana_users
-	ADD COLUMN token_short_name VARCHAR NOT NULL;		
-
+	ADD COLUMN token_short_name VARCHAR NOT NULL,		
 
 -- be unique on address and token, so remove address uniqueness
 -- in favour of combined primary key
-ALTER TABLE solana_users
-	ADD CONSTRAINT unique_address_token PRIMARY KEY (address, token_short_name);
-
-ALTER TABLE solana_users
+	ADD CONSTRAINT unique_address_token PRIMARY KEY (address, token_short_name),
 	DROP CONSTRAINT solana_users_address_key;
-	
 
 -- migrate:down
 
 ALTER TABLE solana_users
-	DROP CONSTRAINT unique_address_token;
-
-ALTER TABLE solana_users
+	DROP CONSTRAINT unique_address_token,
 	DROP COLUMN token_short_name;		
 
 -- aggregate all tokens into one
