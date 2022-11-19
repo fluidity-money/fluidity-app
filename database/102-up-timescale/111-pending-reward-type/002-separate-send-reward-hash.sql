4947ba3dacf62484d790ed2a4bc03b9c859597f8
eli-d <64763513+eli-d@users.noreply.github.com>
2022-11-19 16:17:13 +1030

    fix migration

--- database/102-up-timescale/111-pending-reward-type/002-separate-send-reward-hash.sql
+++ database/102-up-timescale/111-pending-reward-type/002-separate-send-reward-hash.sql
@@ -5,16 +5,19 @@
 -- track-winners knows about reward txn hash
 -- therefore, use separate columns to save both send/receive direction as well as the necessary transaction hashes
 
-ALTER TABLE ethereum_pending_reward_type (
-    RENAME COLUMN transaction_hash TO send_transaction_hash,
+ALTER TABLE ethereum_pending_reward_type
+    RENAME COLUMN transaction_hash TO send_transaction_hash;
+
+ALTER TABLE ethereum_pending_reward_type 
     ALTER COLUMN send_transaction_hash DROP NOT NULL,
-    ADD COLUMN reward_transaction_hash
-);
+    ADD COLUMN reward_transaction_hash VARCHAR;
 
 -- migrate:down
 
-ALTER TABLE ethereum_pending_reward_type (
-    RENAME COLUMN send_transaction_hash TO transaction_hash,
+ALTER TABLE ethereum_pending_reward_type
+    RENAME COLUMN send_transaction_hash TO transaction_hash;
+
+ALTER TABLE ethereum_pending_reward_type
     ALTER COLUMN transaction_hash SET NOT NULL,
-    DROP COLUMN reward_transaction_hash
-);
+    DROP COLUMN reward_transaction_hash;
+
