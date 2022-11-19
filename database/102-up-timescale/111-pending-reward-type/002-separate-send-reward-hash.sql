c488474cda9ccd5ee3a6c8f944f1cd7ca5fd47ad
eli-d <64763513+eli-d@users.noreply.github.com>
2022-11-19 16:01:47 +1030

    make reward txn hash visible in pending reward types table

---
+++ database/102-up-timescale/111-pending-reward-type/002-separate-send-reward-hash.sql
@@ -0,0 +1,20 @@
+-- migrate:up
+
+-- spooler knows about send txn hash
+-- sender knows about send and reward txn hash
+-- track-winners knows about reward txn hash
+-- therefore, use separate columns to save both send/receive direction as well as the necessary transaction hashes
+
+ALTER TABLE ethereum_pending_reward_type (
+    RENAME COLUMN transaction_hash TO send_transaction_hash,
+    ALTER COLUMN send_transaction_hash DROP NOT NULL,
+    ADD COLUMN reward_transaction_hash
+);
+
+-- migrate:down
+
+ALTER TABLE ethereum_pending_reward_type (
+    RENAME COLUMN send_transaction_hash TO transaction_hash,
+    ALTER COLUMN transaction_hash SET NOT NULL,
+    DROP COLUMN reward_transaction_hash
+);
