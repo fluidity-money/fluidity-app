ca2b779d56c671c836f26936c28818790c0fb216
eli-d <64763513+eli-d@users.noreply.github.com>
2022-11-25 15:26:42 +1030

    update providers to include all supported applications

--- web/app.fluidity.money/app/components/ProviderIcon/index.tsx
+++ web/app.fluidity.money/app/components/ProviderIcon/index.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "@remix-run/react";
+import {Application} from "~/queries/useApplicationRewardStatistics";
 import config from "~/webapp.config.server";
 
 export type Providers =
@@ -17,7 +18,44 @@ export type Providers =
   | "Solana"
   | "Solend"
   | "Uniswap"
-  | "Sushiswap";
+  | "Sushiswap"
+  | "Fluidity"
+  | "Balancer"
+  | "Oneinch"
+  | "Mooniswap"
+  | "Curve"
+  | "Multichain"
+  | "XY Finance"
+  | "Raydium"
+  | "Lifinity"
+  | "Mercurial"
+
+const providerNames: {[K in Application]: Providers} = {
+  none: "Fluidity",
+  uniswap_v2: "Uniswap",
+  balancer_v2: "Balancer",
+  oneinch_v1: "Oneinch",
+  oneinch_v2: "Oneinch",
+  oneinch_fixedrate: "Oneinch",
+  mooniswap: "Mooniswap",
+  dodo_v2: "Dodo",
+  curve: "Curve",
+  multichain: "Multichain",
+  xy_finance: "XY Finance",
+
+  spl: "Fluidity",
+  saber: "Saber",
+  orca: "Orca",
+  raydium: "Raydium",
+  aldrinv1: "Aldrin",
+  aldrinv2: "Aldrin",
+  lifinity: "Lifinity",
+  mercurial: "Mercurial",
+}
+
+export const providerToDisplayName = (name: Application) => {
+  return providerNames[name];
+}
 
 type IProviderIcon = {
   provider: Providers;
