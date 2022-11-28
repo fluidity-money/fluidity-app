be61c61587d5548f3c845499a1084e4a17a3e8e3
Alfie Edgeworth <0xalfie@gmail.com>
2022-11-29 10:14:07 +1030

    Improved style for guard clauses.

--- web/fluidity.money/src/utils/gtag.ts
+++ web/fluidity.money/src/utils/gtag.ts
@@ -2,6 +2,7 @@ export const GA_TRACKING_ID='G-EF68MNJRJ7'
 
 export const pageview = (url: URL) => {
   if (typeof window.gtag === 'undefined') return
+  
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url
   })
@@ -16,6 +17,7 @@ type GTagEvent = {
 
 export const event = ({ action, category, label, value }: GTagEvent) => {
   if (typeof window.gtag === 'undefined') return
+
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
