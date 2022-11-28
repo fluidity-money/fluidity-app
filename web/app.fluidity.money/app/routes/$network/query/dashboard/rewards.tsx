fbb659407c540daabb7794ee52b6b45fae449631
Joshua Steele <jsteele@nitrocade.com>
2022-11-28 18:08:22 +1030

    Fix a few errors

--- web/app.fluidity.money/app/routes/$network/query/dashboard/rewards.tsx
+++ web/app.fluidity.money/app/routes/$network/query/dashboard/rewards.tsx
@@ -1,6 +1,5 @@
 import type { Winner } from "~/queries/useUserRewards";
 import type { UserTransaction } from "~/routes/$network/query/userTransactions";
-import useGlobalRewardStatistics from "~/queries/useGlobalRewardStatistics";
 import { JsonRpcProvider } from "@ethersproject/providers";
 import RewardAbi from "~/util/chainUtils/ethereum/RewardPool.json";
 import { getTotalPrizePool } from "~/util/chainUtils/ethereum/transaction";
@@ -9,166 +8,134 @@ import { useUserRewardsAll } from "~/queries";
 import config from "~/webapp.config.server";
 import Transaction from "~/types/Transaction";
 import { Provider } from "~/components/ProviderCard";
+import useApplicationRewardStatistics from "~/queries/useApplicationRewardStatistics";
+import { aggregateRewards } from "~/util/rewardAggregates";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const network = params.network ?? "";
-  const fluidPairs = config.config[network ?? ""].fluidAssets.length;
-
-  const networkFee = 0.002;
-  const gasFee = 0.002;
-
-  const url = new URL(request.url);
-  const _pageStr = url.searchParams.get("page");
-  const _pageUnsafe = _pageStr ? parseInt(_pageStr) : 1;
-  const page = _pageUnsafe > 0 ? _pageUnsafe : 1;
-
-  try {
-    const mainnetId = 0;
-    const infuraRpc = config.drivers["ethereum"][mainnetId].rpc.http;
-
-    const provider = new JsonRpcProvider(infuraRpc);
-
-    const rewardPoolAddr = "0xD3E24D732748288ad7e016f93B1dc4F909Af1ba0";
-
-    const totalPrizePool = await getTotalPrizePool(
-      provider,
-      rewardPoolAddr,
-      RewardAbi
-    );
-
-    const {
-      transactions,
-      count,
-    }: { transactions: UserTransaction[]; count: number } = await (
-      await fetch(
-        `${url.origin}/${network}/query/userTransactions?page=${page}`
-      )
-    ).json();
-
-    const { data, errors } = await useUserRewardsAll(network ?? "");
-
-    if (errors || !data) {
-      throw errors;
-    }
-
-    const winnersMap = data.winners.reduce(
-      (map, winner) => ({
-        ...map,
-        [winner.transaction_hash]: {
-          ...winner,
-        },
-      }),
-      {} as { [key: string]: Winner }
-    );
-
-    const {
-      config: {
-        [network as string]: { tokens },
-      },
-    } = config;
-
-    const fluidTokenMap = tokens.reduce(
-      (map, token) =>
-        token.isFluidOf
-          ? {
-              ...map,
-              [token.symbol]: token.address,
-              [token.symbol.slice(1)]: token.address,
-            }
-          : map,
-      {}
-    );
-
-    const tokenLogoMap = tokens.reduce(
-      (map, token) => ({
-        ...map,
-        [token.symbol]: token.logo,
-      }),
-      {} as Record<string, string>
-    );
-
-    const defaultLogo = "/assets/tokens/usdt.svg";
-
-    const mergedTransactions: Transaction[] =
-      transactions
-        ?.filter((tx) => !!winnersMap[tx.hash])
-        .map((tx) => {
-          const winner = winnersMap[tx.hash];
-
-          return {
-            sender: tx.sender,
-            receiver: tx.receiver,
-            winner: winner.winning_address ?? "",
-            reward: winner
-              ? winner.winning_amount / 10 ** winner.token_decimals
-              : 0,
-            hash: tx.hash,
-            currency: tx.currency,
-            value:
-              tx.currency === "DAI" || tx.currency === "fDAI"
-                ? tx.value / 10 ** 12
-                : tx.value,
-            timestamp: tx.timestamp * 1000,
-            logo: tokenLogoMap[tx.currency] || defaultLogo,
-            provider:
-              (network === "ethereum"
-                ? winner.ethereum_application
-                : winner.solana_application) ?? "Fluidity",
-          };
-        }) ?? [];
-
-    const totalRewards = mergedTransactions.reduce(
-      (sum, { reward }) => sum + reward,
-      0
-    );
-
-    const { data: rewardData, errors: rewardErrors } =
-      await useGlobalRewardStatistics(network ?? "");
-
-    if (rewardErrors || !rewardData) {
-      throw errors;
-    }
-
-    // group rewaObject.values(totalTransactions
-    const totalRewarders = Object.values(
-      mergedTransactions.reduce((map, tx) => {
-        const provider = map[tx.provider];
-
-        return {
-          ...map,
-          [tx.provider]: provider
-            ? {
-                ...provider,
-                count: provider.count + 1,
-                prize: provider.prize + tx.reward,
-              }
-            : {
-                name: tx.provider,
-                count: 1,
-                prize: tx.reward,
-              },
-        };
-      }, {} as { [providerName: string]: { name: string; count: number; prize: number } })
-    )
-      .map(({ count, ...provider }) => ({
-        ...provider,
-        avgPrize: provider.prize / count,
-      }))
-      .sort(({ avgPrize: avgPrizeA }, { avgPrize: avgPrizeB }) =>
-        avgPrizeA > avgPrizeB ? 1 : avgPrizeA === avgPrizeB ? 0 : -1
-      ) as Provider[];
-
-    // Find highest Weekly Rewarder
-    const unixNow = Date.now();
-
-    const highestWeeklyRewarders = Object.values(
-      mergedTransactions
-        .filter(
-          ({ timestamp }) => timestamp >= unixNow - 7 * 24 * 60 * 60 * 1000
+    const network = params.network ?? "";
+    const icons = config.provider_icons;
+    const fluidPairs = config.config[network ?? ""].fluidAssets.length;
+ 
+    const networkFee = 0.002;
+    const gasFee = 0.002;
+ 
+    const url = new URL(request.url);
+    const _pageStr = url.searchParams.get("page");
+    const _pageUnsafe = _pageStr ? parseInt(_pageStr) : 1;
+    const page = _pageUnsafe > 0 ? _pageUnsafe : 1;
+ 
+    try {
+      const mainnetId = 0;
+      const infuraRpc = config.drivers["ethereum"][mainnetId].rpc.http;
+ 
+      const provider = new JsonRpcProvider(infuraRpc);
+ 
+      const rewardPoolAddr = "0xD3E24D732748288ad7e016f93B1dc4F909Af1ba0";
+ 
+      const totalPrizePool = await getTotalPrizePool(
+        provider,
+        rewardPoolAddr,
+        RewardAbi
+      );
+ 
+      const {
+        transactions,
+        count,
+      }: { transactions: UserTransaction[]; count: number } = await (
+        await fetch(
+          `${url.origin}/${network}/query/userTransactions?page=${page}`
         )
-        .reduce((map, tx) => {
+      ).json();
+ 
+      const { data, errors } = await useUserRewardsAll(network ?? "");
+ 
+      if (errors || !data) {
+        throw errors;
+      }
+ 
+      const winnersMap = data.winners.reduce(
+        (map, winner) => ({
+          ...map,
+          [winner.transaction_hash]: {
+            ...winner,
+          },
+        }),
+        {} as { [key: string]: Winner }
+      );
+ 
+      const {
+        config: {
+          [network as string]: { tokens },
+        },
+      } = config;
+ 
+      const fluidTokenMap = tokens.reduce(
+        (map, token) =>
+          token.isFluidOf
+            ? {
+                ...map,
+                [token.symbol]: token.address,
+                [token.symbol.slice(1)]: token.address,
+              }
+            : map,
+        {}
+      );
+ 
+      const tokenLogoMap = tokens.reduce(
+        (map, token) => ({
+          ...map,
+          [token.symbol]: token.logo,
+        }),
+        {} as Record<string, string>
+      );
+ 
+      const defaultLogo = "/assets/tokens/usdt.svg";
+ 
+      const mergedTransactions: Transaction[] =
+        transactions
+          ?.filter((tx) => !!winnersMap[tx.hash])
+          .map((tx) => {
+            const winner = winnersMap[tx.hash];
+ 
+            return {
+              sender: tx.sender,
+              receiver: tx.receiver,
+              winner: winner.winning_address ?? "",
+              reward: winner
+                ? winner.winning_amount / 10 ** winner.token_decimals
+                : 0,
+              hash: tx.hash,
+              currency: tx.currency,
+              value:
+                tx.currency === "DAI" || tx.currency === "fDAI"
+                  ? tx.value / 10 ** 12
+                  : tx.value,
+              timestamp: tx.timestamp * 1000,
+              logo: tokenLogoMap[tx.currency] || defaultLogo,
+              provider:
+                (network === "ethereum"
+                  ? winner.ethereum_application
+                  : winner.solana_application) ?? "Fluidity",
+            };
+          }) ?? [];
+ 
+      const totalRewards = mergedTransactions.reduce(
+        (sum, { reward }) => sum + reward,
+        0
+      );
+ 
+      const { data: rewardData, errors: rewardErrors } =
+        await useApplicationRewardStatistics(network ?? "");
+      if (rewardErrors || !rewardData) {
+        throw errors;
+      }
+ 
+      const rewarders = aggregateRewards(rewardData);
+ 
+      const totalRewarders = Object.values(
+        mergedTransactions.reduce((map, tx) => {
           const provider = map[tx.provider];
-
+ 
           return {
             ...map,
             [tx.provider]: provider
@@ -184,38 +151,33 @@ export const loader: LoaderFunction = async ({ request, params }) => {
                 },
           };
         }, {} as { [providerName: string]: { name: string; count: number; prize: number } })
-    )
-      .map(({ count, ...provider }) => ({
-        ...provider,
-        avgPrize: provider.prize / count,
-      }))
-      .sort(({ avgPrize: avgPrizeA }, { avgPrize: avgPrizeB }) =>
-        avgPrizeA > avgPrizeB ? -1 : avgPrizeA === avgPrizeB ? 0 : 1
-      );
-
-    const highestWeeklyRewarder = highestWeeklyRewarders.length
-      ? highestWeeklyRewarders[0]
-      : undefined;
-
-    const timestamp = new Date().getTime();
-
-    return json({
-      fluidTokenMap,
-      highestWeeklyRewarder,
-      page,
-      network,
-      fluidPairs,
-      totalTransactions: mergedTransactions,
-      totalCount: count,
-      totalRewards,
-      totalRewarders,
-      totalPrizePool,
-      networkFee,
-      gasFee,
-      timestamp,
-    });
-  } catch (err) {
-    console.log(err);
-    throw new Error(`Could not fetch Rewards on ${network}: ${err}`);
-  } // Fail silently - for now.
-};
+      )
+        .map(({ count, ...provider }) => ({
+          ...provider,
+          avgPrize: provider.prize / count,
+        }))
+        .sort(({ avgPrize: avgPrizeA }, { avgPrize: avgPrizeB }) =>
+          avgPrizeA > avgPrizeB ? 1 : avgPrizeA === avgPrizeB ? 0 : -1
+        ) as Provider[];
+ 
+      return json({
+        icons,
+        rewarders,
+        fluidTokenMap,
+        page,
+        network,
+        fluidPairs,
+        totalTransactions: mergedTransactions,
+        totalCount: count,
+        totalRewards,
+        totalRewarders,
+        totalPrizePool,
+        networkFee,
+        gasFee,
+        timestamp: new Date().getTime(),
+      });
+    } catch (err) {
+      console.log(err);
+      throw new Error(`Could not fetch Rewards on ${network}: ${err}`);
+    } // Fail silently - for now.
+  };
--- web/app.fluidity.money/app/routes/$network/query/fluidify.tsx
+++ web/app.fluidity.money/app/routes/$network/query/fluidify.tsx
@@ -1,184 +1,111 @@
-import {
-    getTotalPrizePool,
-} from "~/util/chainUtils/ethereum/transaction";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import config from "~/webapp.config.server";
+import config, { colors } from "~/webapp.config.server";
 import { json, LoaderFunction } from "@remix-run/node";
-import useApplicationRewardStatistics from "~/queries/useApplicationRewardStatistics";
-import { aggregateRewards } from "~/util/rewardAggregates";
-import Transaction from "~/types/Transaction";
-import { useUserRewardsAll } from "~/queries";
-import { UserTransaction } from "~/queries/useUserTransactions";
-import RewardAbi from "~/util/chainUtils/ethereum/RewardPool.json";
-import { Winner } from "~/queries/useUserRewards";
-import { Provider } from "~/components/ProviderCard";
+import AugmentedToken from "~/types/AugmentedToken";
+import { getUsdUserMintLimit, userMintLimitedEnabled } from "~/util/chainUtils/ethereum/transaction";
+import tokenAbi from "~/util/chainUtils/ethereum/Token.json";
 
-export const loader: LoaderFunction = async ({ request, params }) => {
-    const network = params.network ?? "";
-    const icons = config.provider_icons;
-    const fluidPairs = config.config[network ?? ""].fluidAssets.length;
+export const loader: LoaderFunction = async ({ params }) => {
+    const { network } = params;
  
-    const networkFee = 0.002;
-    const gasFee = 0.002;
+    if (!network) throw new Error("Network not found");
  
-    const url = new URL(request.url);
-    const _pageStr = url.searchParams.get("page");
-    const _pageUnsafe = _pageStr ? parseInt(_pageStr) : 1;
-    const page = _pageUnsafe > 0 ? _pageUnsafe : 1;
+    const ethereumWallets = config.config["ethereum"].wallets;
  
-    try {
+    const {
+      config: {
+        [network as string]: { tokens },
+      },
+    } = config;
+ 
+    if (network === "ethereum") {
       const mainnetId = 0;
-      const infuraRpc = config.drivers["ethereum"][mainnetId].rpc.http;
- 
-      const provider = new JsonRpcProvider(infuraRpc);
- 
-      const rewardPoolAddr = "0xD3E24D732748288ad7e016f93B1dc4F909Af1ba0";
- 
-      const totalPrizePool = await getTotalPrizePool(
-        provider,
-        rewardPoolAddr,
-        RewardAbi
-      );
  
       const {
-        transactions,
-        count,
-      }: { transactions: UserTransaction[]; count: number } = await (
-        await fetch(
-          `${url.origin}/${network}/query/userTransactions?page=${page}`
-        )
-      ).json();
- 
-      const { data, errors } = await useUserRewardsAll(network ?? "");
- 
-      if (errors || !data) {
-        throw errors;
-      }
- 
-      const winnersMap = data.winners.reduce(
-        (map, winner) => ({
-          ...map,
-          [winner.transaction_hash]: {
-            ...winner,
+        drivers: {
+          ethereum: {
+            [mainnetId]: {
+              rpc: { http: infuraUri },
+            },
           },
-        }),
-        {} as { [key: string]: Winner }
-      );
- 
-      const {
-        config: {
-          [network as string]: { tokens },
         },
       } = config;
  
-      const fluidTokenMap = tokens.reduce(
-        (map, token) =>
-          token.isFluidOf
-            ? {
-                ...map,
-                [token.symbol]: token.address,
-                [token.symbol.slice(1)]: token.address,
-              }
-            : map,
-        {}
-      );
+      const provider = new JsonRpcProvider(infuraUri);
  
-      const tokenLogoMap = tokens.reduce(
-        (map, token) => ({
-          ...map,
-          [token.symbol]: token.logo,
-        }),
-        {} as Record<string, string>
-      );
+      const augmentedTokens: AugmentedToken[] = await Promise.all(
+        tokens.map(async (token) => {
+          const { isFluidOf, address } = token;
  
-      const defaultLogo = "/assets/tokens/usdt.svg";
- 
-      const mergedTransactions: Transaction[] =
-        transactions
-          ?.filter((tx) => !!winnersMap[tx.transaction.hash])
-          .map((tx) => {
-            const winner = winnersMap[tx.transaction.hash];
- 
+          // Reverting has no mint limits
+          if (isFluidOf) {
             return {
-              sender: tx.sender.address,
-              receiver: tx.receiver.address,
-              winner: winner.winning_address ?? "",
-              reward: winner
-                ? winner.winning_amount / 10 ** winner.token_decimals
-                : 0,
-              hash: tx.transaction.hash,
-              currency: tx.currency.symbol,
-              value:
-                tx.currency.symbol === "DAI" || tx.currency.symbol === "fDAI"
-                  ? tx.amount / 10 ** 12
-                  : tx.amount,
-              timestamp: tx.block.timestamp.unixtime * 1000,
-              logo: tokenLogoMap[tx.currency.symbol] || defaultLogo,
-              provider:
-                (network === "ethereum"
-                  ? winner.ethereum_application
-                  : winner.solana_application) ?? "Fluidity",
+              ...token,
+              userMintLimit: undefined,
+              userTokenBalance: 0,
             };
-          }) ?? [];
+          }
  
-      const totalRewards = mergedTransactions.reduce(
-        (sum, { reward }) => sum + reward,
-        0
-      );
+          const fluidPair = tokens.find(({ isFluidOf }) => isFluidOf === address);
  
-      const { data: rewardData, errors: rewardErrors } =
-        await useApplicationRewardStatistics(network ?? "");
-      if (rewardErrors || !rewardData) {
-        throw errors;
-      }
+          if (!fluidPair)
+            throw new Error(`Could not find fluid Pair of ${token.name}`);
  
-      const rewarders = aggregateRewards(rewardData);
+          const mintLimitEnabled = await userMintLimitedEnabled(
+            provider,
+            fluidPair.address,
+            tokenAbi
+          );
  
-      const totalRewarders = Object.values(
-        mergedTransactions.reduce((map, tx) => {
-          const provider = map[tx.provider];
+          const userMintLimit = mintLimitEnabled
+            ? await getUsdUserMintLimit(provider, fluidPair.address, tokenAbi)
+            : undefined;
  
           return {
-            ...map,
-            [tx.provider]: provider
-              ? {
-                  ...provider,
-                  count: provider.count + 1,
-                  prize: provider.prize + tx.reward,
-                }
-              : {
-                  name: tx.provider,
-                  count: 1,
-                  prize: tx.reward,
-                },
+            ...token,
+            userMintLimit: userMintLimit,
+            userTokenBalance: 0,
           };
-        }, {} as { [providerName: string]: { name: string; count: number; prize: number } })
-      )
-        .map(({ count, ...provider }) => ({
-          ...provider,
-          avgPrize: provider.prize / count,
-        }))
-        .sort(({ avgPrize: avgPrizeA }, { avgPrize: avgPrizeB }) =>
-          avgPrizeA > avgPrizeB ? 1 : avgPrizeA === avgPrizeB ? 0 : -1
-        ) as Provider[];
+        })
+      );
  
       return json({
-        icons,
-        rewarders,
-        fluidTokenMap,
-        page,
         network,
-        fluidPairs,
-        totalTransactions: mergedTransactions,
-        totalCount: count,
-        totalRewards,
-        totalRewarders,
-        totalPrizePool,
-        networkFee,
-        gasFee,
+        tokens: augmentedTokens,
+        ethereumWallets,
+        colors: (await colors)[network as string],
       });
-    } catch (err) {
-      console.log(err);
-      throw new Error(`Could not fetch Rewards on ${network}: ${err}`);
-    } // Fail silently - for now.
+    }
+ 
+    // Network === "solana"
+    const augmentedTokens = await Promise.all(
+      tokens.map(async (token) => {
+        const { isFluidOf } = token;
+        // const { name, symbol } = token;
+ 
+        const mintLimit = isFluidOf
+          ? // ?await userMintLimit(name)
+            undefined
+          : undefined;
+ 
+        const tokensMinted = mintLimit
+          ? // ?await userAmountMinted(symbol)
+            0
+          : 0;
+ 
+        return {
+          ...token,
+          userMintLimit: mintLimit,
+          userMintedAmt: tokensMinted,
+          userTokenBalance: 0,
+        };
+      })
+    );
+ 
+    return json({
+      network,
+      tokens: augmentedTokens,
+      ethereumWallets,
+      colors: (await colors)[network as string],
+    });
   };
