8fe2badfcd0289e523bdd31b22f7901159999152
elle <elle@catgirl.software>
2022-12-08 10:34:32 +1030

    fix syntax in migration

--- database/102-up-timescale/107-worker/017-update-atx-index.sql
+++ database/102-up-timescale/107-worker/017-update-atx-index.sql
@@ -2,7 +2,7 @@
 
 -- recreate worker_buffered_atx as a new index on token details
 
-DROP INDEX atx_blocknum ON worker_buffered_atx;
+DROP INDEX IF EXISTS atx_blocknum;
 
 CREATE INDEX atx_blocks_by_token ON worker_buffered_atx (network, token_short_name, block_number DESC);
 
@@ -10,4 +10,4 @@ CREATE INDEX atx_blocks_by_token ON worker_buffered_atx (network, token_short_na
 
 CREATE INDEX atx_blocknum ON worker_buffered_atx (block_number DESC);
 
-DROP INDEX atx_blocks_by_token;
+DROP INDEX IF EXISTS atx_blocks_by_token;
