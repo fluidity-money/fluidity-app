b8083002ac25c33f2be46375e06d732fddcf151e
user <alex@iometelabs.io>
2022-12-20 12:02:12 +1030

    Remove unused imports from liquidity provider test

--- contracts/ethereum/test/shifting-liquidityprovider.ts
+++ contracts/ethereum/test/shifting-liquidityprovider.ts
@@ -1,9 +1,7 @@
 import * as hre from 'hardhat';
 import * as ethers from 'ethers';
-import { fUsdtAccount, fUsdtOperator } from './setup-mainnet';
-import { getStorageAt } from './test-utils';
+import { fUsdtOperator } from './setup-mainnet';
 import { AAVE_V2_POOL_PROVIDER_ADDR, AUSDT_ADDR } from '../test-constants';
-import { accountAddr } from './setup-common';
 
 describe("token aave integration", async function () {
   it("should support swapping from compound to aave v2", async function () {
