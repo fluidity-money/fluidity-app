96f2a60619db84e8838a98167fdb00e7bbe3855a
glo12345 <glorison@fluidity.money>
2022-12-28 12:08:18 +1100

    test: added unclaimed loader unit test

---
+++ web/app.fluidity.money/app/routes/$network/query/dashboard/__tests__/unclaimed.spec.ts
@@ -0,0 +1,20 @@
+import { loader } from "../unclaimed";
+
+describe("Path Loader", () => {
+  it("should return a response", async () => {
+    const reqParams = {
+      network: "ethereum",
+      address: "0xeb6b882a295d316ac62c8cfcc81c3e37c084b7c5",
+    };
+
+    const response = await loader({
+      request: new Request(`http://app.com/?address=${reqParams.address}`),
+      params: {
+        network: reqParams.network,
+      },
+      context: {},
+    });
+
+    expect(response).toBeInstanceOf(Response);
+  });
+});
