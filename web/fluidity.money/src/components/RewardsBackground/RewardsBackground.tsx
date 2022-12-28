f7e14fa8a4857bf827069a37666a56fd798a3dba
eli-d <64763513+eli-d@users.noreply.github.com>
2022-12-28 17:12:24 +1030

    memoise reward components to avoid re-renders

--- web/fluidity.money/src/components/RewardsBackground/RewardsBackground.tsx
+++ web/fluidity.money/src/components/RewardsBackground/RewardsBackground.tsx
@@ -14,6 +14,7 @@ import styles from "./RewardsBackground.module.scss";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import useViewport from "hooks/useViewport";
+import {useEffect, useMemo, useState} from "react";
 
 interface Reward {
   token: string;
@@ -23,7 +24,7 @@ interface Reward {
   transaction: string;
 }
 
-const rewardLimit = 50
+const rewardLimit = 50;
 
 const RewardsBackground = () => {
   const { apiState, chain, network } = useChainContext();
@@ -31,13 +32,13 @@ const RewardsBackground = () => {
   const { width } = useViewport();
   const { weekWinnings } = apiState;
 
-  const rewards: Reward[] = weekWinnings.map((winning) => ({
+  const rewards: Reward[] = useMemo(() => weekWinnings.map((winning) => ({
     token: winning.token_short_name,
     amount: winning.winning_amount / 10 ** winning.token_decimals,
     address: winning.winning_address,
     date: new Date(winning.awarded_time),
     transaction: winning.transaction_hash,
-  }));
+  })), [weekWinnings]);
 
   const carouselVariants = {
     appear: { x: 0 },
@@ -56,72 +57,84 @@ const RewardsBackground = () => {
     }
   };
 
-  const CarouselInfo = () => {
-    return <div>
-      {rewards
-        .slice(0, rewardLimit)
-        .sort(() => Math.random() - 0.5)
-        .map(({ token, amount, address, date, transaction }, i) => (
-          <div key={`winner-${i}`} className={styles.winner}>
-            <a
-              href={txExplorerUrl(transaction)}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={"/assets/images/tokenIcons/f" + token + ".png"}
-                alt="tokenIcon"
-              />
+  const [carouselInfo, setCarouselInfo] = useState<JSX.Element>(undefined)
 
-              <Text as="p" prominent={true} className={styles.hover}>
-                {numberToMonetaryString(amount)}{" "}
-              </Text>
+  useEffect(() => {
+    setCarouselInfo(
+      <div>
+        {rewards
+          .slice(0, rewardLimit)
+          .sort(() => Math.random() - 0.5)
+          .map(({token, amount, address, date, transaction}, i) => (
+            <div key={`winner-${i}`} className={styles.winner}>
+              <a
+                href={txExplorerUrl(transaction)}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={"/assets/images/tokenIcons/f" + token + ".png"}
+                  alt="tokenIcon"
+                />
 
-              <Text as="p" className={styles.hover}>
-                {`sent to`}{" "}
-              </Text>
+                <Text as="p" prominent={true} className={styles.hover}>
+                  {numberToMonetaryString(amount)}{" "}
+                </Text>
 
-              <Text as="p" className={styles.hoverUnderline}>
-                {`${trimAddressShort(address)}`}{" "}
-              </Text>
+                <Text as="p" className={styles.hover}>
+                  {`sent to`}{" "}
+                </Text>
 
-              <Text as="p" className={styles.hover}>
-                {formatTo12HrDate(date)}
-              </Text>
-            </a>
-          </div>
-        ))}
-    </div>
-  }
+                <Text as="p" className={styles.hoverUnderline}>
+                  {`${trimAddressShort(address)}`}{" "}
+                </Text>
+
+                <Text as="p" className={styles.hover}>
+                  {formatTo12HrDate(date)}
+                </Text>
+              </a>
+            </div>
+          ))}
+      </div>
+    )
+  }, [rewards]);
+
+  const [rewardCarousels, setRewardCarousels] = useState<JSX.Element[]>(undefined);
+
+  useEffect(() => {
+    setRewardCarousels(
+      Array.from({length: rewardLimit}).map((_, i) => (
+        <div key={`reward-carousel-${i}`}>
+          <motion.div
+            initial={width < 500 && width > 0 ? {x: -500} : {x: -1500}}
+            variants={carouselVariants}
+            animate={inView && "appear"}
+            transition={{type: "tween", duration: 5}}
+          >
+            <ContinuousCarousel background={true} direction="right">
+              {carouselInfo}
+            </ContinuousCarousel>
+          </motion.div>
+          <motion.div
+            initial={width < 500 && width > 0 ? {x: 500} : {x: 1500}}
+            variants={carouselVariants}
+            animate={inView && "appear"}
+            transition={{type: "tween", duration: 5}}
+          >
+            <ContinuousCarousel background={true} direction="left">
+              {carouselInfo}
+            </ContinuousCarousel>
+          </motion.div>
+        </div>
+      ))
+    )
+  }, [carouselInfo])
 
   return (
     <div className={styles.container}>
       <div className={styles.shade}></div>
       <div className={styles.rewardsBackground} ref={ref}>
-        {Array.from({ length: rewardLimit }).map(() => (
-          <>
-            <motion.div
-              initial={width < 500 && width > 0 ? { x: -500 } : { x: -1500 }}
-              variants={carouselVariants}
-              animate={inView && "appear"}
-              transition={{ type: "tween", duration: 5 }}
-            >
-              <ContinuousCarousel background={true} direction="right">
-                <CarouselInfo />
-              </ContinuousCarousel>
-            </motion.div>
-            <motion.div
-              initial={width < 500 && width > 0 ? { x: 500 } : { x: 1500 }}
-              variants={carouselVariants}
-              animate={inView && "appear"}
-              transition={{ type: "tween", duration: 5 }}
-            >
-              <ContinuousCarousel background={true} direction="left">
-                <CarouselInfo />
-              </ContinuousCarousel>
-            </motion.div>
-          </>
-        ))}
+      {rewardCarousels}
       </div>
     </div>
   );
