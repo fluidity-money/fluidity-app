57e4c99cd1de6b8685a0a252bcc7066d9995dcef
glo12345 <glorison@fluidity.money>
2023-01-03 12:07:18 +1100

    fix: readded table to rewards page

--- web/app.fluidity.money/app/routes/$network/dashboard/rewards/index.tsx
+++ web/app.fluidity.money/app/routes/$network/dashboard/rewards/index.tsx
@@ -588,37 +588,20 @@ export default function Rewards() {
       </section>
 
       <section id="table">
-        {transactions.length === 0 ? (
-          globalTransactionsData?.loaded !== true ? (
-            <>
-              Fetching table data...
-              <div className="center-table-loading-anim loader-dots">
-                <LoadingDots />
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="center-table-loading-anim loader-dots">
-                <Text size="lg">No reward record found!</Text>
-              </div>
-            </>
-          )
-        ) : (
-          <Table
-            itemName="rewards"
-            headings={txTableColumns}
-            pagination={{
-              page,
-              rowsPerPage: 12,
-            }}
-            count={count}
-            data={transactions}
-            renderRow={TransactionRow(network)}
-            filters={txTableFilters}
-            onFilter={setActiveTableFilterIndex}
-            activeFilterIndex={activeTableFilterIndex}
-          />
-        )}
+        <Table
+          itemName="rewards"
+          headings={txTableColumns}
+          pagination={{
+            page,
+            rowsPerPage: 12,
+          }}
+          count={count}
+          data={transactions}
+          renderRow={TransactionRow(network)}
+          filters={txTableFilters}
+          onFilter={setActiveTableFilterIndex}
+          activeFilterIndex={activeTableFilterIndex}
+        />
       </section>
 
       {/* Highest Rewarders */}
