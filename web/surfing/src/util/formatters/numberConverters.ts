65ade8bb135b6bed079875faba528020f90c3232
Joshua Steele <jsteele@nitrocade.com>
2023-01-10 11:28:32 +1030

    Fix a regression and ensure testing works

--- web/surfing/src/util/formatters/numberConverters.ts
+++ web/surfing/src/util/formatters/numberConverters.ts
@@ -72,6 +72,14 @@ const toSignificantDecimals = (num: number, decimals_?: number): string => {
       : 0;
 
   const totalSigDecimals = decimalsToFirstSig + decimals - 1;
+
+  const wholeDecimalValue = Math.round(
+    (decimalValue * 10 ** totalSigDecimals) % 10 ** totalSigDecimals
+  );
+
+  return `${numberToCommaSeparated(
+    wholeValue
+  )}.${`${wholeDecimalValue}`.padStart(decimalsToFirstSig, "0")}`;
 };
 
 //trim a string to <limit> decimal places
