07352e2ab8442fa6980191aaa1f935ef8c436706
Joshua Steele <jsteele@nitrocade.com>
2023-01-25 11:34:32 +1030

    Prettier-ify it

--- web/app.fluidity.money/app/util/server/split.ts
+++ web/app.fluidity.money/app/util/server/split.ts
@@ -3,10 +3,10 @@ import { SplitFactory } from "@splitsoftware/splitio";
 import { Attributes } from "@splitsoftware/splitio/types/splitio";
 
 const sdk = SplitFactory({
-    core: {
-        authorizationKey: process.env.SPLIT_API_KEY ?? "",
-        key: "server",
-    },
+  core: {
+    authorizationKey: process.env.SPLIT_API_KEY ?? "",
+    key: "server",
+  },
 });
 
 const client = sdk.client();
@@ -14,36 +14,57 @@ const client = sdk.client();
 /**
  * @param {string} experiment
  * @param {T} value
- * 
+ *
  * @description
  * Server only version of the split experiment hook
  * @returns {T} the value you passed in or an empty array, function, object or undefined
  */
 
-function useSplitExperiment<T extends (...vargs: unknown[]) => unknown>(experiment: string, value: T, args?: Attributes): T | (() => void);
-function useSplitExperiment<T>(experiment: string, value: T[], args?: Attributes): T[];
-function useSplitExperiment<T extends object>(experiment: string, value: object, args?: Attributes): T & { [key: string]: unknown };
-function useSplitExperiment<T>(experiment: string, value: T, args?: Attributes): T | undefined;
-function useSplitExperiment(experiment: string, value: unknown, args?: Attributes): unknown | undefined
-{
+function useSplitExperiment<T extends (...vargs: unknown[]) => unknown>(
+  experiment: string,
+  value: T,
+  args?: Attributes
+): T | (() => void);
+function useSplitExperiment<T>(
+  experiment: string,
+  value: T[],
+  args?: Attributes
+): T[];
+function useSplitExperiment<T extends object>(
+  experiment: string,
+  value: object,
+  args?: Attributes
+): T & { [key: string]: unknown };
+function useSplitExperiment<T>(
+  experiment: string,
+  value: T,
+  args?: Attributes
+): T | undefined;
+function useSplitExperiment(
+  experiment: string,
+  value: unknown,
+  args?: Attributes
+): unknown | undefined {
   if (process.env.NODE_ENV !== "production") {
     return value;
   }
 
-  const enabled = client.getTreatment(experiment, args)  === "on";
+  const enabled = client.getTreatment(experiment, args) === "on";
 
   if (!enabled) {
     if (isFunction(value)) {
-        console.log("Returning empty function");
-        return (() => { return; });
+      console.log("Returning empty function");
+      return () => {
+        return;
+      };
     }
-    
+
     if (isArray(value)) {
-        return [];
+      return [];
     }
 
     if (isObject(value)) {
-        return {};
+      return {};
     }
 
     return undefined;
@@ -52,6 +73,4 @@ function useSplitExperiment(experiment: string, value: unknown, args?: Attribute
   return value;
 }
 
-export { 
-    useSplitExperiment
-};
+export { useSplitExperiment };
