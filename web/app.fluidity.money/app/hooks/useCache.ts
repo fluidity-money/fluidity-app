9e7e88a324327968b60901e919d646c18020bb7f
Alfie Edgeworth <0xalfie@gmail.com>
2023-02-02 01:04:24 +1030

    Added suspense to useCache

--- web/app.fluidity.money/app/hooks/useCache.ts
+++ web/app.fluidity.money/app/hooks/useCache.ts
@@ -3,10 +3,10 @@ import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-type UseCache = <Data = never>(key: string) => SWRResponse<Data>;
+type UseCache = <Data = never>(key: string, suspense?: boolean) => SWRResponse<Data>;
 
-const useCache: UseCache = (key: string) => {
-  return useSWR(key, fetcher);
+const useCache: UseCache = (key: string, suspense?: boolean) => {
+  return useSWR(key, fetcher, { suspense });
 };
 
 export { useCache };
