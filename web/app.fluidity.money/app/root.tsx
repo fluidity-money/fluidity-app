32ea922d236bd5bef5135beb297540cfcddb8df4
Joshua Steele <jsteele@nitrocade.com>
2023-02-06 17:56:56 +1030

    Fix gtag import

--- web/app.fluidity.money/app/root.tsx
+++ web/app.fluidity.money/app/root.tsx
@@ -254,10 +254,18 @@ function App() {
         <Links />
         {isProduction && (
               <>
-                <script
-                  src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-                  async
+                <script dangerouslySetInnerHTML={{
+                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${GTM_ID}');`,
+                }} 
                 />
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`} 
+                /> 
                 <script
                   dangerouslySetInnerHTML={{
                     __html: `
