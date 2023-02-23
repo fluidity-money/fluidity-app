12f0200677d505c0560a503782eb58a6683c64b5
Alfie Edgeworth <0xalfie@gmail.com>
2023-02-23 15:40:21 +1030

    fixed export

--- web/app.fluidity.money/app/queries/index.ts
+++ web/app.fluidity.money/app/queries/index.ts
@@ -3,3 +3,4 @@ export * from "./useUserTransactions";
 export { default as useUserUnclaimedRewards } from "./useUserUnclaimedRewards";
 export * from "./useUserRewards";
 export * from "./useUserYield";
+export { default as useTokenRewardStatistics } from './useTokenRewardStatistics'
