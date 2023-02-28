462539028e65cb669cafc8a27a02c2707386f501
elle <elle@catgirl.software>
2023-02-28 17:22:01 +1030

    don't run pipeline tests with test-go take 2

--- tests/pipeline/main.go
+++ tests/pipeline/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"os"
+	"testing"
+)
+
+const EnvRunPipelineTests = `FLU_RUN_PIPELINE_TESTS`
+
+func TestMain(m *testing.M) {
+    if os.Getenv(EnvRunPipelineTests) == "true" {
+        m.Run()
+    }
+}
+
 func main() {
 
 }
