5028d95919024927e8201bb824b58d2896d064d3
user <alex@iometelabs.io>
2023-03-01 06:55:22 -0700

    Format

--- contracts/ethereum/contracts/VEGovLockup.sol
+++ contracts/ethereum/contracts/VEGovLockup.sol
@@ -12,141 +12,180 @@ uint256 constant MAX_LOCK_TIME = 100;
 uint256 constant FP_COEFFICIENT = 1e6;
 
 struct Lockup {
-	uint256 lockTime;
-	uint256 bptLocked;
-	uint256 lockTimestamp;
+    uint256 lockTime;
+    uint256 bptLocked;
+    uint256 lockTimestamp;
 }
 
 contract VEGovLockup {
-	IERC20 private token_;
+    IERC20 private token_;
 
-	mapping (address => Lockup) lockups_;
+    mapping (address => Lockup) lockups_;
 
-	function swag(IERC20 _token) public {
-		token_ = _token;
-	}
+    function swag(IERC20 _token) public {
+        token_ = _token;
+    }
 
-	function getLockTime(address _spender) public view returns (uint256) {
-		return lockups_[_spender].lockTime;
-	}
+    function getLockTime(address _spender) public view returns (uint256) {
+        return lockups_[_spender].lockTime;
+    }
 
-	function getBPTLocked(address _spender) public view returns (uint256) {
-		return lockups_[_spender].bptLocked;
-	}
+    function getBPTLocked(address _spender) public view returns (uint256) {
+        return lockups_[_spender].bptLocked;
+    }
 
-	function getLockTimestamp(address _spender) public view returns (uint256) {
-		return lockups_[_spender].lockTimestamp;
-	}
+    function getLockTimestamp(address _spender) public view returns (uint256) {
+        return lockups_[_spender].lockTimestamp;
+    }
 
-	function getVEFluidBalance(uint256 _bptLocked, uint256 _lockTime) public pure returns (uint256) {
-		return FP_COEFFICIENT * (_bptLocked * _lockTime) / MAX_LOCK_TIME;
-	}
+    function getVEFluidBalance(uint256 _bptLocked, uint256 _lockTime) public pure returns (uint256) {
+        return FP_COEFFICIENT * (_bptLocked * _lockTime) / MAX_LOCK_TIME;
+    }
 
-	/// @notice veFluidBalanceAtLock for the VE gov that the user receives at lock
-	function getVEFluidBalanceAtLock(address _spender) public view returns (uint256) {
-		return getVEFluidBalance(getBPTLocked(_spender), getLockTime(_spender));
-	}
+    /// @notice veFluidBalanceAtLock for the VE gov that the user receives at lock
+    function getVEFluidBalanceAtLock(address _spender) public view returns (uint256) {
+        return getVEFluidBalance(getBPTLocked(_spender), getLockTime(_spender));
+    }
 
-	/// @notice bptDecay to calculate how much bpt you get back per second
-	function getBPTDecay(address _spender) public view returns (uint256) {
-		return FP_COEFFICIENT * getBPTLocked(_spender) / getLockTime(_spender);
-	}
+    /// @notice bptDecay to calculate how much bpt you get back per second
+    function getBPTDecay(address _spender) public view returns (uint256) {
+        return FP_COEFFICIENT * getBPTLocked(_spender) / getLockTime(_spender);
+    }
 
-	/// @notice veFluidDecayPerSecond to calculate how much VE Fluid you burn per second
-	function getVEFluidDecayPerSecond(address _spender) public view returns (uint256) {
-		return  FP_COEFFICIENT * getBPTLocked(_spender) / MAX_LOCK_TIME;
-	}
+    /// @notice veFluidDecayPerSecond to calculate how much VE Fluid you burn per second
+    function getVEFluidDecayPerSecond(address _spender) public view returns (uint256) {
+        return  FP_COEFFICIENT * getBPTLocked(_spender) / MAX_LOCK_TIME;
+    }
 
-	function getSecondsSinceLock(address _spender) public view returns (uint256) {
-		return block.timestamp - getLockTimestamp(_spender);
-	}
+    function getSecondsSinceLock(address _spender) public view returns (uint256) {
+        return block.timestamp - getLockTimestamp(_spender);
+    }
 
-	/// @notice balanceOf the user's balance (the current VEFluid)
-	function balanceOf(address _spender) public view returns (uint256) {
-		uint256 veFluidBalanceAtLock = getVEFluidBalanceAtLock(_spender);
-		uint256 veFluidDecayPerSecond = getVEFluidDecayPerSecond(_spender);
-		uint256 secondsSinceLock = getSecondsSinceLock(_spender);
+    /// @notice balanceOf the user's balance (the current VEFluid)
+    function balanceOf(address _spender) public view returns (uint256) {
+        uint256 veFluidBalanceAtLock = getVEFluidBalanceAtLock(_spender);
+        uint256 veFluidDecayPerSecond = getVEFluidDecayPerSecond(_spender);
+        uint256 secondsSinceLock = getSecondsSinceLock(_spender);
 
-		return veFluidBalanceAtLock - (veFluidDecayPerSecond * secondsSinceLock);
-	}
+        return veFluidBalanceAtLock - (veFluidDecayPerSecond * secondsSinceLock);
+    }
 
-	function timestamp() public view returns (uint256) {
-		return block.timestamp;
-	}
+    function timestamp() public view returns (uint256) {
+        return block.timestamp;
+    }
 
-	/// @notice createLock for the user with the amount given (only one position!)
-	function createLock(uint256 _amount, uint256 _lockTime) public returns (uint256) {
-		require(_amount > 0, "amount = 0");
-		require(_lockTime > 0, "lock time = 0");
-		require(_lockTime < MAX_LOCK_TIME, "lock time too great");
+    /// @notice createLock for the user with the amount given (only one position!)
+    function createLock(uint256 _amount, uint256 _lockTime) public returns (uint256) {
+        require(_amount > 0, "amount = 0");
+        require(_lockTime > 0, "lock time = 0");
+        require(_lockTime < MAX_LOCK_TIME, "lock time too great");
 
-		bool rc = token_.transferFrom(msg.sender, address(this), _amount);
+        bool rc = token_.transferFrom(msg.sender, address(this), _amount);
 
-		require(rc, "failed to transfer");
+        require(rc, "failed to transfer");
 
-		require(lockups_[msg.sender].bptLocked == 0, "already locked");
+        require(lockups_[msg.sender].bptLocked == 0, "already locked");
 
-		lockups_[msg.sender].lockTime = _lockTime;
-		lockups_[msg.sender].bptLocked = _amount;
-		lockups_[msg.sender].lockTimestamp = block.timestamp;
+        lockups_[msg.sender].lockTime = _lockTime;
+        lockups_[msg.sender].bptLocked = _amount;
+        lockups_[msg.sender].lockTimestamp = block.timestamp;
 
-		return block.timestamp;
-	}
+        return block.timestamp;
+    }
 
-	function getLockExists(address _spender) public view returns (bool) {
-		return lockups_[_spender].lockTime > 0;
-	}
+    function getLockExists(address _spender) public view returns (bool) {
+        return lockups_[_spender].lockTime > 0;
+    }
 
-	function extendLockTime(address _spender, uint256 _amount) internal view returns (uint256) {
-		uint256 newLockTimestamp = block.timestamp;
+    function extendLockTime(address _spender, uint256 _amount) internal view returns (uint256) {
+        uint256 newLockTimestamp = block.timestamp;
 
-		uint256 lockTimestamp = getLockTimestamp(_spender);
+        uint256 lockTimestamp = getLockTimestamp(_spender);
 
-		uint256 lockTime = getLockTime(_spender);
+        uint256 lockTime = getLockTime(_spender);
 
-		bool lockPeriodPassed = newLockTimestamp + lockTimestamp > lockTime;
+        bool lockPeriodPassed = newLockTimestamp + lockTimestamp > lockTime;
 
-		if (lockPeriodPassed) {
-			return _amount;
-		} else {
-			// extend the lock time by taking the amount of time remaining in the
-			// lock and adding the new time that we want to it too
-			return lockTime - newLockTimestamp - lockTimestamp + _amount;
-		}
-	}
+        if (lockPeriodPassed) {
+            return _amount;
+        } else {
+            // extend the lock time by taking the amount of time remaining in the
+            // lock and adding the new time that we want to it too
+            return lockTime - newLockTimestamp - lockTimestamp + _amount;
+        }
+    }
 
-	function increaseBptAmount(uint256 _amount) public returns (uint256) {
-		require(_amount > 0, "amount = 0");
-		require(getLockExists(msg.sender), "lock doesn't exist");
+    function increaseBptAmount(uint256 _amount) public returns (uint256) {
+        require(_amount > 0, "amount = 0");
+        require(getLockExists(msg.sender), "lock doesn't exist");
 
-		bool rc = token_.transferFrom(msg.sender, address(this), _amount);
+        bool rc = token_.transferFrom(msg.sender, address(this), _amount);
 
-		require(rc, "failed to transfer tokens");
+        require(rc, "failed to transfer tokens");
 
-		uint256 newBptLocked = getBPTLocked(msg.sender) + _amount;
+        uint256 newBptLocked = getBPTLocked(msg.sender) + _amount;
 
-		uint256 newLockTime = extendLockTime(msg.sender, 0);
+        uint256 newLockTime = extendLockTime(msg.sender, 0);
 
-		lockups_[msg.sender] = Lockup({
-			lockTime: newLockTime,
-			bptLocked: newBptLocked,
-			lockTimestamp: block.timestamp
-		});
+        lockups_[msg.sender] = Lockup({
+            lockTime: newLockTime,
+            bptLocked: newBptLocked,
+            lockTimestamp: block.timestamp
+        });
 
-		require(newLockTime <= MAX_LOCK_TIME, "too long");
+        require(newLockTime <= MAX_LOCK_TIME, "too long");
 
-		return block.timestamp;
-	}
+        return block.timestamp;
+    }
 
-	function increaseLockTime(uint256 _extraLockTime) public {
-		require(_extraLockTime > 0, "extra lock time = 0");
-		require(getLockExists(msg.sender), "lock doesn't exist");
+    function increaseLockTime(uint256 _extraLockTime) public {
+        require(_extraLockTime > 0, "extra lock time = 0");
+        require(getLockExists(msg.sender), "lock doesn't exist");
 
-		uint256 newLockTime = extendLockTime(msg.sender, _extraLockTime);
+        uint256 newLockTime = extendLockTime(msg.sender, _extraLockTime);
 
-		require(newLockTime <= MAX_LOCK_TIME, "too long");
+        require(newLockTime <= MAX_LOCK_TIME, "too long");
 
-		lockups_[msg.sender].lockTime = newLockTime;
-		lockups_[msg.sender].lockTimestamp = block.timestamp;
-	}
+        lockups_[msg.sender].lockTime = newLockTime;
+        lockups_[msg.sender].lockTimestamp = block.timestamp;
+    }
+
+    function hasLockExpired(address _spender) public view returns (bool) {
+        return balanceOf(_spender) == 0;
+    }
+
+    function disableLock(address _spender) internal {
+        lockups_[_spender] = Lockup({
+            lockTime: 0,
+            bptLocked: 0,
+            lockTimestamp: 0
+        });
+    }
+
+    function withdraw(uint256 _bptAmount) public {
+        require(_bptAmonut > 0, "bpt amount = 0");
+        require(getLockExists(msg.sender), "lock doesn't exist");
+
+        uint256 bptWithdrawable = 0;
+
+        require(bptWithdrawable >= _bptAmount, "withdrawing too much");
+
+        bool rc = token_.transfer(msg.sender, _bptAmount);
+
+        require(rc, "failed to transfer tokens");
+
+        uint256 newBptLocked = getBPTLocked(msg.sender) - _amount;
+
+        uint256 newLockTime = extendLockTime(msg.sender, 0);
+
+        lockups_[msg.sender] = Lockup({
+            lockTime: newLockTime,
+            bptLocked: newBptLocked,
+            lockTimestamp: block.timestamp
+        });
+
+        require(newLockTime <= MAX_LOCK_TIME, "too long");
+
+        return block.timestamp;
+    }
 }
