679c7c8b27d8b5f610b53efa8f7ecf9974d0cf99
Joshua Steele <josh@fluidity.money>
2023-02-28 19:00:06 -0700

    Notification service update (#1728)

--- web/fanfare/src/core/register.ts
+++ web/fanfare/src/core/register.ts
@@ -8,7 +8,7 @@ import { TransactionProvider } from "../types/Transaction";
  * This function creates an event bus that is composed of multiple services.
  * If you want to add a new service, you can simply add it to the list of services.
  */
-const createEventBus = (...services: Observable<Transaction>[]) => {
+export const createEventBus = (...services: Observable<Transaction>[]) => {
     const firehose = of(...services).pipe(
         mergeAll(),
     )
