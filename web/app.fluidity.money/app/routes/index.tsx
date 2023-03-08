14b5ece48213d98c6ff3b7fcb5be12413342c75b
glo12345 <glorison@fluidity.money>
2023-03-08 12:24:38 +1100

    feat: redirect users to arbitrum, on split change

--- web/app.fluidity.money/app/routes/index.tsx
+++ web/app.fluidity.money/app/routes/index.tsx
@@ -1,7 +1,10 @@
 import { LoaderFunction, redirect } from "@remix-run/node";
+import { useSplitExperiment } from "~/util/split";
 
 export const loader: LoaderFunction = async () => {
-  const defaultNetwork = "ethereum";
+  const defaultNetwork = useSplitExperiment("enable-arbitrum", true)
+    ? "arbitrum"
+    : "ethereum";
 
   return redirect(`/${defaultNetwork}/dashboard/home`);
 };
