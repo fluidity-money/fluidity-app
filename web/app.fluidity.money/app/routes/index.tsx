b97237923b404c15e1c80550196d1747092a480f
Glorison Lai <98363943+glo12345@users.noreply.github.com>
2023-03-10 17:22:16 +1100

    feat: redirect / to /arbitrum (#1771)

--- web/app.fluidity.money/app/routes/index.tsx
+++ web/app.fluidity.money/app/routes/index.tsx
@@ -1,10 +1,7 @@
 import { LoaderFunction, redirect } from "@remix-run/node";
-import { useSplitExperiment } from "~/util/split";
 
 export const loader: LoaderFunction = async () => {
-  const defaultNetwork = useSplitExperiment("enable-arbitrum", true)
-    ? "arbitrum"
-    : "ethereum";
+  const defaultNetwork = "arbitrum";
 
   return redirect(`/${defaultNetwork}/dashboard/home`);
 };
