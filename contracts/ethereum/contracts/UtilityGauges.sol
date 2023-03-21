86988ad6eafc09f29ced90b6351904387b56e755
elle <elle@catgirl.software>
2023-03-22 09:55:00 +1030

    cleanup

--- contracts/ethereum/contracts/UtilityGauges.sol
+++ contracts/ethereum/contracts/UtilityGauges.sol
@@ -15,7 +15,13 @@ contract UtilityGauges is IUtilityGauges, IOperatorOwned {
     uint256 constant GAUGE_EPOCH_LENGTH = 7 days;
 
     event UtilityGaugesReset(uint256 indexed timestamp);
-    event Voted(address indexed token, string indexed gauge, uint256 indexed weight, uint256 gaugeWeight);
+    event Voted(
+        address indexed token,
+        string indexed gauge,
+        address indexed user,
+        uint256 weight,
+        uint256 gaugeWeight
+    );
     event NewGauge(address indexed token, string indexed gauge);
 
     struct GaugeWeight {
@@ -67,6 +73,8 @@ contract UtilityGauges is IUtilityGauges, IOperatorOwned {
         operator_ = _newOperator;
     }
 
+    /// @dev updates the lastReset_ variable
+    /// @dev must be called before lastReset_ or totalWeight_ are used!
     function _checkEpoch() internal {
         if (block.timestamp > lastReset_ + GAUGE_EPOCH_LENGTH) {
             // calculate how many epochs we're ahead by, and only move forward by that length
@@ -108,6 +116,13 @@ contract UtilityGauges is IUtilityGauges, IOperatorOwned {
         return votes.votes;
     }
 
+    /**
+     * @notice adds votes to a token/utility pair
+     *
+     * @param token the address of the fluid token
+     * @param gauge the name of the utility
+     * @param weight the number of votes to add
+     */
     function vote(address token, string memory gauge, uint256 weight) public {
         _checkEpoch();
 
@@ -127,9 +142,17 @@ contract UtilityGauges is IUtilityGauges, IOperatorOwned {
         userAmountVoted_[msg.sender].votes += weight;
         totalWeight_ += weight;
 
-        emit Voted(token, gauge, weight, data.weight);
+        emit Voted(token, gauge, msg.sender, weight, data.weight);
     }
 
+    /**
+     * @notice gets the ratio of votes in a specific gauge
+     *
+     * @param token the address of the fluid token
+     * @param gauge the name of the utility
+     *
+     * @return pair of (number of votes in that utility, number of votes total)
+     */
     function getWeight(address token, string memory gauge) public returns (uint256, uint256) {
         _checkEpoch();
 
@@ -146,6 +169,12 @@ contract UtilityGauges is IUtilityGauges, IOperatorOwned {
         return (weight, totalWeight_);
     }
 
+    /**
+     * @notice operator only, adds a new utility to be voted on
+     *
+     * @param token the address of the fluid token
+     * @param gauge the name of the utility
+     */
     function addUtility(address token, string memory gauge) public {
         require(msg.sender == operator(), "operator only");
 
