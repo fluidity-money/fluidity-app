3ac0e2c5313855e907193d85f0fec9048396fbf3
Alfie Edgeworth <0xalfie@gmail.com>
2023-03-23 15:44:37 +1030

    added ProgressBar variants and styles

--- web/surfing/src/components/ProgressBar/ProgressBar.tsx
+++ web/surfing/src/components/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,3 @@
-
 import { motion } from 'framer-motion'
 import Card, { ICard } from '../Container/Card/Card'
 import styles from './ProgressBar.module.scss'
@@ -6,27 +5,41 @@ import styles from './ProgressBar.module.scss'
 interface IProgressBar extends ICard {
   value: number
   max: number
-  type: 'box-prominent' | 'box' | 'holobox' | 'transparent' | 'gray'
+  type?: 'box-prominent' | 'box' | 'holobox' | 'transparent' | 'gray'
+  size?: 'sm' | 'md' | 'lg'
+  cap?: 'round' | 'square'
+  barColor?: string
 }
 
+// Max height is 24px due to card border radius
 const ProgressBar: React.FC<IProgressBar> = ({
   value,
   max,
   type='box-prominent',
   props,
   rounded,
+  size='md',
+  cap='round',
+  barColor
 }) => {
   return <Card 
-    className={styles.ProgressBar}
+    className={`${styles.ProgressBar} ${styles[size]}`}
     fill
     type={type}
     {...props}
     rounded={rounded}
   >
-    <motion.div 
-      className={styles.bar} 
-      style={{width: `${value / max * 100}%`}} 
-    />
+    <motion.div
+      className={`${styles.barContainer} ${styles[type]} ${styles[cap]}`}
+      style={{width: `${value / max * 100}%`,
+      background: barColor ? barColor : ''
+    }} 
+    >
+      <motion.div 
+        className={`${styles.bar} ${styles[size]} ${styles[type]}`} 
+        style={barColor ? {background: barColor } : {}}
+      />
+    </motion.div>
   </Card>
 }
 
