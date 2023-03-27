625fe67293d2488acda4225155a68acceecac0cb
user <alex@iometelabs.io>
2023-03-27 17:48:48 +1030

    Add scripts for registry

--- contracts/ethereum/hardhat.config.ts
+++ contracts/ethereum/hardhat.config.ts
@@ -254,7 +254,7 @@ module.exports = {
   },
   networks: {
     localhost: {
-      url: "http://localhost:8545",
+      url: "http://127.0.2.1:8545",
       timeout: 100000,
     },
     hardhat: {
