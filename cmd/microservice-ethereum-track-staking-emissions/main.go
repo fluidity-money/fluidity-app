1435ac751553701dddfd2a826b4cf7447cd56e74
eli-d <64763513+eli-d@users.noreply.github.com>
2023-04-05 10:52:38 +0930

    move staking decoder to common

--- cmd/microservice-ethereum-track-staking-emissions/main.go
+++ cmd/microservice-ethereum-track-staking-emissions/main.go
@@ -5,95 +5,18 @@
 package main
 
 import (
-    "fmt"
-    "math/big"
-    "time"
-
-    ethCommon "github.com/ethereum/go-ethereum/common"
-    "github.com/fluidity-money/fluidity-app/common/ethereum/fluidity"
-    "github.com/fluidity-money/fluidity-app/lib/databases/timescale/airdrop"
-    "github.com/fluidity-money/fluidity-app/lib/log"
-    ethLogs "github.com/fluidity-money/fluidity-app/lib/queues/ethereum"
-    "github.com/fluidity-money/fluidity-app/lib/types/ethereum"
-    "github.com/fluidity-money/fluidity-app/lib/types/misc"
+	"github.com/fluidity-money/fluidity-app/common/ethereum/fluidity"
+	"github.com/fluidity-money/fluidity-app/lib/databases/timescale/airdrop"
+	"github.com/fluidity-money/fluidity-app/lib/log"
+	ethLogs "github.com/fluidity-money/fluidity-app/lib/queues/ethereum"
 )
-
-func classifyStakingEvent(l ethLogs.Log) (ethereum.StakingEvent, error) {
-    var (
-        logTopics = l.Topics
-        logData   = l.Data
-
-        stakingEvent ethereum.StakingEvent
-    )
-
-    var (
-        eventSignatureString = logTopics[0].String()
-        eventSignature       = ethCommon.HexToHash(eventSignatureString)
-    )
-
-    switch eventSignature {
-    case fluidity.StakingAbi.Events["Staked"].ID:
-
-        decodedData, err := fluidity.StakingAbi.Unpack("Staked", logData)
-
-        if err != nil {
-            return stakingEvent, fmt.Errorf(
-                "failed to unpack Staked event data! %v",
-                err,
-            )
-        }
-
-        // lockupLength, lockedTimestamp, fusdcAmount, usdAmount, wethAmount
-        if dataLen := len(decodedData); dataLen != 5 {
-            return stakingEvent, fmt.Errorf(
-                "Unexpected number of log data! expected %d, got %d!",
-                5,
-                dataLen,
-            )
-        }
-
-        // event, sender address
-        if topicsLen := len(logTopics); topicsLen != 2 {
-            return stakingEvent, fmt.Errorf(
-                "Unexpected number of log topics! expected %d, got %d!",
-                2,
-                topicsLen,
-            )
-        }
-
-        var (
-            addressString      = logTopics[1].String()
-            lockupLengthInt    = decodedData[0].(*big.Int)
-            lockedTimestampInt = decodedData[1].(*big.Int)
-            usdAmountInt       = decodedData[3].(*big.Int)
-        )
-
-        if !lockedTimestampInt.IsInt64() {
-            log.Fatal(func(k *log.Log) {
-                k.Message = "Decoded a timestamp that was larger than int64!" 
-                k.Payload = lockedTimestampInt.String()
-            })
-        }
-
-        lockedTimestampInt64 := lockedTimestampInt.Int64()
-        lockedTimestamp := time.Unix(lockedTimestampInt64, 0)
-
-        stakingEvent.Address = ethereum.AddressFromString(addressString)
-        stakingEvent.InsertedDate = lockedTimestamp
-        stakingEvent.UsdAmount = misc.NewBigIntFromInt(*usdAmountInt)
-        stakingEvent.LockupLength = int(lockupLengthInt.Int64())
-
-        return stakingEvent, nil
-    default:
-        return stakingEvent, fluidity.ErrWrongEvent
-    }
-}
 
 func main() {
     ethLogs.Logs(func(l ethLogs.Log) {
-        stakingEvent, err := classifyStakingEvent(l)
+        stakingEvent, err := fluidity.TryDecodeStakingEventData(l)
 
-        if err != nil {
+        switch err {
+        case fluidity.ErrWrongEvent:
             log.Debug(func(k *log.Log) {
                 k.Format(
                     "Event for log %d of transaction %x was not a staking event!",
@@ -102,8 +25,15 @@ func main() {
                 )
             })
             return
+
+        case nil:
+            airdrop.InsertStakingEvent(stakingEvent)
+
+        default:
+            log.Fatal(func(k *log.Log) {
+                k.Message = "Failed to classify a staking event!"
+                k.Payload = err
+            })
         }
-        
-        airdrop.InsertStakingEvent(stakingEvent)
     })
 }
