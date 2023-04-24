a8c8f49cdf25b404e4eac45583083c51660edda0
glo12345 <glorison@fluidity.money>
2023-04-24 17:35:40 +1000

    fix: prevent modals from closing on click

--- web/app.fluidity.money/app/components/ReferralModal/index.tsx
+++ web/app.fluidity.money/app/components/ReferralModal/index.tsx
@@ -44,6 +44,9 @@ const ReferralModal = ({
 
   return (
     <Card
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
       className="referrals-container"
       type="frosted"
       border="solid"
