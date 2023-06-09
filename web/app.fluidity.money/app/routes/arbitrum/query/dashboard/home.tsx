fdf79ef5a6b61143c8b5a63e37cb496c86aabd71
eli-d <64763513+eli-d@users.noreply.github.com>
2023-06-09 14:09:47 +0930

    add arbitrum queries

---
+++ web/app.fluidity.money/app/routes/arbitrum/query/dashboard/home.tsx
@@ -0,0 +1,78 @@
+import type { TimeSepUserYield } from "~/queries/useUserYield";
+
+import { LoaderFunction, json, } from "@remix-run/node";
+import { jsonGet } from "~/util";
+import { useUserYieldAll, useUserYieldByAddress } from "~/queries";
+import config from "~/webapp.config.server";
+import {GraphData, useGraphData} from "~/queries/useGraphData";
+import {ArbitrumVolume} from "../volumeStats";
+import {Chain} from "~/util/chainUtils/chains";
+
+export type HomeLoaderData = {
+  rewards: TimeSepUserYield;
+  volumeStats: ArbitrumVolume;
+  graph: GraphData;
+  totalFluidPairs: number;
+  network: Chain;
+  timestamp: number;
+  loaded: boolean;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const network = "arbitrum"
+
+  const url = new URL(request.url);
+  const address = url.searchParams.get("address");
+
+  const fluidPairs = config.config[network ?? ""].fluidAssets.length;
+
+  const timestamp = new Date().getTime();
+
+  try {
+    const [
+      { volume },
+      { data: rewardsData, errors: rewardsErr },
+      { data: graphData, errors: graphErr }
+    ] = await Promise.all([
+      address
+        ? jsonGet<{ address: string }, { volume: ArbitrumVolume }>(
+            `${url.origin}/${network}/query/volumeStats`,
+            {
+              address,
+            }
+          )
+        : jsonGet<Record<string, never>, { volume: ArbitrumVolume }>(
+            `${url.origin}/${network}/query/volumeStats`
+          ),
+      address
+        ? useUserYieldByAddress(network ?? "", address)
+        : useUserYieldAll(network ?? ""),
+      useGraphData(network ?? "")
+    ]);
+
+    if (!volume) {
+      throw new Error("Could not fetch volume data");
+    }
+
+    if (rewardsErr || !rewardsData) {
+      throw new Error("Could not fetch rewards data");
+    }
+
+    if (graphErr || !graphData) {
+      throw new Error("Could not fetch graph data");
+    }
+
+    return json({
+      rewards: rewardsData,
+      graph: graphData,
+      volumeStats: volume,
+      totalFluidPairs: fluidPairs,
+      network,
+      timestamp,
+      loaded: true,
+    } satisfies HomeLoaderData);
+  } catch (err) {
+    console.log(err);
+    throw new Error(`Could not fetch Transactions on ${network}: ${err}`);
+  } // Fail silently - for now.
+};
---
+++ web/app.fluidity.money/app/routes/arbitrum/query/volumeStats.tsx
@@ -0,0 +1,52 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import {useVolumeStats, useVolumeStatsByAddress} from "~/queries/useVolumeStats";
+
+export type ArbitrumVolume = {
+  day: [{
+    totalVolume: number;
+    actionCount: number;
+  }]
+  week: [{
+    totalVolume: number;
+    actionCount: number;
+  }]
+  month: [{
+    totalVolume: number;
+    actionCount: number;
+  }]
+  year: [{
+    totalVolume: number;
+    actionCount: number;
+  }]
+  all: [{
+    totalVolume: number;
+    actionCount: number;
+  }]
+};
+
+export type ArbitrumVolumeLoaderData = {
+  volume?: ArbitrumVolume;
+  loaded: boolean;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const network = "arbitrum";
+
+  const url = new URL(request.url);
+  const address = url.searchParams.get("address");
+
+  const {data: volumeData, errors: volumeErrors}= address
+    ? await useVolumeStatsByAddress(
+        network,
+        address,
+      )
+    : await useVolumeStats(network);
+
+  if (!volumeData || volumeErrors)
+    throw volumeErrors
+
+  return json({
+    volume: volumeData,
+    loaded: true,
+  } satisfies ArbitrumVolumeLoaderData);
+};
