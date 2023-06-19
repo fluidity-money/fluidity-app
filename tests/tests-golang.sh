4435d370932c185162700b641b938c2e5e600b78
user <alex@iometelabs.io>
2023-06-19 10:20:33 +0930

    Add check inside the tests-golang.sh script for database availability

--- tests/tests-golang.sh
+++ tests/tests-golang.sh
@@ -1,5 +1,19 @@
-#!/bin/sh -e
+#!/bin/sh
 
 . ./tests-profile.sh
+
+for i in `seq 5`; do
+	psql "$FLU_TIMESCALE_URI" --quiet -c 'select 1;' 2>&1 >/dev/null
+	if [ $? = 0 ]; then break; fi
+	sleep 5
+	>&2 echo 'retrying timescale'
+done
+
+for i in `seq 5`; do
+	psql "$FLU_POSTGRES_URI" --quiet -c 'select 1;' 2>&1 >/dev/null
+	if [ $? = 0 ]; then break; fi
+	sleep 5
+	>&2 echo 'retrying postgres'
+done
 
 go test ../...
