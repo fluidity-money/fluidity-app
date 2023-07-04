53fee5ea07e889ff43bcaad0c1db96fa2fbccd9f
elle <elle@catgirl.software>
2023-07-04 11:41:55 +0930

    fail contract tests if any tests failed

--- contracts/ethereum/run-tests.sh
+++ contracts/ethereum/run-tests.sh
@@ -8,16 +8,30 @@ if [ -z "$FLU_ETHEREUM_FORKNET_URL_GOERLI" ] || [ -z "FLU_ETHEREUM_FORKNET_URL_M
 	exit 1
 fi
 
+failedcode=0
+run_test() {
+	"$@"
+	ret=$?
+	if [[ "$ret" -ne 0 ]]; then
+		>&2 echo "test command '$@' failed with status $ret!"
+		failedcode=$ret
+	fi
+}
+
 >&2 echo "testing arbitrum..."
 
-FLU_FORKNET_NETWORK=arbitrum $HARDHAT test
-$FORGE test
+FLU_FORKNET_NETWORK=arbitrum run_test $HARDHAT test
+run_test $FORGE test
 
 >&2 echo "testing mainnet..."
 
-FLU_FORKNET_NETWORK=mainnet $HARDHAT test
+FLU_FORKNET_NETWORK=mainnet run_test $HARDHAT test
 
 >&2 echo "testing goerli..."
 
-FLU_FORKNET_NETWORK=goerli $HARDHAT test
+FLU_FORKNET_NETWORK=goerli run_test $HARDHAT test
 
+if [ "$failedcode" -ne 0 ]; then
+	>&2 echo "some tests failed!"
+	exit $failedcode
+fi
