da979846d69f27129a6254ecf2714804ad47b34c
Maria Rykova <maria.nigay@yandex.ru>
2024-01-22 09:20:41 +0800

    remove installGlobals to watch how npm-test run

--- web/app.fluidity.money/app/queries/useUserUnclaimedRewards.spec.ts
+++ web/app.fluidity.money/app/queries/useUserUnclaimedRewards.spec.ts
@@ -1,9 +1,9 @@
 import { installGlobals } from "@remix-run/node";
 import useUserUnclaimedRewards from "./useUserUnclaimedRewards";
 
-beforeAll(() => {
-  installGlobals();
-});
+//beforeAll(() => {
+//  installGlobals();
+//});
 
 describe("useUserUnclaimedRewards", () => {
   it("should return a valid response", async () => {
