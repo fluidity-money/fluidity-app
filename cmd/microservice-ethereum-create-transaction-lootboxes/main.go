c6f6ba9ca91374ba541a34bc679bec3002a8eac5
user <alex@iometelabs.io>
2024-01-22 19:02:55 +1030

    Increase protocol rewards even further

--- cmd/microservice-ethereum-create-transaction-lootboxes/main.go
+++ cmd/microservice-ethereum-create-transaction-lootboxes/main.go
@@ -362,7 +362,7 @@ func main() {
 func protocolMultiplier(application applications.Application) *big.Rat {
 	switch application {
 	case applications.ApplicationJumper, applications.ApplicationUniswapV3, applications.ApplicationTraderJoe, applications.ApplicationCamelot, applications.ApplicationSushiswap, applications.ApplicationRamses:
-		return big.NewRat(3, 5)
+		return big.NewRat(4, 5)
 	}
 
 	return big.NewRat(1, 10)
