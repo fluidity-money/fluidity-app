423a8a4b0354b94a27b7957a540f332e84c72b34
eli-d <64763513+eli-d@users.noreply.github.com>
2024-01-29 17:21:42 +1030

    add camelot v3 to protocol multipliers (#2516)

--- cmd/microservice-ethereum-create-transaction-lootboxes/main.go
+++ cmd/microservice-ethereum-create-transaction-lootboxes/main.go
@@ -401,7 +401,7 @@ func main() {
 
 func protocolMultiplier(application applications.Application) *big.Rat {
 	switch application {
-	case applications.ApplicationJumper, applications.ApplicationUniswapV3, applications.ApplicationTraderJoe, applications.ApplicationCamelot, applications.ApplicationSushiswap, applications.ApplicationRamses:
+	case applications.ApplicationJumper, applications.ApplicationUniswapV3, applications.ApplicationTraderJoe, applications.ApplicationCamelot, applications.ApplicationCamelotV3, applications.ApplicationSushiswap, applications.ApplicationRamses:
 		return big.NewRat(3, 5)
 	}
 
