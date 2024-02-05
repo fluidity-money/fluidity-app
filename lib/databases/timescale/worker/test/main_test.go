4839e4932150b5aa6dc6b482003c614cfb227745
user <alex@iometelabs.io>
2024-02-05 18:06:41 +0800

    Add a cleanup stage to the testing for the emissions

--- lib/databases/timescale/worker/test/main_test.go
+++ lib/databases/timescale/worker/test/main_test.go
@@ -7,14 +7,35 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/fluidity-money/fluidity-app/lib/timescale"
 	"github.com/fluidity-money/fluidity-app/lib/types/network"
 	"github.com/fluidity-money/fluidity-app/lib/databases/timescale/worker"
 )
 
 func TestEmptyInsertion(t *testing.T) {
+	var (
+		testingHash = "0xtesting123123"
+		network_ = string(network.NetworkArbitrum)
+	)
+
 	worker.InsertEmissions(worker.Emission{
-		Network: string(network.NetworkArbitrum),
+		Network: network_,
+		TransactionHash: testingHash,
 	})
+
+	timescale := timescale.Client()
+
+	statementText := fmt.Sprintf(
+		"DELETE FROM %s WHERE transaction_hash = $1 AND network = $2",
+		worker.TableEmissions,
+	)
+
+	_, err := timescale.Exec(statementText, testingHash, network_)
+
+	if err != nil {
+		t.Fatalf("failed to exec deletion transaction: %v", err)
+	}
 }
