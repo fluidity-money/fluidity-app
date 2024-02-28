6b0a01af2052664f1dd7492f5d35d7db905de2b5
eli-d <64763513+eli-d@users.noreply.github.com>
2024-02-28 17:37:07 +1030

    implement sui connector by paginating every checkpoint

--- cmd/connector-sui-amqp/README.md
+++ cmd/connector-sui-amqp/README.md
@@ -1,13 +1,15 @@
 
 # connector-sui-amqp
 
-An all-in-one connector for reading events from the Sui blockchain to be processed by the worker and database infrastructure.
+An all-in-one connector for reading events from the Sui blockchain to be processed by the worker and database infrastructure. Assumes only a single instance running at a time, and uses a single Redis "last seen checkpoint" key as opposed to Solana's multi-websocket design requiring synchronisation to avoid duplicating slots.
 
 ## Environment variables
 
 |             Name             |                                  Description
 |------------------------------|------------------------------------------------------------------------------|
-| `ENV_SUI_WS_URL`             | URL of the Sui RPC Websocket for event subscription.                         |
+| `FLU_SUI_HTTP_URL`                     | URL of the Sui RPC Websocket for event subscription.               |
+| `FLU_SUI_FIRST_CHECKPOINT`             | Number of the checkpoint to start watching from.                   |
+| `FLU_SUI_PAGINATION_WAIT_TIME_SECONDS` | Time to wait for new checkpoints to be added, in seconds.          |
 
 ## Building
 
--- cmd/connector-sui-amqp/main.go
+++ cmd/connector-sui-amqp/main.go
@@ -6,59 +6,214 @@ package main
 
 import (
 	"context"
+	"math/big"
+	"strconv"
+	"time"
 
 	"github.com/block-vision/sui-go-sdk/models"
+
 	"github.com/block-vision/sui-go-sdk/sui"
-	"github.com/block-vision/sui-go-sdk/utils"
+	"github.com/fluidity-money/fluidity-app/cmd/connector-solana-amqp/lib/redis"
 	"github.com/fluidity-money/fluidity-app/lib/log"
+	"github.com/fluidity-money/fluidity-app/lib/queue"
+	sui_queue "github.com/fluidity-money/fluidity-app/lib/queues/sui"
+	"github.com/fluidity-money/fluidity-app/lib/types/misc"
+	sui_types "github.com/fluidity-money/fluidity-app/lib/types/sui"
 	"github.com/fluidity-money/fluidity-app/lib/util"
 )
 
 const (
-    EnvSuiWsUrl = `FLU_SUI_WS_URL`
+	RedisLastCheckpoint  = `sui.last-checkpoint`
+	RedisSentCheckpoints = `sui.sent-checkpoints`
+
+	// RedisBufferSize is the length of the buffer for previously seen blocks
+	RedisBufferSize = 100
+)
+
+const (
+	// EnvSuiHttpUrl is the HTTP URL of a Sui RPC endpoint
+	EnvSuiHttpUrl = `FLU_SUI_HTTP_URL`
+
+	// EnvFirstCheckpoint is the checkpoint to begin watching from, overriding the last block
+	EnvFirstCheckpoint = `FLU_SUI_FIRST_CHECKPOINT`
+
+	// EnvPaginationWaitTime is the number of seconds to wait between paginating checkpoints
+	EnvPaginationWaitTime = `FLU_SUI_PAGINATION_WAIT_TIME_SECONDS`
 )
 
 func main() {
-    var (
-        suiWsUrl = util.GetEnvOrFatal(EnvSuiWsUrl)
-    )
+	var (
+		suiHttpUrl = util.GetEnvOrFatal(EnvSuiHttpUrl)
+		// by default, start from the very first block
+		firstCheckpointEnv = util.GetEnvOrDefault(EnvFirstCheckpoint, "0")
+		// by default, wait 5 seconds when no checkpoints are available, doubling each time
+		waitTimeString = util.GetEnvOrDefault(EnvPaginationWaitTime, "5")
 
-    var (
-        client = sui.NewSuiWebsocketClient(suiWsUrl)
-        ch     = make(chan(models.SuiEventResponse))
-        ctx    = context.Background()
-    )
+		httpClient = sui.NewSuiClient(suiHttpUrl)
+	)
 
-    err := client.SubscribeEvent(
-        ctx,
-        models.SuiXSubscribeEventsRequest{
-            // subscribe to all relevant events (separately?)
-            // will need filters for fluid token send/swaps/wins, utility wins, application swaps
-            SuiEventFilter: models.EventFilterByPackage{
-                Package: "",
-            },
-        },
-        ch,
-    )
+	firstCheckpoint, err := strconv.ParseUint(firstCheckpointEnv, 10, 64)
 
-    if err != nil {
-        log.Fatal(func(k *log.Log) {
-            k.Message = "Failed to subscribe to Sui events!"
-            k.Payload = err
-        })
-    }
+	if err != nil {
+		log.Fatal(func(k *log.Log) {
+			k.Message = "Failed to parse starting checkpoint from env!"
+			k.Payload = err
+		})
+	}
 
-    for {
-        select {
-        // receive Sui transaction effects
-        case msg := <-ch:
-            utils.PrettyPrint(msg)
-            // send through amqp
-        case <-ctx.Done():
-            log.App(func(k *log.Log) {
-                k.Message = "Context finished, quitting!"
-            })
-        return
-        }
-    }
+	// if unset, try use the last checkpoint
+	if firstCheckpoint == 0 {
+		firstCheckpoint = redis.GetLastBlock(RedisLastCheckpoint)
+	}
+
+	// if env unset and nothing in Redis, start from the very first block
+	// TODO - this maybe shouldn't be allowed as the contract will never be deployed in the first block
+	if firstCheckpoint != 0 {
+		firstCheckpoint -= 1
+	}
+
+	waitSeconds, err := strconv.ParseUint(waitTimeString, 10, 64)
+
+	if err != nil {
+		log.Fatal(func(k *log.Log) {
+			k.Message = "Failed to parse pagination wait time from env!"
+			k.Payload = err
+		})
+	}
+
+	waitTime := time.Duration(time.Second * time.Duration(waitSeconds))
+
+	paginateCheckpoints(httpClient, firstCheckpoint, waitTime)
+}
+
+// paginateCheckpoints to infinitely search for new checkpoints and send them down a queue
+func paginateCheckpoints(client sui.ISuiAPI, firstCheckpoint uint64, waitDuration time.Duration) {
+	var lastCheckpoint uint64 = redis.GetLastBlock(RedisLastCheckpoint)
+
+	// user-set checkpoint takes precedence
+	if lastCheckpoint < firstCheckpoint {
+		lastCheckpoint = firstCheckpoint
+	}
+
+	for {
+		currentCheckpoint, err := client.SuiGetLatestCheckpointSequenceNumber(context.Background())
+
+		if err != nil {
+			log.Fatal(func(k *log.Log) {
+				k.Message = "Failed to get latest checkpoint sequence number!"
+				k.Payload = err
+			})
+		}
+
+		log.Debug(func(k *log.Log) {
+			k.Format(
+				"Last checkpoint %v, first %v, current %v",
+				lastCheckpoint,
+				firstCheckpoint,
+				currentCheckpoint,
+			)
+		})
+
+		// wait with a backoff for there to be at least 25 new checkpoints
+		if currentCheckpoint <= lastCheckpoint+25 {
+			log.Debug(func(k *log.Log) {
+				k.Format(
+					"Last checkpoint %v, current %v, wanted %v, waiting %v seconds!",
+					lastCheckpoint,
+					currentCheckpoint,
+					lastCheckpoint+25,
+					waitDuration.Seconds(),
+				)
+			})
+
+			time.Sleep(waitDuration)
+			waitDuration *= 2
+
+			continue
+		} else {
+			waitDuration = time.Duration(time.Second * 5)
+		}
+
+		cursor := strconv.Itoa(int(lastCheckpoint))
+
+		// not up to date, fetch up to limit (50 is a hard limit in the Go SDK - the RPC limit is either 50 or 100)
+		checkpointsResponse, err := client.SuiGetCheckpoints(context.Background(), models.SuiGetCheckpointsRequest{
+			Cursor:          cursor,
+			Limit:           50,
+			DescendingOrder: false,
+		})
+
+		if err != nil {
+			log.Fatal(func(k *log.Log) {
+				k.Format(
+					"Failed to get Sui checkpoints using cursor %v",
+					lastCheckpoint,
+				)
+				k.Payload = err
+			})
+		}
+
+		// send minimal checkpoints individually down queue
+		for _, checkpoint := range checkpointsResponse.Data {
+			var (
+				sequenceNumberString = checkpoint.SequenceNumber
+				timestampMs          = checkpoint.TimestampMs
+				transactions         = checkpoint.Transactions
+			)
+
+			sequenceNumber, err := strconv.ParseUint(sequenceNumberString, 10, 64)
+
+			if err != nil {
+				log.Fatal(func(k *log.Log) {
+					k.Format(
+						"Failed to convert sequence number %v to a number!",
+						sequenceNumberString,
+					)
+					k.Payload = err
+				})
+			}
+
+			// convert Unix MS timestamp to Go time
+			timestamp_, err := strconv.ParseInt(timestampMs, 10, 64)
+
+			if err != nil {
+				log.Fatal(func(k *log.Log) {
+					k.Format(
+						"Failed to convert checkpoint timestamp %v to a number!",
+						timestampMs,
+					)
+					k.Payload = err
+				})
+			}
+
+			timestamp := time.Unix(timestamp_/1000, 0).UTC()
+
+			redis.WriteLastBlock(RedisLastCheckpoint, sequenceNumber)
+
+			sequenceNumberInt, _ := new(big.Int).SetString(sequenceNumberString, 10)
+
+			checkpoint := sui_types.Checkpoint{
+				SequenceNumber: misc.NewBigIntFromInt(*sequenceNumberInt),
+				Timestamp:      timestamp,
+				Transactions:   transactions,
+			}
+			queue.SendMessage(sui_queue.TopicCheckpoints, checkpoint)
+
+			log.Debug(func(k *log.Log) {
+				k.Format(
+					"Sent checkpoint %v to queue",
+					sequenceNumber,
+				)
+			})
+
+			lastCheckpoint = sequenceNumber
+		}
+
+		if !checkpointsResponse.HasNextPage {
+			log.Debug(func(k *log.Log) {
+				k.Message = "Up to date, sleeping!"
+			})
+			time.Sleep(waitDuration)
+		}
+	}
 }
