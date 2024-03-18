07c412e211d53940365456199d6c8b8a02edca7a
eli-d <64763513+eli-d@users.noreply.github.com>
2024-03-18 18:13:09 +1030

    t

--- lib/types/user-actions/user-actions.go
+++ lib/types/user-actions/user-actions.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	solApplications "github.com/fluidity-money/fluidity-app/common/solana/applications"
+	suiApplications "github.com/fluidity-money/fluidity-app/common/sui/applications"
 	"github.com/fluidity-money/fluidity-app/lib/types/applications"
 	"github.com/fluidity-money/fluidity-app/lib/types/ethereum"
 	"github.com/fluidity-money/fluidity-app/lib/types/misc"
@@ -293,6 +294,11 @@ func NewSendSolana(senderAddress, recipientAddress, transactionHash string, amou
 }
 
 func NewSendSui(network_ network.BlockchainNetwork, senderAddress, recipientAddress, transactionHash string, amount, txIndex misc.BigInt, fee *big.Rat, application *string, tokenShortName string, tokenDecimals int) UserAction {
+	applicationString := suiApplications.ApplicationNone.String()
+	if application == nil {
+		applicationString = *application
+	}
+
 	return UserAction{
 		Network:          network.NetworkSui,
 		Type:             "send",
@@ -305,7 +311,7 @@ func NewSendSui(network_ network.BlockchainNetwork, senderAddress, recipientAddr
 		TokenDetails:     token_details.New(tokenShortName, tokenDecimals),
 		Time:             time.Now(),
 		AdjustedFee:      fee,
-		Application:      *application,
+		Application:      applicationString,
 	}
 }
 
