48998729ffdef5d4bdd29056c09b7b5a1d9a21ec
eli-d <64763513+eli-d@users.noreply.github.com>
2024-03-28 15:29:59 +1030

    add stake/unstake icons

---
+++ web/surfing/src/components/Images/StakeIcon.tsx
@@ -0,0 +1,4 @@
+import { ReactComponent as StakeIcon } from "~/assets/images/StakeIcon.svg"
+import { ReactComponent as UnstakeIcon } from "~/assets/images/UnstakeIcon.svg"
+
+export { StakeIcon, UnstakeIcon };
