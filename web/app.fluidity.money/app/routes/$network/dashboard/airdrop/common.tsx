61bc037dfdecbbb087104bd84f6ddf03d94d3975
user <alex@iometelabs.io>
2024-04-04 00:05:08 +1030

    amount argument

--- web/app.fluidity.money/app/routes/$network/dashboard/airdrop/common.tsx
+++ web/app.fluidity.money/app/routes/$network/dashboard/airdrop/common.tsx
@@ -2127,7 +2127,7 @@ const RecapModal = ({
   const [completedClaimStakeModal, setCompletedClaimStakeModal] = useState(false);
 
   // called when someone completes the staking modal with a claim complete state.
-  const handleClaimStakingModalComplete = (_amount: BN) => {
+  const handleClaimStakingModalComplete = (_: BN) => {
     setCompletedClaimStakeModal(true);
   };
 
