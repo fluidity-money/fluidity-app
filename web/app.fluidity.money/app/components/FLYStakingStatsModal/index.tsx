e71838d9386c029e6a8b4ca9aff837d37db66dd9
user <alex@iometelabs.io>
2024-04-04 03:47:03 +1030

    Hack to show if nothing shows

--- web/app.fluidity.money/app/components/FLYStakingStatsModal/index.tsx
+++ web/app.fluidity.money/app/components/FLYStakingStatsModal/index.tsx
@@ -376,7 +376,12 @@ const FlyStakingStatsModal = ({ visible, close, showConnectWalletModal, shouldUp
                               }
                             >
                               <div className="flex-column">
-                                <Text size="lg" prominent>{getValueFromFlyAmount(new BN(flyStaked.toString()))?.toString()}</Text>
+                                <Text size="lg" prominent>{
+                                  (() => {
+                                    const s = getValueFromFlyAmount(new BN(flyStaked.toString()))?.toString();
+                                    if (!s) return flyStaked.toString();
+                                    return s;
+                                  })() }</Text>
                                 <div className="text-with-info-popup">
                                   <Text size="lg">Staked</Text>
                                   <InfoCircle className="info-circle-grey" />
