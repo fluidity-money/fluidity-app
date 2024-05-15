d5311c95e66865a75753299e0a30c3106f44405b
Alex <71931113+af-afk@users.noreply.github.com>
2024-05-15 12:12:56 +0100

    Develop yield to stakers new migration (#2644)
    * Tweak the migration for the yield to stakers
    
    * Also add sqlfluff to the actions

--- database/101-up-postgres/20240508052809-worker_config_enable_yield_to_stakers.sql
+++ database/101-up-postgres/20240508052809-worker_config_enable_yield_to_stakers.sql
@@ -2,9 +2,9 @@
 -- migrate:up
 
 ALTER TABLE worker_config_ethereum
-	ADD COLUMN yield_to_stakers NOT NULL DEFAULT FALSE;
+	ADD COLUMN yield_to_stakers BOOLEAN NOT NULL DEFAULT FALSE;
 
 -- migrate:down
 
 ALTER TABLE worker_config_ethereum
-	DROP COLUMN yield_to_stakers NOT NULL DEFAULT FALSE;
+	DROP COLUMN yield_to_stakers;
