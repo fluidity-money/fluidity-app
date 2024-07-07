acf341afd781f2dc0573c1fb8c3d76b61a6a154a
Alex <71931113+af-afk@users.noreply.github.com>
2024-07-07 15:23:39 +0930

    Finalise epoch 3 (#2665)
    * Finalise epoch 3
    
    * Try to fix a Sui issue by making a field explicitly any and add tanstack
    
    * Ignore any
    
    * Remove the any explicit type and the ts-ignore

--- web/app.fluidity.money/app/routes/$network/dashboard/airdrop/index.tsx
+++ web/app.fluidity.money/app/routes/$network/dashboard/airdrop/index.tsx
@@ -1072,10 +1072,8 @@ const AirdropStats = ({
   navigate,
   isMobile,
 }: IAirdropStats) => {
-  const dayDiff = epochMax - epochDays;
-  const epochDaysLeft = dayDiff > 0 ? dayDiff : 0;
-  const percentage = Math.floor((epochDays / epochMax) * 100);
-  const epochPercentage = percentage < 100 ? percentage : 100;
+  const epochDaysLeft = 0;
+  const epochPercentage = 100;
 
   return (
     <div
