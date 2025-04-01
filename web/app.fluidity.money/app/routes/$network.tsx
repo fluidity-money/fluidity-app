22266a6ac907f7e55a54ca8807073b121c5dc105
bayge <alex@fluidity.money>
2025-04-01 12:25:02 +0300

    Whitespace commit

--- web/app.fluidity.money/app/routes/$network.tsx
+++ web/app.fluidity.money/app/routes/$network.tsx
@@ -5,6 +5,8 @@ import { redirect } from "@remix-run/node";
 import { useEffect, useMemo, useState } from "react";
 import config from "../../webapp.config.js";
 
+//
+
 import EthereumProvider from "contexts/EthereumProvider";
 
 import { Fragment } from "react";
